7299e4c2fe18062526de7e4cdab4954553c364a1
Joshua Hawxwell <m@hawx.me>
2022-03-09 09:11:25 +0000

    Detect when in an iframe

--- web/assets/main.js
+++ web/assets/main.js
@@ -4,14 +4,30 @@ import $ from "jquery";
 import "./main.scss";
 
 document.body.className = document.body.className
-  ? document.body.className + " js-enabled"
-  : "js-enabled";
+    ? document.body.className + " js-enabled"
+    : "js-enabled";
 
 // Expose jQuery on window so MOJFrontend can use it
 window.$ = $;
 
 // we aren't using the JS tabs, but they try to initialise this will stop them breaking
-GOVUKFrontend.Tabs.prototype.setup = () => {};
+GOVUKFrontend.Tabs.prototype.setup = () => { };
 
 GOVUKFrontend.initAll();
 MOJFrontend.initAll();
+
+if (window.self !== window.top) {
+    document.body.className += " app-!-embedded";
+
+    const success = document.querySelector('.moj-banner--success');
+    if (success) {
+        window.top.postMessage('form-done', `${window.location.protocol}//${window.location.host}`);
+    }
+
+    document.querySelectorAll('[data-app-iframe-cancel]').forEach(el => {
+        el.addEventListener('click', event => {
+            window.top.postMessage('form-cancel', `${window.location.protocol}//${window.location.host}`);
+            event.preventDefault();
+        });
+    });
+}
