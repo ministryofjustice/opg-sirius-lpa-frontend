4b9061d9e8f22c4ba67c75c583aa3c2466140e7e
Joshua Hawxwell <m@hawx.me>
2022-03-22 08:27:59 +0000

    Add form to create an event

---
+++ cypress/integration/create-event.spec.js
@@ -0,0 +1,18 @@
+describe("Create an event", () => {
+    beforeEach(() => {
+        cy.setCookie("Other", "other");
+        cy.setCookie("XSRF-TOKEN", "abcde");
+        cy.setCookie("OPG-Bypass-Membrane", "1");
+        cy.visit("/create-event?id=800&entity=lpa");
+    });
+
+    it("creates an event", () => {
+        cy.contains("Create Event");
+        cy.get(".moj-banner").should("not.exist");
+        cy.get("#f-type").select("Application processing");
+        cy.get("#f-name").type("A title");
+        cy.get("#f-description").type("More words");
+        cy.get("button[type=submit]").click();
+        cy.get(".moj-banner").should("exist");
+    });
+});
