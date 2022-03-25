91a90f5d8fefac333077d34058629565f131b7d0
Joshua Hawxwell <m@hawx.me>
2022-03-25 11:06:23 +0000

    Use accessible-autocomplete

--- web/assets/main.js
+++ web/assets/main.js
@@ -1,17 +1,17 @@
 import MOJFrontend from "@ministryofjustice/frontend/moj/all.js";
+import accessibleAutocomplete from 'accessible-autocomplete';
 import GOVUKFrontend from "govuk-frontend/govuk/all.js";
 import $ from "jquery";
-import TomSelect from "tom-select";
 
 document.body.className = document.body.className
-  ? document.body.className + " js-enabled"
-  : "js-enabled";
+    ? document.body.className + " js-enabled"
+    : "js-enabled";
 
 // Expose jQuery on window so MOJFrontend can use it
 window.$ = $;
 
 // we aren't using the JS tabs, but they try to initialise this will stop them breaking
-GOVUKFrontend.Tabs.prototype.setup = () => {};
+GOVUKFrontend.Tabs.prototype.setup = () => { };
 
 GOVUKFrontend.initAll();
 MOJFrontend.initAll();
@@ -20,48 +20,50 @@ const prefix = document.body.getAttribute("data-prefix");
 
 const selectUser = document.querySelector("[data-select-user]");
 if (selectUser) {
-  new TomSelect("[data-select-user]", {
-    maxItems: 1,
-    create: false,
-    valueField: "id",
-    labelField: "displayName",
-    searchField: "displayName",
-    load(query, callback) {
-      fetch(`${prefix}/search-users?q=${encodeURIComponent(query)}`)
-        .then((response) => response.json())
-        .then((json) => {
-          callback(
-            json.map(({ id, displayName }) => ({
-              id: id + ":" + displayName,
-              displayName,
-            }))
-          );
-        })
-        .catch(() => {
-          callback();
-        });
-    },
-  });
+    accessibleAutocomplete.enhanceSelectElement({
+        selectElement: selectUser,
+        minLength: 3,
+        confirmOnBlur: false,
+        source(query, callback) {
+            fetch(`${prefix}/search-users?q=${encodeURIComponent(query)}`)
+                .then((response) => response.json())
+                .then((json) => {
+                    callback(
+                        json.map(({ id, displayName }) => ({ id, text: displayName }))
+                    );
+                })
+                .catch(() => {
+                    callback([]);
+                });
+        },
+        templates: {
+            inputValue(value) { return !value ? '' : value.text },
+            suggestion(value) { return value.text },
+        },
+        onConfirm(selected) {
+            selectUser.innerHTML = `<option value="${selected.id}:${selected.text}" selected>${selected.text}</option>`;
+        },
+    });
 }
 
 if (window.self !== window.parent) {
-  document.body.className += " app-!-embedded";
+    document.body.className += " app-!-embedded";
 
-  const success = document.querySelector(".moj-banner--success");
-  if (success) {
-    window.parent.postMessage(
-      "form-done",
-      `${window.location.protocol}//${window.location.host}`
-    );
-  }
+    const success = document.querySelector(".moj-banner--success");
+    if (success) {
+        window.parent.postMessage(
+            "form-done",
+            `${window.location.protocol}//${window.location.host}`
+        );
+    }
 
-  document.querySelectorAll("[data-app-iframe-cancel]").forEach((el) => {
-    el.addEventListener("click", (event) => {
-      window.parent.postMessage(
-        "form-cancel",
-        `${window.location.protocol}//${window.location.host}`
-      );
-      event.preventDefault();
+    document.querySelectorAll("[data-app-iframe-cancel]").forEach((el) => {
+        el.addEventListener("click", (event) => {
+            window.parent.postMessage(
+                "form-cancel",
+                `${window.location.protocol}//${window.location.host}`
+            );
+            event.preventDefault();
+        });
     });
-  });
 }
