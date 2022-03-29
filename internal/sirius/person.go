4211d4805bebc1324a0431ac463fea8e139338ee
Joshua Hawxwell <m@hawx.me>
2022-03-29 10:19:25 +0100

    VEGA-1246 Add form to create a task (#14)

--- internal/sirius/person.go
+++ internal/sirius/person.go
@@ -1,10 +1,6 @@
 package sirius
 
-import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-)
+import "fmt"
 
 type Person struct {
 	Firstname string `json:"firstname"`
@@ -13,25 +9,7 @@ type Person struct {
 
 func (c *Client) Person(ctx Context, id int) (Person, error) {
 	var v Person
+	err := c.get(ctx, fmt.Sprintf("/api/v1/persons/%d", id), &v)
 
-	req, err := c.newRequest(ctx, http.MethodGet, fmt.Sprintf("/api/v1/persons/%d", id), nil)
-	if err != nil {
-		return v, err
-	}
-
-	res, err := c.http.Do(req)
-	if err != nil {
-		return v, err
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != http.StatusOK {
-		return v, newStatusError(res)
-	}
-
-	if err := json.NewDecoder(res.Body).Decode(&v); err != nil {
-		return v, err
-	}
-
-	return v, nil
+	return v, err
 }
---
+++ internal/sirius/teams_test.go
@@ -0,0 +1,105 @@
+package sirius
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/pact-foundation/pact-go/dsl"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTeams(t *testing.T) {
+	pact := newPact()
+	defer pact.Teardown()
+
+	testCases := []struct {
+		name             string
+		setup            func()
+		cookies          []*http.Cookie
+		expectedResponse []Team
+		expectedError    func(int) error
+	}{
+		{
+			name: "OK",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("User exists").
+					UponReceiving("A request for teams").
+					WithRequest(dsl.Request{
+						Method: http.MethodGet,
+						Path:   dsl.String("/api/v1/teams"),
+						Headers: dsl.MapMatcher{
+							"X-XSRF-TOKEN":        dsl.String("abcde"),
+							"Cookie":              dsl.String("XSRF-TOKEN=abcde; Other=other"),
+							"OPG-Bypass-Membrane": dsl.String("1"),
+						},
+					}).
+					WillRespondWith(dsl.Response{
+						Status:  http.StatusOK,
+						Headers: dsl.MapMatcher{"Content-Type": dsl.String("application/json")},
+						Body: dsl.EachLike(map[string]interface{}{
+							"id":          dsl.Like(123),
+							"displayName": dsl.Like("Cool Team"),
+						}, 1),
+					})
+			},
+			cookies: []*http.Cookie{
+				{Name: "XSRF-TOKEN", Value: "abcde"},
+				{Name: "Other", Value: "other"},
+			},
+			expectedResponse: []Team{
+				{
+					ID:          123,
+					DisplayName: "Cool Team",
+				},
+			},
+		},
+		{
+			name: "Unauthorized",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("User exists").
+					UponReceiving("A request for teams without cookies").
+					WithRequest(dsl.Request{
+						Method: http.MethodGet,
+						Path:   dsl.String("/api/v1/teams"),
+						Headers: dsl.MapMatcher{
+							"OPG-Bypass-Membrane": dsl.String("1"),
+						},
+					}).
+					WillRespondWith(dsl.Response{
+						Status: http.StatusUnauthorized,
+					})
+			},
+			expectedError: func(port int) error {
+				return StatusError{
+					Code:   http.StatusUnauthorized,
+					URL:    fmt.Sprintf("http://localhost:%d/api/v1/teams", port),
+					Method: http.MethodGet,
+				}
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup()
+
+			assert.Nil(t, pact.Verify(func() error {
+				client := NewClient(http.DefaultClient, fmt.Sprintf("http://localhost:%d", pact.Server.Port))
+
+				users, err := client.Teams(getContext(tc.cookies))
+				assert.Equal(t, tc.expectedResponse, users)
+				if tc.expectedError == nil {
+					assert.Nil(t, err)
+				} else {
+					assert.Equal(t, tc.expectedError(pact.Server.Port), err)
+				}
+				return nil
+			}))
+		})
+	}
+}
