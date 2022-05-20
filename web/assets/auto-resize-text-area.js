b1536cc48733e91c68130b894793f0394871ea85
GemTay <gemma.taylor@digital.justice.gov.uk>
2022-05-20 16:00:01 +0100

    restyled js wih prettier

--- web/assets/auto-resize-text-area.js
+++ web/assets/auto-resize-text-area.js
@@ -1,13 +1,17 @@
 const autoResizeTextArea = () => {
-    const txtareas = document.querySelectorAll('[data-module="app-auto-resize"]');
-    for (let i = 0; i < txtareas.length; i++) {
-        txtareas[i].style.height = "auto";
-        txtareas[i].style.height = (txtareas[i].scrollHeight) + 4 + "px";
+  const txtareas = document.querySelectorAll('[data-module="app-auto-resize"]');
+  for (let i = 0; i < txtareas.length; i++) {
+    txtareas[i].style.height = "auto";
+    txtareas[i].style.height = txtareas[i].scrollHeight + 4 + "px";
 
-        txtareas[i].addEventListener("input", function () {
-            txtareas[i].style.height = "auto";
-            txtareas[i].style.height = (txtareas[i].scrollHeight) + 4 + "px";
-        }, false);
-    }
+    txtareas[i].addEventListener(
+      "input",
+      function () {
+        txtareas[i].style.height = "auto";
+        txtareas[i].style.height = txtareas[i].scrollHeight + 4 + "px";
+      },
+      false
+    );
+  }
 };
 export default autoResizeTextArea;
