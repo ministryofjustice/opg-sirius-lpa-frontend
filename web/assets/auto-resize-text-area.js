38505bc5fed3ab8b3e013cf18014f60e50a27952
GemTay <gemma.taylor@digital.justice.gov.uk>
2022-05-20 15:37:17 +0100

    added 4px to height to fix issue in firefox where it doesnt include the border in the scroll height

--- web/assets/auto-resize-text-area.js
+++ web/assets/auto-resize-text-area.js
@@ -1,13 +1,12 @@
 const autoResizeTextArea = () => {
-    const txtareas = document.querySelectorAll(".js-auto-resize-textarea");
+    const txtareas = document.querySelectorAll('[data-module="app-auto-resize"]');
     for (let i = 0; i < txtareas.length; i++) {
         txtareas[i].style.height = "auto";
-        txtareas[i].style.height = (txtareas[i].scrollHeight) + "px";
-        txtareas[i].style.overflowY = "hidden";
+        txtareas[i].style.height = (txtareas[i].scrollHeight) + 4 + "px";
 
         txtareas[i].addEventListener("input", function () {
             txtareas[i].style.height = "auto";
-            txtareas[i].style.height = (txtareas[i].scrollHeight) + "px";
+            txtareas[i].style.height = (txtareas[i].scrollHeight) + 4 + "px";
         }, false);
     }
 };
