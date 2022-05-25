a06df764ac37a279f30a641f2248b4e8d92bb469
Joshua Hawxwell <m@hawx.me>
2022-05-25 09:44:17 +0100

    Add create relationship form

---
+++ internal/sirius/search_donors_test.go
@@ -0,0 +1,130 @@
+package sirius
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/pact-foundation/pact-go/dsl"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSearchDonors(t *testing.T) {
+	pact := newPact()
+	defer pact.Teardown()
+
+	testCases := []struct {
+		name             string
+		setup            func()
+		cookies          []*http.Cookie
+		expectedResponse []Person
+		expectedError    func(int) error
+	}{
+		{
+			name: "OK",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("A donor exists to be referenced").
+					UponReceiving("A search for donors").
+					WithRequest(dsl.Request{
+						Method: http.MethodPost,
+						Path:   dsl.String("/api/v1/search/persons"),
+						Body: dsl.Like(map[string]interface{}{
+							"term":        "7000-9999-0001",
+							"personTypes": []string{"Donor"},
+						}),
+						Headers: dsl.MapMatcher{
+							"X-XSRF-TOKEN":        dsl.String("abcde"),
+							"Cookie":              dsl.String("XSRF-TOKEN=abcde; Other=other"),
+							"OPG-Bypass-Membrane": dsl.String("1"),
+						},
+					}).
+					WillRespondWith(dsl.Response{
+						Status:  http.StatusOK,
+						Headers: dsl.MapMatcher{"Content-Type": dsl.String("application/json")},
+						Body: dsl.Like(map[string]interface{}{
+							"results": dsl.EachLike(map[string]interface{}{
+								"id":         dsl.Like(47),
+								"uid":        dsl.Like("7000-0000-0003"),
+								"salutation": dsl.Like("Mr"),
+								"firstname":  dsl.Like("John"),
+								"surname":    dsl.Like("Doe"),
+							}, 1),
+						}),
+					})
+			},
+			cookies: []*http.Cookie{
+				{Name: "XSRF-TOKEN", Value: "abcde"},
+				{Name: "Other", Value: "other"},
+			},
+			expectedResponse: []Person{
+				{
+					ID:         47,
+					UID:        "7000-0000-0003",
+					Salutation: "Mr",
+					Firstname:  "John",
+					Surname:    "Doe",
+				},
+			},
+		},
+
+		{
+			name: "Unauthorized",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("A donor exists to be referenced").
+					UponReceiving("A search for donors without cookies").
+					WithRequest(dsl.Request{
+						Method: http.MethodPost,
+						Path:   dsl.String("/api/v1/search/persons"),
+						Body: dsl.Like(map[string]interface{}{
+							"term":        "7000-9999-0001",
+							"personTypes": []string{"Donor"},
+						}),
+						Headers: dsl.MapMatcher{
+							"OPG-Bypass-Membrane": dsl.String("1"),
+						},
+					}).
+					WillRespondWith(dsl.Response{
+						Status: http.StatusUnauthorized,
+					})
+			},
+			expectedError: func(port int) error {
+				return StatusError{
+					Code:   http.StatusUnauthorized,
+					URL:    fmt.Sprintf("http://localhost:%d/api/v1/search/persons", port),
+					Method: http.MethodPost,
+				}
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup()
+
+			assert.Nil(t, pact.Verify(func() error {
+				client := NewClient(http.DefaultClient, fmt.Sprintf("http://localhost:%d", pact.Server.Port))
+
+				donors, err := client.SearchDonors(getContext(tc.cookies), "7000-9999-0001")
+				assert.Equal(t, tc.expectedResponse, donors)
+				if tc.expectedError == nil {
+					assert.Nil(t, err)
+				} else {
+					assert.Equal(t, tc.expectedError(pact.Server.Port), err)
+				}
+				return nil
+			}))
+		})
+	}
+}
+
+func TestSearchDonorsTooShort(t *testing.T) {
+	client := NewClient(http.DefaultClient, "")
+
+	donors, err := client.SearchDonors(getContext(nil), "ad")
+	assert.Nil(t, donors)
+	assert.Equal(t, fmt.Errorf("Search term must be at least three characters"), err)
+}
