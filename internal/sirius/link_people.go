6d9e4e97f14b4d5b45ff935bd63cb5362a41482e
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2022-06-09 14:56:20 +0100

    Add form to link two person records
    Users search for the UID of the person they want to link, check the details, select a primary record (if possible) and save.
    
    Fixes VEGA-1251 #minor

---
+++ internal/sirius/link_people.go
@@ -0,0 +1,53 @@
+package sirius
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+func (c *Client) LinkPeople(ctx Context, parentId int, childId int) error {
+	postData, err := json.Marshal(struct {
+		ParentId int `json:"parentId"`
+		ChildId  int `json:"childId"`
+	}{
+		ParentId: parentId,
+		ChildId:  childId,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	req, err := c.newRequest(
+		ctx,
+		http.MethodPost,
+		"/api/v1/person-links",
+		bytes.NewReader(postData),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	res, err := c.http.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusBadRequest {
+		var v ValidationError
+		if err := json.NewDecoder(res.Body).Decode(&v); err != nil {
+			return err
+		}
+		return v
+	}
+
+	if res.StatusCode != http.StatusCreated {
+		return newStatusError(res)
+	}
+
+	return nil
+}
