1e2957baa1f686007ddf55bb4d14763e1759da27
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2022-06-09 14:07:17 +0100

    Add form to link two person records
    Users search for the UID of the person they want to link, check the details, select a primary record (if possible) and save.
    
    Fixes VEGA-1251 #minor

--- internal/sirius/person_by_uid_test.go
+++ internal/sirius/person_by_uid_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPerson(t *testing.T) {
+func TestPersonByUid(t *testing.T) {
 	pact := newPact()
 	defer pact.Teardown()
 
@@ -26,10 +26,10 @@ func TestPerson(t *testing.T) {
 				pact.
 					AddInteraction().
 					Given("A donor exists").
-					UponReceiving("A request for the person").
+					UponReceiving("A request for the person by UID").
 					WithRequest(dsl.Request{
 						Method: http.MethodGet,
-						Path:   dsl.String("/api/v1/persons/189"),
+						Path:   dsl.String("/api/v1/persons/by-uid/7000-0000-0001"),
 						Headers: dsl.MapMatcher{
 							"X-XSRF-TOKEN":        dsl.String("abcde"),
 							"Cookie":              dsl.String("XSRF-TOKEN=abcde; Other=other"),
@@ -52,7 +52,61 @@ func TestPerson(t *testing.T) {
 				{Name: "XSRF-TOKEN", Value: "abcde"},
 				{Name: "Other", Value: "other"},
 			},
-			expectedResponse: Person{ID: 103, UID: "7000-0000-0001", Firstname: "John", Surname: "Doe", DateOfBirth: DateString("1970-01-01")},
+			expectedResponse: Person{
+				ID:          103,
+				UID:         "7000-0000-0001",
+				Firstname:   "John",
+				Surname:     "Doe",
+				DateOfBirth: DateString("1970-01-01"),
+			},
+		},
+		{
+			name: "OK with children",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("A donor exists with children").
+					UponReceiving("A request for the person by UID").
+					WithRequest(dsl.Request{
+						Method: http.MethodGet,
+						Path:   dsl.String("/api/v1/persons/by-uid/7000-0000-0001"),
+						Headers: dsl.MapMatcher{
+							"X-XSRF-TOKEN":        dsl.String("abcde"),
+							"Cookie":              dsl.String("XSRF-TOKEN=abcde; Other=other"),
+							"OPG-Bypass-Membrane": dsl.String("1"),
+						},
+					}).
+					WillRespondWith(dsl.Response{
+						Status: http.StatusOK,
+						Body: dsl.Like(map[string]interface{}{
+							"id":         dsl.Like(103),
+							"uid":        dsl.Term("7000-0000-0001", `7\d{3}-\d{4}-\d{4}`),
+							"firstname":  dsl.String("John"),
+							"surname":    dsl.String("Doe"),
+							"dob":        dsl.Term("01/01/1970", `^\d{1,2}/\d{1,2}/\d{4}$`),
+							"children": dsl.EachLike(map[string]interface{}{
+								"id": dsl.Like(104),
+							}, 1),
+						}),
+						Headers: dsl.MapMatcher{"Content-Type": dsl.String("application/json")},
+					})
+			},
+			cookies: []*http.Cookie{
+				{Name: "XSRF-TOKEN", Value: "abcde"},
+				{Name: "Other", Value: "other"},
+			},
+			expectedResponse: Person{
+				ID:          103,
+				UID:         "7000-0000-0001",
+				Firstname:   "John",
+				Surname:     "Doe",
+				DateOfBirth: DateString("1970-01-01"),
+				Children: []Person{
+					{
+						ID: 104,
+					},
+				},
+			},
 		},
 		{
 			name: "Unauthorized",
@@ -60,10 +114,10 @@ func TestPerson(t *testing.T) {
 				pact.
 					AddInteraction().
 					Given("A donor exists").
-					UponReceiving("A request for the person without cookies").
+					UponReceiving("A request for the person by UID without cookies").
 					WithRequest(dsl.Request{
 						Method: http.MethodGet,
-						Path:   dsl.String("/api/v1/persons/189"),
+						Path:   dsl.String("/api/v1/persons/by-uid/7000-0000-0001"),
 					}).
 					WillRespondWith(dsl.Response{
 						Status: http.StatusUnauthorized,
@@ -72,7 +126,7 @@ func TestPerson(t *testing.T) {
 			expectedError: func(port int) error {
 				return StatusError{
 					Code:   http.StatusUnauthorized,
-					URL:    fmt.Sprintf("http://localhost:%d/api/v1/persons/189", port),
+					URL:    fmt.Sprintf("http://localhost:%d/api/v1/persons/by-uid/7000-0000-0001", port),
 					Method: http.MethodGet,
 				}
 			},
@@ -86,7 +140,7 @@ func TestPerson(t *testing.T) {
 			assert.Nil(t, pact.Verify(func() error {
 				client := NewClient(http.DefaultClient, fmt.Sprintf("http://localhost:%d", pact.Server.Port))
 
-				caseitem, err := client.Person(getContext(tc.cookies), 189)
+				caseitem, err := client.PersonByUid(getContext(tc.cookies), "7000-0000-0001")
 
 				assert.Equal(t, tc.expectedResponse, caseitem)
 				if tc.expectedError == nil {
