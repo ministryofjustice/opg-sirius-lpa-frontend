f9b4bb3447331c0bca904c0ec839d91dcb5582fd
Joshua Hawxwell <m@hawx.me>
2022-07-04 11:10:00 +0100

    VEGA-1248 Add allocate cases form (#76)

--- cypress/integration/edit-complaint.spec.js
+++ cypress/integration/edit-complaint.spec.js
@@ -1,24 +1,30 @@
 describe("Edit complaint", () => {
-    beforeEach(() => {
-        cy.setCookie("Other", "other");
-        cy.setCookie("XSRF-TOKEN", "abcde");
-        cy.setCookie("OPG-Bypass-Membrane", "1");
-        cy.visit("/edit-complaint?id=986");
-    });
+  beforeEach(() => {
+    cy.setCookie("Other", "other");
+    cy.setCookie("XSRF-TOKEN", "abcde");
+    cy.setCookie("OPG-Bypass-Membrane", "1");
+    cy.visit("/edit-complaint?id=986");
+  });
 
-    it("edits a complaint", () => {
-        cy.contains("Edit Complaint");
-        cy.get(".moj-banner").should("not.exist");
-        cy.contains(".govuk-radios__label", "Major").parent().get("input").should("be.checked");
-        cy.get("#f-summary").should("have.value", "This and that");
-        cy.get("#f-description").should("have.value", "This is seriously bad");
-        cy.get("#f-receivedDate").should("have.value", "2022-04-05");
-        cy.contains(".govuk-radios__label", "Correspondence").parent().get("input").should("be.checked");
-        cy.get("#f-subCategory-01").should("have.value", "07");
-        cy.get("#f-resolutionDate").type("2022-05-06");
-        cy.contains(".govuk-radios__label", "Complaint Not Upheld").click();
-        cy.get("#f-resolutionInfo").type("Because...");
-        cy.get("button[type=submit]").click();
-        cy.get(".moj-banner").should("exist");
-    });
+  it("edits a complaint", () => {
+    cy.contains("Edit Complaint");
+    cy.get(".moj-banner").should("not.exist");
+    cy.contains(".govuk-radios__label", "Major")
+      .parent()
+      .get("input")
+      .should("be.checked");
+    cy.get("#f-summary").should("have.value", "This and that");
+    cy.get("#f-description").should("have.value", "This is seriously bad");
+    cy.get("#f-receivedDate").should("have.value", "2022-04-05");
+    cy.contains(".govuk-radios__label", "Correspondence")
+      .parent()
+      .get("input")
+      .should("be.checked");
+    cy.get("#f-subCategory-01").should("have.value", "07");
+    cy.get("#f-resolutionDate").type("2022-05-06");
+    cy.contains(".govuk-radios__label", "Complaint Not Upheld").click();
+    cy.get("#f-resolutionInfo").type("Because...");
+    cy.get("button[type=submit]").click();
+    cy.get(".moj-banner").should("exist");
+  });
 });
---
+++ internal/sirius/allocate_cases_test.go
@@ -0,0 +1,143 @@
+package sirius
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/pact-foundation/pact-go/dsl"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAllocateCases(t *testing.T) {
+	t.Parallel()
+
+	pact := newPact()
+	defer pact.Teardown()
+
+	testCases := []struct {
+		name          string
+		setup         func()
+		cookies       []*http.Cookie
+		allocations   []CaseAllocation
+		expectedError func(int) error
+		file          *NoteFile
+	}{
+		{
+			name: "OK",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("I have a pending case assigned").
+					UponReceiving("A request to change the assignee of the case").
+					WithRequest(dsl.Request{
+						Method: http.MethodPut,
+						Path:   dsl.String("/api/v1/users/47/cases/800"),
+						Body: map[string]interface{}{
+							"data": []map[string]interface{}{{
+								"id":       800,
+								"caseType": "LPA",
+							}},
+						},
+						Headers: dsl.MapMatcher{
+							"X-XSRF-TOKEN":        dsl.String("abcde"),
+							"Cookie":              dsl.String("XSRF-TOKEN=abcde; Other=other"),
+							"OPG-Bypass-Membrane": dsl.String("1"),
+							"Content-Type":        dsl.String("application/json"),
+						},
+					}).
+					WillRespondWith(dsl.Response{
+						Status:  http.StatusOK,
+						Headers: dsl.MapMatcher{"Content-Type": dsl.String("application/json")},
+					})
+			},
+			cookies: []*http.Cookie{
+				{Name: "XSRF-TOKEN", Value: "abcde"},
+				{Name: "Other", Value: "other"},
+			},
+			allocations: []CaseAllocation{{ID: 800, CaseType: "LPA"}},
+		},
+		{
+			name: "OK multiple",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("Multiple cases exist").
+					UponReceiving("A request to change the assignee of multiple cases").
+					WithRequest(dsl.Request{
+						Method: http.MethodPut,
+						Path:   dsl.String("/api/v1/users/47/cases/800+801+802"),
+						Body: map[string]interface{}{
+							"data": []map[string]interface{}{{
+								"id":       800,
+								"caseType": "LPA",
+							}, {
+								"id":       801,
+								"caseType": "LPA",
+							}, {
+								"id":       802,
+								"caseType": "EPA",
+							}},
+						},
+						Headers: dsl.MapMatcher{
+							"X-XSRF-TOKEN":        dsl.String("abcde"),
+							"Cookie":              dsl.String("XSRF-TOKEN=abcde; Other=other"),
+							"OPG-Bypass-Membrane": dsl.String("1"),
+							"Content-Type":        dsl.String("application/json"),
+						},
+					}).
+					WillRespondWith(dsl.Response{
+						Status:  http.StatusOK,
+						Headers: dsl.MapMatcher{"Content-Type": dsl.String("application/json")},
+					})
+			},
+			cookies: []*http.Cookie{
+				{Name: "XSRF-TOKEN", Value: "abcde"},
+				{Name: "Other", Value: "other"},
+			},
+			allocations: []CaseAllocation{{ID: 800, CaseType: "LPA"}, {ID: 801, CaseType: "LPA"}, {ID: 802, CaseType: "EPA"}},
+		},
+		{
+			name: "Unauthorized",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("I have a pending case assigned").
+					UponReceiving("A request to change the assignee of the case without cookies").
+					WithRequest(dsl.Request{
+						Method: http.MethodPut,
+						Path:   dsl.String("/api/v1/users/47/cases/800"),
+					}).
+					WillRespondWith(dsl.Response{
+						Status: http.StatusUnauthorized,
+					})
+			},
+			allocations: []CaseAllocation{{ID: 800, CaseType: "LPA"}},
+			expectedError: func(port int) error {
+				return StatusError{
+					Code:   http.StatusUnauthorized,
+					URL:    fmt.Sprintf("http://localhost:%d/api/v1/users/47/cases/800", port),
+					Method: http.MethodPut,
+				}
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup()
+
+			assert.Nil(t, pact.Verify(func() error {
+				client := NewClient(http.DefaultClient, fmt.Sprintf("http://localhost:%d", pact.Server.Port))
+
+				err := client.AllocateCases(getContext(tc.cookies), 47, tc.allocations)
+				if (tc.expectedError) == nil {
+					assert.Nil(t, err)
+				} else {
+					assert.Equal(t, tc.expectedError(pact.Server.Port), err)
+				}
+				return nil
+			}))
+		})
+	}
+}
