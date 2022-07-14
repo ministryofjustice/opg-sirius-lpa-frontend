d5545853153856133f41eea24da31da93e15ccb6
Gemma Taylor <gemma.taylor@digital.justice.gov.uk>
2022-07-14 08:52:03 +0100

    VEGA-1252 Fix unlink person pact tests (#77)
    * uncommented unlink person pact test
    
    * unlink person request now a patch. Fixed up pact tests

--- internal/sirius/unlink_person_test.go
+++ internal/sirius/unlink_person_test.go
@@ -2,10 +2,10 @@ package sirius
 
 import (
 	"fmt"
+	"github.com/pact-foundation/pact-go/dsl"
+	"github.com/stretchr/testify/assert"
 	"net/http"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestUnlinkPerson(t *testing.T) {
@@ -20,61 +20,61 @@ func TestUnlinkPerson(t *testing.T) {
 		cookies       []*http.Cookie
 		expectedError func(int) error
 	}{
-		// {
-		// 	name: "OK",
-		// 	setup: func() {
-		// 		pact.
-		// 			AddInteraction().
-		// 			Given("A donor exists with children").
-		// 			UponReceiving("A request to unlink those cases").
-		// 			WithRequest(dsl.Request{
-		// 				Method: http.MethodDelete,
-		// 				Path:   dsl.String("/api/v1/person-links/189"),
-		// 				Headers: dsl.MapMatcher{
-		// 					"X-XSRF-TOKEN":        dsl.String("abcde"),
-		// 					"Cookie":              dsl.String("XSRF-TOKEN=abcde; Other=other"),
-		// 					"OPG-Bypass-Membrane": dsl.String("1"),
-		// 				},
-		// 				Body: map[string]interface{}{
-		// 					"childIds": dsl.Like([]int{5}),
-		// 				},
-		// 			}).
-		// 			WillRespondWith(dsl.Response{
-		// 				Status:  http.StatusNoContent,
-		// 				Headers: dsl.MapMatcher{"Content-Type": dsl.String("application/json")},
-		// 			})
-		// 	},
-		// 	cookies: []*http.Cookie{
-		// 		{Name: "XSRF-TOKEN", Value: "abcde"},
-		// 		{Name: "Other", Value: "other"},
-		// 	},
-		// },
-		// {
-		// 	name: "Unauthorized",
-		// 	setup: func() {
-		// 		pact.
-		// 			AddInteraction().
-		// 			Given("I have a case linked with another").
-		// 			UponReceiving("A request to unlink those cases without cookies").
-		// 			WithRequest(dsl.Request{
-		// 				Method: http.MethodDelete,
-		// 				Path:   dsl.String("/api/v1/person-links/189"),
-		// 				Body: map[string]interface{}{
-		// 					"childIds": dsl.Like([]int{5}),
-		// 				},
-		// 			}).
-		// 			WillRespondWith(dsl.Response{
-		// 				Status: http.StatusUnauthorized,
-		// 			})
-		// 	},
-		// 	expectedError: func(port int) error {
-		// 		return StatusError{
-		// 			Code:   http.StatusUnauthorized,
-		// 			URL:    fmt.Sprintf("http://localhost:%d/api/v1/person-links/189", port),
-		// 			Method: http.MethodDelete,
-		// 		}
-		// 	},
-		// },
+		{
+			name: "OK",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("A donor exists with children").
+					UponReceiving("A request to unlink those cases").
+					WithRequest(dsl.Request{
+						Method: http.MethodPatch,
+						Path:   dsl.String("/api/v1/person-links/189"),
+						Body: map[string]interface{}{
+							"childIds": []int{105},
+						},
+						Headers: dsl.MapMatcher{
+							"X-XSRF-TOKEN":        dsl.String("abcde"),
+							"Cookie":              dsl.String("XSRF-TOKEN=abcde; Other=other"),
+							"OPG-Bypass-Membrane": dsl.String("1"),
+							"Content-Type":        dsl.String("application/json"),
+						},
+					}).
+					WillRespondWith(dsl.Response{
+						Status: http.StatusNoContent,
+					})
+			},
+			cookies: []*http.Cookie{
+				{Name: "XSRF-TOKEN", Value: "abcde"},
+				{Name: "Other", Value: "other"},
+			},
+		},
+		{
+			name: "Unauthorized",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("A donor exists with children").
+					UponReceiving("A request to unlink those cases without cookies").
+					WithRequest(dsl.Request{
+						Method: http.MethodPatch,
+						Path:   dsl.String("/api/v1/person-links/189"),
+						Body: map[string]interface{}{
+							"childIds": []int{105},
+						},
+					}).
+					WillRespondWith(dsl.Response{
+						Status: http.StatusUnauthorized,
+					})
+			},
+			expectedError: func(port int) error {
+				return StatusError{
+					Code:   http.StatusUnauthorized,
+					URL:    fmt.Sprintf("http://localhost:%d/api/v1/person-links/189", port),
+					Method: http.MethodPatch,
+				}
+			},
+		},
 	}
 
 	for _, tc := range testCases {
@@ -84,7 +84,7 @@ func TestUnlinkPerson(t *testing.T) {
 			assert.Nil(t, pact.Verify(func() error {
 				client := NewClient(http.DefaultClient, fmt.Sprintf("http://localhost:%d", pact.Server.Port))
 
-				err := client.UnlinkPerson(getContext(tc.cookies), 189, 5)
+				err := client.UnlinkPerson(getContext(tc.cookies), 189, 105)
 
 				if tc.expectedError == nil {
 					assert.Nil(t, err)
