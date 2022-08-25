d52ae509ca1d389bc747e732ff2b63552c0ef6ec
GemTay <gemma.taylor@digital.justice.gov.uk>
2022-08-25 17:32:16 +0100

    remove unused func

--- internal/sirius/payment.go
+++ internal/sirius/payment.go
@@ -23,7 +23,3 @@ func (c *Client) Payments(ctx Context, id int) ([]Payment, error) {
 func PoundsToPence(pounds float64) int {
 	return int(pounds * 100)
 }
-
-func PenceToPounds(pence int) float64 {
-	return float64(pence / 100)
-}
