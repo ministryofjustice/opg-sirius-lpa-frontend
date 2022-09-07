b283bc08b5590fb8b86d9cb4560da1ccae347e86
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2022-09-07 08:47:22 +0100

    VEGA-1446 remove cookies from cypress tests

--- cypress/e2e/add-complaint.cy.js
+++ cypress/e2e/add-complaint.cy.js
@@ -1,8 +1,5 @@
 describe("Add a complaint", () => {
   beforeEach(() => {
-    cy.setCookie("Other", "other");
-    cy.setCookie("XSRF-TOKEN", "abcde");
-    cy.setCookie("OPG-Bypass-Membrane", "1");
     cy.visit("/add-complaint?id=800&case=lpa");
   });
 
--- cypress/e2e/create-warning.cy.js
+++ cypress/e2e/create-warning.cy.js
@@ -1,8 +1,5 @@
 describe("Create a warning", () => {
   beforeEach(() => {
-    cy.setCookie("Other", "other");
-    cy.setCookie("XSRF-TOKEN", "abcde");
-    cy.setCookie("OPG-Bypass-Membrane", "1");
     cy.visit("/create-warning?id=189");
   });
 
