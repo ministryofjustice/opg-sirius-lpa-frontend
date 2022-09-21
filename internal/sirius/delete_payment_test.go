26594da42562ee554b2328d7af5b9f7c84facd92
Gemma Taylor <gemma.taylor@digital.justice.gov.uk>
2022-09-21 15:51:20 +0100

    VEGA-1476 Allow payments to be deleted (#139)
    * payments can now be deleted. VEGA-1476 #minor
    
    * coloured delete payment link red
    
    * edit and delete payment links now span across both columns. Changed naming convention of red text class

---
+++ internal/sirius/delete_payment_test.go
@@ -0,0 +1,59 @@
+package sirius
+
+import (
+	"context"
+	"fmt"
+	"github.com/pact-foundation/pact-go/dsl"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestDeletePayment(t *testing.T) {
+	t.Parallel()
+
+	pact := newPact()
+	defer pact.Teardown()
+
+	testCases := []struct {
+		name          string
+		setup         func()
+		expectedError func(int) error
+	}{
+		{
+			name: "OK",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("I have an lpa which has been paid for").
+					UponReceiving("A request to delete a payment").
+					WithRequest(dsl.Request{
+						Method: http.MethodDelete,
+						Path:   dsl.String("/lpa-api/v1/payments/123"),
+					}).
+					WillRespondWith(dsl.Response{
+						Status: http.StatusNoContent,
+					})
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup()
+
+			assert.Nil(t, pact.Verify(func() error {
+				client := NewClient(http.DefaultClient, fmt.Sprintf("http://localhost:%d", pact.Server.Port))
+
+				err := client.DeletePayment(Context{Context: context.Background()}, 123)
+
+				if tc.expectedError == nil {
+					assert.Nil(t, err)
+				} else {
+					assert.Equal(t, tc.expectedError(pact.Server.Port), err)
+				}
+				return nil
+			}))
+		})
+	}
+}
