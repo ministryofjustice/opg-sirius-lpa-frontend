05b4d04641eb0b335f3c2520c4aeb90361bdd57f
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2022-09-27 15:50:32 +0100

    VEGA-1469 set pre wrap styling to display new lines

--- cypress/e2e/payments.cy.js
+++ cypress/e2e/payments.cy.js
@@ -33,7 +33,7 @@ describe("View a payment", () => {
             cy.contains("Date reduction approved:");
             cy.contains("24/01/2022");
             cy.contains("Evidence:");
-            cy.contains("Test evidence");
+            cy.contains("Test multiple line evidence");
             cy.get(".govuk-link").contains("Edit fee reduction");
             cy.get(".govuk-link").contains("Delete fee reduction");
         });
