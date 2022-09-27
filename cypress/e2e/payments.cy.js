af9f2bc2bd722cf50a467aa20da1a39ec803d1f3
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2022-09-27 14:35:26 +0100

    VEGA-1469 display fee reduction details

--- cypress/e2e/payments.cy.js
+++ cypress/e2e/payments.cy.js
@@ -33,7 +33,7 @@ describe("View a payment", () => {
             cy.contains("Date reduction approved:");
             cy.contains("24/01/2022");
             cy.contains("Evidence:");
-            cy.contains("Test multiple line evidence");
+
             cy.get(".govuk-link").contains("Edit fee reduction");
             cy.get(".govuk-link").contains("Delete fee reduction");
         });
@@ -67,6 +67,7 @@ describe("View a payment", () => {
             cy.contains("24/01/2022");
             cy.contains("Evidence:");
             cy.contains("Test multiple line evidence");
+            cy.contains("Test evidence");
             cy.get(".govuk-link").contains("Edit fee reduction");
             cy.get(".govuk-link").contains("Delete fee reduction");
         });
