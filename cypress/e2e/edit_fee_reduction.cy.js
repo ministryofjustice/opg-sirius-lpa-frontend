f45e0ced3b170fc06e755ef06831452d58207c5c
GemTay <gemma.taylor@digital.justice.gov.uk>
2022-09-30 15:25:18 +0100

    users can now edit a fee reduction. Uses the same edit payment endpoint and sirius side change will determine if it is a payment or fee reduction

---
+++ cypress/e2e/edit_fee_reduction.cy.js
@@ -0,0 +1,16 @@
+describe("Edit a fee reduction", () => {
+    beforeEach(() => {
+        cy.visit("/edit-fee-reduction?id=123");
+    });
+
+    it("edits an existing fee reduction", () => {
+        cy.contains("Edit fee reduction");
+        cy.contains("7000-0000-0000");
+        cy.get(".moj-banner").should("not.exist");
+        cy.get("#f-feeReductionType" ).select("Remission");
+        cy.get("#f-paymentEvidence").type("Edited test evidence");
+        cy.get("#f-paymentDate").type("2022-03-26");
+        cy.get("button[type=submit]").click();
+        cy.get(".moj-banner").should("exist");
+    });
+});
