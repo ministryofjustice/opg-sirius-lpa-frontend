59611931e32a30d9dca0d85c53284808217d1739
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2022-10-03 15:57:44 +0100

    VEGA-1464 amend case uid

--- cypress/e2e/delete-fee-reduction.cy.js
+++ cypress/e2e/delete-fee-reduction.cy.js
@@ -5,7 +5,7 @@ describe("Delete a fee reduction", () => {
 
     it("deletes a fee reduction on a case", () => {
         cy.contains("Delete remission");
-        cy.contains("7000-0000-0000");
+        cy.contains("7000-0000-0002");
         cy.get(".moj-banner").should("not.exist");
         cy.get("button[type=submit]").click();
         cy.get(".moj-banner").should("exist");
