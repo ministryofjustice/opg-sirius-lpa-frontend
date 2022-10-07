53a9239a6eb4b886b81bab5e74ead55e5594f315
Greg Tyler <gregtyler@users.noreply.github.com>
2022-10-07 10:44:35 +0100

    Prevent users from rerunning report (#159)
    Because they're not given immediate feedback, we believe some users are clicking on the "Download" button multiple times and re-running the report.
    
    This change disables the button after the first time it's pressed, and adds a notification banner to explain that it will open in a new window when ready.
    
    Fixes VEGA-1506 #minor

--- cypress/e2e/mi-reporting.cy.js
+++ cypress/e2e/mi-reporting.cy.js
@@ -13,5 +13,17 @@ describe("MI Reporting", () => {
 
     cy.contains("Number of EPAs received");
     cy.contains("10");
+
+    // Make the link open in a new tab because otherwise Cypress
+    // throws cross-domain errors
+    cy.contains("a", "Download").then(($input) => {
+      $input[0].setAttribute("target", "_blank");
+    });
+
+    cy.contains("a", "Download").click();
+    cy.contains("a", "Download")
+      .invoke("attr", "class")
+      .should("contain", "govuk-button--disabled");
+    cy.contains("Your download will open in a new window when ready");
   });
 });
