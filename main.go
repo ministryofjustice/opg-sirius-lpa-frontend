30548ba0a892b6274222c89c0aaa34349a2d196e
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2022-11-03 12:35:47 +0000

    Trace requests and send to XRay
    Mostly use OpenTelemetry to trace the requests but tie them into AWS infra by:
    - Detecting the ECS resource
    - Generating XRay-compliant IDs
    - Using the XRay propagator
    
    Fixes VEGA-1538 #minor

--- main.go
+++ main.go
@@ -14,8 +14,48 @@ import (
 	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/server"
 	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/sirius"
 	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/templatefn"
+	"go.opentelemetry.io/contrib/detectors/aws/ecs"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/contrib/propagators/aws/xray"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/trace"
 	"golang.org/x/mod/sumdb/dirhash"
+	"google.golang.org/grpc"
 )
+
+func initTracerProvider(ctx context.Context, logger *logging.Logger) func() {
+	resource, err := ecs.NewResourceDetector().Detect(ctx)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	traceExporter, err := otlptracegrpc.New(ctx,
+		otlptracegrpc.WithInsecure(),
+		otlptracegrpc.WithEndpoint("0.0.0.0:4317"),
+		otlptracegrpc.WithDialOption(grpc.WithBlock()),
+	)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	idg := xray.NewIDGenerator()
+	tp := trace.NewTracerProvider(
+		trace.WithResource(resource),
+		trace.WithSampler(trace.AlwaysSample()),
+		trace.WithBatcher(traceExporter),
+		trace.WithIDGenerator(idg),
+	)
+
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(xray.Propagator{})
+
+	return func() {
+		if err := tp.Shutdown(ctx); err != nil {
+			logger.Fatal(err)
+		}
+	}
+}
 
 func main() {
 	logger := logging.New(os.Stdout, "opg-sirius-lpa-frontend")
@@ -36,7 +76,15 @@ func main() {
 		logger.Fatal(err)
 	}
 
-	client := sirius.NewClient(http.DefaultClient, siriusURL)
+	if env.Get("TRACING_ENABLED", "0") == "1" {
+		shutdown := initTracerProvider(context.Background(), logger)
+		defer shutdown()
+	}
+
+	httpClient := http.DefaultClient
+	httpClient.Transport = otelhttp.NewTransport(httpClient.Transport)
+
+	client := sirius.NewClient(httpClient, siriusURL)
 
 	server := &http.Server{
 		Addr:              ":" + port,
