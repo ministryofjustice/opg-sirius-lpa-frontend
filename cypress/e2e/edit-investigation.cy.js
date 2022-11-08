12ff9831a591f7efbec775a33eebf35e9ea69167
hibo.abdilaahi <51047911+hiboabd@users.noreply.github.com>
2022-11-08 15:06:44 +0000

    VEGA-1442 recreate edit investigation form and add tests

---
+++ cypress/e2e/edit-investigation.cy.js
@@ -0,0 +1,22 @@
+describe("Edit investigation", () => {
+  beforeEach(() => {
+    cy.visit("/edit-investigation?id=300");
+  });
+
+  it("edits a investigation", () => {
+    cy.contains("Edit Investigation");
+    cy.get(".moj-banner").should("not.exist");
+    cy.get("#f-title").should("have.value", "Test title");
+    cy.get("#f-information").should("have.value", "Some test info");
+    cy.contains(".govuk-radios__label", "Normal")
+        .parent()
+        .get("input")
+        .should("be.checked");
+    cy.get("#f-dateReceived").should("have.value", "2022-01-23");
+    cy.get("#f-approvalDate").type("2022-03-04");
+    cy.get("#f-approvalOutcome").select("Court Application");
+    cy.get("#f-investigationClosureDate").type("2022-03-04");
+    cy.get("button[type=submit]").click();
+    cy.get(".moj-banner").should("exist");
+  });
+});
---
+++ internal/sirius/edit_investigation_test.go
@@ -0,0 +1,80 @@
+package sirius
+
+import (
+	"context"
+	"fmt"
+	"github.com/pact-foundation/pact-go/dsl"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestEditInvestigation(t *testing.T) {
+	t.Parallel()
+
+	pact := newPact()
+	defer pact.Teardown()
+
+	testCases := []struct {
+		name          string
+		setup         func()
+		expectedError func(int) error
+	}{
+		{
+			name: "OK",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("I have a case assigned which has an investigation open").
+					UponReceiving("A request to edit the investigation").
+					WithRequest(dsl.Request{
+						Method: http.MethodPut,
+						Path:   dsl.String("/lpa-api/v1/investigations/300"),
+						Headers: dsl.MapMatcher{
+							"Content-Type": dsl.String("application/json"),
+						},
+						Body: dsl.Like(map[string]interface{}{
+							"investigationTitle":        "Test title",
+							"additionalInformation":     "Some test info",
+							"type":                      "Normal",
+							"investigationReceivedDate": "23/01/2022",
+							"approvalDate":              "05/04/2022",
+							"approvalOutcome":           "Court Application",
+							"investigationClosureDate":  "05/04/2022",
+						}),
+					}).
+					WillRespondWith(dsl.Response{
+						Status:  http.StatusOK,
+						Headers: dsl.MapMatcher{"Content-Type": dsl.String("application/json")},
+					})
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup()
+
+			assert.Nil(t, pact.Verify(func() error {
+				client := NewClient(http.DefaultClient, fmt.Sprintf("http://localhost:%d", pact.Server.Port))
+
+				err := client.EditInvestigation(Context{Context: context.Background()}, 300, Investigation{
+					Title:                    "Test title",
+					Information:              "Some test info",
+					Type:                     "Normal",
+					DateReceived:             DateString("2022-01-23"),
+					ApprovalDate:             DateString("2022-04-05"),
+					ApprovalOutcome:          "Court Application",
+					InvestigationClosureDate: DateString("2022-04-05"),
+				})
+
+				if tc.expectedError == nil {
+					assert.Nil(t, err)
+				} else {
+					assert.Equal(t, tc.expectedError(pact.Server.Port), err)
+				}
+				return nil
+			}))
+		})
+	}
+}
