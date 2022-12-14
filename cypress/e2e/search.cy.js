f367f8f2bf79a05ec249dd207a866bf1e8ddc06d
GemTay <gemma.taylor@digital.justice.gov.uk>
2022-12-14 10:47:43 +0000

    removed unnecessary pact test

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -23,33 +23,9 @@ describe("Search", () => {
         });
     });
 
-    describe("Search with filters", () => {
-        beforeEach(() => {
-            cy.visit("/search?term=jack");
-        });
-
-        it("finds an attorney when filtered", () => {
-            cy.contains("You searched for: jack");
-            cy.contains("Showing 1 to 1 of 1 cases");
-            cy.contains("Donor (1)");
-            const $row = cy.get("table > tbody > tr");
-            $row
-                .contains("7000-8548-8461")
-                .should("have.attr", "href")
-                .should("contain", "/person/36/23");
-            $row.should("contain", "jack smith");
-            $row.should("contain", "123 Somewhere Road");
-            $row.should("contain", "perfect");
-            $row.should("contain", "LPA - PFA");
-        });
-    });
-
     describe("Search donor not associated with case", () => {
-        beforeEach(() => {
-            cy.visit("/search?term=daniel");
-        });
-
         it("finds a donor not associated with a case", () => {
+            cy.visit("/search?term=daniel");
             cy.contains("Showing 1 to 1 of 1 cases");
             const $row = cy.get("table > tbody > tr");
             $row.should("contain", "Not associated with a case");
@@ -62,11 +38,8 @@ describe("Search", () => {
     });
 
     describe("Search deleted case", () => {
-        beforeEach(() => {
-            cy.visit("/search?term=700000005555");
-        });
-
         it("finds a deleted case when searching by uid", () => {
+            cy.visit("/search?term=700000005555");
             cy.contains("Search results");
             const $row = cy.get("table > tbody > tr");
             $row.should("contain", "7000-0000-5555");
