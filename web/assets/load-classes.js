d2e6c66056abf84e95c047556a90acbe42e139e8
Gemma Taylor <gemma.taylor@digital.justice.gov.uk>
2022-12-20 16:14:41 +0000

    VEGA-1613 enable dark mode on non-embedded pages (#238)
    * applied styling to achieve desired dark mode appearance on search results page
    
    * changed id to class with app- prefix

--- web/assets/load-classes.js
+++ web/assets/load-classes.js
@@ -5,11 +5,11 @@ document.body.className = document.body.className
 if (window.self !== window.parent) {
   document.documentElement.className += " app-!-html-class--embedded";
   document.body.className += " app-!-embedded";
+}
 
-  if (
-    document.cookie.indexOf("siriusTheme=dark") > -1 ||
-    document.cookie.indexOf("siriusTheme=accessible-dark") > -1
-  ) {
-    document.documentElement.className += " app-!-html-class--dark";
-  }
+if (
+  document.cookie.indexOf("siriusTheme=dark") > -1 ||
+  document.cookie.indexOf("siriusTheme=accessible-dark") > -1
+) {
+  document.documentElement.className += " app-!-html-class--dark";
 }
