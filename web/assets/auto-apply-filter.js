78a46fbed3e1434f949cb03fd20726f593e5835a
Mabel Li <33980519+mabely@users.noreply.github.com>
2023-05-16 09:42:51 +0100

    Wait before submitting form
    in case the user selects multiple filters, instead of pausing after selections
    
    Co-authored-by: Greg Tyler <gregtyler@users.noreply.github.com>

--- web/assets/auto-apply-filter.js
+++ web/assets/auto-apply-filter.js
@@ -1,9 +1,12 @@
 export default function autoApplyFilter() {
     const filters = document.querySelectorAll("input.govuk-checkboxes__input");
+    let timeout = null;
 
     filters.forEach((filter) => {
         filter.addEventListener('click', () => {
-            setTimeout(() => {
+            if (timeout !== null) clearTimeout(timeout);
+
+            timeout = setTimeout(() => {
                 document.forms["search-filters"].submit();
             }, 500)
         })
