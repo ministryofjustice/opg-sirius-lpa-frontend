5114527fe7687eca76f06fc8263ce87fec7ad495
gemtay <gemma.taylor@digital.justice.gov.uk>
2023-06-28 11:37:14 +0100

    added mocks for create warning cypress tests. VEGA-1774 #minor

--- cypress/e2e/create-warning.cy.js
+++ cypress/e2e/create-warning.cy.js
@@ -1,9 +1,27 @@
 describe("Create a warning", () => {
   beforeEach(() => {
+    cy.addMock("/lpa-api/v1/persons/189", "GET", {
+      status: 200,
+      body: {
+        dob: "05/05/1970",
+        firstname: "John",
+        id: 189,
+        surname: "Doe",
+        uId: "7000-0000-0007"
+      },
+    });
     cy.visit("/create-warning?id=189");
   });
 
   it("creates a warning", () => {
+    cy.addMock("/lpa-api/v1/warnings", "POST", {
+      status: 201,
+      body: {
+        personId: 189,
+        warningText: "Some warning notes",
+        warningType: "Complaint Received"
+      },
+    });
     cy.contains("Create Warning");
     cy.get(".moj-banner").should("not.exist");
     cy.get("#case-id-0").should("not.exist");
@@ -16,10 +34,50 @@ describe("Create a warning", () => {
 
 describe("Create a warning on multiple cases", () => {
   beforeEach(() => {
+    cy.addMock("/lpa-api/v1/persons/400", "GET", {
+      status: 200,
+      body: {
+        dob: "05/05/1970",
+        firstname: "John",
+        id: 400,
+        surname: "Doe",
+        uId: "7000-0000-0007",
+        cases: [
+          {
+            caseSubtype: "pfa",
+            caseType: "LPA",
+            id: 405,
+            status: "Perfect",
+            uId: "7000-5382-4438"
+          },
+          {
+            caseSubtype: "hw",
+            caseType: "LPA",
+            id: 406,
+            status: "Pending",
+            uId: "7000-5382-8764"
+          }
+        ],
+      },
+    });
+
     cy.visit("/create-warning?id=400");
   });
 
   it("creates a warning on multiple cases", () => {
+    cy.addMock("/lpa-api/v1/warnings", "POST", {
+      status: 201,
+      body: {
+        personId: 400,
+        warningText: "Some warning notes for multiple cases",
+        warningType: "Complaint Received",
+        caseIds: [
+          405,
+          406
+        ],
+      },
+    });
+
     cy.contains("Create Warning");
     cy.get(".moj-banner").should("not.exist");
     cy.get("#case-id-0").click();
