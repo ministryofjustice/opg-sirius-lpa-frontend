6976e533c1c25e50061b2a3e2a8f6fbb508069e0
Elliot Smith <elliot.smith@justice.digital.gov.uk>
2023-09-07 12:10:30 +0100

    Fill out unit tests for lpa

--- internal/server/lpa_test.go
+++ internal/server/lpa_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"errors"
 	"net/http"
 	"testing"
 
@@ -20,7 +21,47 @@ func (m *mockLpaClient) DigitalLpa(ctx sirius.Context, uid string) (sirius.Digit
 
 func (m *mockLpaClient) TasksForCase(ctx sirius.Context, id int) ([]sirius.Task, error) {
 	args := m.Called(ctx, id)
+	arg := args.Get(0)
+
+	if arg == nil {
+		return nil, args.Error(1)
+	}
+
 	return args.Get(0).([]sirius.Task), args.Error(1)
+}
+
+func TestGetLpaErrorRetrievingTasksForCase(t *testing.T) {
+	digitalLpa := sirius.DigitalLpa{
+		ID:      88,
+		UID:     "M-AAAA-9876-9876",
+		Subtype: "hw",
+	}
+
+	client := &mockLpaClient{}
+	client.
+		On("DigitalLpa", mock.Anything, "M-AAAA-9876-9876").
+		Return(digitalLpa, nil)
+	client.
+		On("TasksForCase", mock.Anything, 88).
+		Return(nil, errors.New("foo"))
+
+	template := &mockTemplate{}
+	/*
+	template.
+		On("Func", mock.Anything, lpaData{
+			Lpa: digitalLpa,
+			TaskList: taskList,
+		}).
+		Return(nil)
+	*/
+
+	server := newMockServer("/lpa/{uid}", Lpa(client, template.Func))
+
+	req, _ := http.NewRequest(http.MethodGet, "/lpa/M-AAAA-9876-9876", nil)
+	_, err := server.serve(req)
+
+	assert.Equal(t, "foo", err.Error())
+	mock.AssertExpectationsForObjects(t, client, template)
 }
 
 func TestGetLpa(t *testing.T) {
