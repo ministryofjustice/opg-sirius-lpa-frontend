65142fbe06ef4e1aa8b86248b1e38a5ce901edf3
Elliot Smith <elliot.smith@justice.digital.gov.uk>
2023-10-20 17:22:29 +0100

    Add pact test to extend unit test coverage

--- internal/sirius/add_fee_decision_test.go
+++ internal/sirius/add_fee_decision_test.go
@@ -31,16 +31,18 @@ func TestAddFeeDecision(t *testing.T) {
 
 	testCases := []struct {
 		name          string
+		caseId        int
 		setup         func()
 		expectedError func(int) error
 	}{
 		{
 			name: "OK",
+			caseId: 999999,
 			setup: func() {
 				pact.
 					AddInteraction().
-					Given("I have a case assigned").
-					UponReceiving("A request to add a fee decision").
+					Given("I have case 999999 assigned").
+					UponReceiving("A request to add a fee decision for case 999999").
 					WithRequest(dsl.Request{
 						Method: http.MethodPost,
 						Path:   dsl.String("/lpa-api/v1/cases/999999/fee-decisions"),
@@ -59,6 +61,40 @@ func TestAddFeeDecision(t *testing.T) {
 					})
 			},
 		},
+		{
+			name: "InternalServerError",
+			caseId: 111,
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("I have case 111 assigned").
+					UponReceiving("A request to add a fee decision for case 111").
+					WithRequest(dsl.Request{
+						Method: http.MethodPost,
+						Path:   dsl.String("/lpa-api/v1/cases/111/fee-decisions"),
+						Headers: dsl.MapMatcher{
+							"Content-Type": dsl.String("application/json"),
+						},
+						Body: map[string]interface{}{
+							"decisionType": "DECLINED_REMISSION",
+							"decisionReason": "Insufficient evidence",
+							"decisionDate": "18/10/2023",
+						},
+					}).
+					WillRespondWith(dsl.Response{
+						Status:  http.StatusInternalServerError,
+						Headers: dsl.MapMatcher{"Content-Type": dsl.String("application/json")},
+					})
+			},
+			expectedError: func(pactPort int) error {
+				return StatusError{
+					Code: http.StatusInternalServerError,
+					URL: fmt.Sprintf("http://localhost:%d/lpa-api/v1/cases/111/fee-decisions", pactPort),
+					Method: http.MethodPost,
+					CorrelationId: "",
+				}
+			},
+		},
 	}
 
 	for _, tc := range testCases {
@@ -71,7 +107,7 @@ func TestAddFeeDecision(t *testing.T) {
 				// ctx Context, caseID int, decisionType string, decisionReason string, decisionDate DateString
 				err := client.AddFeeDecision(
 					Context{Context: context.Background()},
-					999999,
+					tc.caseId,
 					"DECLINED_REMISSION",
 					"Insufficient evidence",
 					DateString("2023-10-18"),
