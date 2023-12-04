2634b869203a96db1c0551493657040853eb262a
Gemma Taylor <gemma.taylor@digital.justice.gov.uk>
2023-12-04 09:32:01 +0000

    VEGA-2165 add casework summary to digital LPA page (#423)
    * added case summary to digital LPA page as per design. VEGA-2165 #minor
    
    * changed show hide case summary JS to check for all elements. VEGA-2165 #minor
    
    * ran prettier to fix linting. VEGA-2165 #minor
    
    * added vertical padding to show hide accordian button to make sure box shadow doesnt exceed height of case summary when collapsed. VEGA-2165 #minor
    
    * code review tweaks. VEGA-2165 #minor
    
    * ran yarn prettier. VEGA-2165 #minor
    
    * changed js to check if button is found on page before trying to access other elements within. VEGA-2165 #minor
    
    * fixed js with prettier. VEGA-2165 #minor
    
    * changed event listener back to the button rather than case summary container. VEGA-2165 #minor

---
+++ web/assets/show-hide-case-summary.js
@@ -0,0 +1,40 @@
+export default function showHideCaseSummary(prefix) {
+  const button = document.querySelector(
+    '[data-module="app-case-summary-toggle"]',
+  );
+
+  if (button) {
+    const container = button.closest(
+      '[data-id="caseworker-summary-container"]',
+    );
+    const buttonText = container.querySelector(
+      '[data-id="case-summary-toggle-text"]',
+    );
+    const buttonIcon = container.querySelector(
+      '[data-id="case-summary-toggle-icon"]',
+    );
+    const caseSummary = container.querySelector('[data-id="case-summary"]');
+
+    if (container && buttonText && buttonIcon && caseSummary) {
+      button.addEventListener("click", () => {
+        if (buttonText.innerText === "Hide") {
+          buttonText.innerText = "Show";
+          buttonIcon.classList.replace(
+            "govuk-accordion-nav__chevron--up",
+            "govuk-accordion-nav__chevron--down",
+          );
+          caseSummary.hidden = true;
+          caseSummary.ariaExpanded = "false";
+        } else {
+          buttonText.innerText = "Hide";
+          buttonIcon.classList.replace(
+            "govuk-accordion-nav__chevron--down",
+            "govuk-accordion-nav__chevron--up",
+          );
+          caseSummary.hidden = false;
+          caseSummary.ariaExpanded = "true";
+        }
+      });
+    }
+  }
+}
