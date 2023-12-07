3fec9b3cd54ce4696bd5133459d1dd31b4238b6a
gemtay <gemma.taylor@digital.justice.gov.uk>
2023-12-07 15:17:38 +0000

    ran prettier. VEGA-1852 #minor

--- web/assets/full-width-container.js
+++ web/assets/full-width-container.js
@@ -3,16 +3,18 @@ const fullWidthContainer = () => {
     const headerContainer = document.getElementsByClassName(
       "moj-header__container",
     )[0];
-    const mainContainers = document.querySelectorAll(
-      ".govuk-width-container",
-    );
+    const mainContainers = document.querySelectorAll(".govuk-width-container");
 
     if (headerContainer && mainContainers) {
       headerContainer.style.maxWidth = "none";
       headerContainer.className =
         headerContainer.className +
         " govuk-!-margin-left-5 govuk-!-margin-right-5";
-      mainContainers.forEach((container) => container.className = "govuk-!-margin-left-5 govuk-!-margin-right-5");
+      mainContainers.forEach(
+        (container) =>
+          (container.className =
+            "govuk-!-margin-left-5 govuk-!-margin-right-5"),
+      );
     }
   }
 };
