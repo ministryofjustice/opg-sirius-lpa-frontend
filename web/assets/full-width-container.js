6d77c7ee12a71035b82f9b2d00ad140d5421b030
gemtay <gemma.taylor@digital.justice.gov.uk>
2023-12-07 14:34:25 +0000

    implemented standard header with responsive design considerations. VEGA-1852 #minor

--- web/assets/full-width-container.js
+++ web/assets/full-width-container.js
@@ -3,15 +3,16 @@ const fullWidthContainer = () => {
     const headerContainer = document.getElementsByClassName(
       "moj-header__container",
     )[0];
-    const mainContainer = document.getElementsByClassName(
-      "govuk-width-container",
-    )[0];
-    if (headerContainer && mainContainer) {
+    const mainContainers = document.querySelectorAll(
+      ".govuk-width-container",
+    );
+
+    if (headerContainer && mainContainers) {
       headerContainer.style.maxWidth = "none";
       headerContainer.className =
         headerContainer.className +
         " govuk-!-margin-left-5 govuk-!-margin-right-5";
-      mainContainer.className = "govuk-!-margin-left-5 govuk-!-margin-right-5";
+      mainContainers.forEach((container) => container.className = "govuk-!-margin-left-5 govuk-!-margin-right-5");
     }
   }
 };
