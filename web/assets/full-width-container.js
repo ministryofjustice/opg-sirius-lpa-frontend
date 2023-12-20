204d204ca78356a6888b0d596a0a819c93b38623
gemtay <gemma.taylor@digital.justice.gov.uk>
2023-12-20 14:01:45 +0000

    fixed full width container js to work correctly on search results page. VEGA-1853 #minor

--- web/assets/full-width-container.js
+++ web/assets/full-width-container.js
@@ -1,21 +1,16 @@
 const fullWidthContainer = () => {
-    if (window.location.pathname.includes("/search")) {
-        const headerContainer = document.getElementsByClassName(
-            "moj-header__container",
-        )[0];
-        const mainContainers = document.querySelectorAll(".govuk-width-container");
+  if (window.location.pathname.includes("/search")) {
+    const mainContainers = document.querySelectorAll(".govuk-width-container");
 
-        if (headerContainer && mainContainers) {
-            headerContainer.style.maxWidth = "none";
-            headerContainer.className =
-                headerContainer.className +
-                " govuk-!-margin-left-5 govuk-!-margin-right-5";
-            mainContainers.forEach(
-                (container) =>
-                    (container.className =
-                        "govuk-!-margin-left-5 govuk-!-margin-right-5"),
-            );
-        }
+    if (mainContainers) {
+      mainContainers.forEach((container) => {
+        container.style.maxWidth = "none";
+        container.classList.add(
+          "govuk-!-margin-left-5",
+          "govuk-!-margin-right-5",
+        );
+      });
     }
+  }
 };
 export default fullWidthContainer;
