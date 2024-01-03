4d60e857613a75937a0d583fc73539a5cdcc9de9
Gemma Taylor <gemma.taylor@digital.justice.gov.uk>
2024-01-03 11:49:19 +0000

    VEGA-1853 support inline search in new header (#433)
    * implemented standard header with responsive design considerations. VEGA-1852 #minor
    
    * ran prettier. VEGA-1852 #minor
    
    * code review tweaks to html & css. VEGA-1852 #minor
    
    * implemented dropdown search results component in new header. VEGA-1853 #minor
    
    * cypress test change for inline search. VEGA-1853 #minor
    
    * created dockerfile to install git ontop of node alpine to fix error cannot find binary git. Removed cypress test. VEGA-1853 #minor
    
    * fixed full width container js to work correctly on search results page. VEGA-1853 #minor
    
    * use tagged version on the search ui componenent to allow it to be PMQAd, will remove tag once this is done. VEGA-1853 #minor
    
    * moved id onto search input form to attach the dropdown to just the input box not including search button. VEGA-1853 #minor
    
    * prettier fix. VEGA-1853 #minor
    
    * updated search ui component tag to latest changes. VEGA-1853 #minor

--- web/assets/full-width-container.js
+++ web/assets/full-width-container.js
@@ -1,20 +1,15 @@
 const fullWidthContainer = () => {
   if (window.location.pathname.includes("/search")) {
-    const headerContainer = document.getElementsByClassName(
-      "moj-header__container",
-    )[0];
     const mainContainers = document.querySelectorAll(".govuk-width-container");
 
-    if (headerContainer && mainContainers) {
-      headerContainer.style.maxWidth = "none";
-      headerContainer.className =
-        headerContainer.className +
-        " govuk-!-margin-left-5 govuk-!-margin-right-5";
-      mainContainers.forEach(
-        (container) =>
-          (container.className =
-            "govuk-!-margin-left-5 govuk-!-margin-right-5"),
-      );
+    if (mainContainers) {
+      mainContainers.forEach((container) => {
+        container.style.maxWidth = "none";
+        container.classList.add(
+          "govuk-!-margin-left-5",
+          "govuk-!-margin-right-5",
+        );
+      });
     }
   }
 };
