99b3976b1b03ffe69a2953da51a72ea1b0768357
Elliot Smith <elliot.smith@justice.digital.gov.uk>
2024-01-05 10:30:17 +0000

    Complete HTML to display warnings (still dummy data)

--- internal/sirius/case_summary.go
+++ internal/sirius/case_summary.go
@@ -24,8 +24,11 @@ func (c *Client) CaseSummary(ctx Context, uid string) (CaseSummary, error) {
 	}
 
 	cs.WarningList = []Warning{
-		Warning{ID: 1, WarningType: "Warning type", WarningText: "Warning text sits underneath. Warning text sits underneath.", DateAdded: "11th Aug 2023",},
 		Warning{ID: 2, WarningType: "Warning type", WarningText: "Warning text sits underneath. Warning text sits underneath.", DateAdded: "12th Dec 2023",},
+		Warning{ID: 1, WarningType: "Warning type", WarningText: "Warning text sits underneath. Warning text sits underneath.", DateAdded: "11th Aug 2023",},
+		Warning{ID: 3, WarningType: "Warning type", WarningText: "Warning text sits underneath. Warning text sits underneath.", DateAdded: "8th Jan 2023",},
+		Warning{ID: 4, WarningType: "Warning type", WarningText: "Warning text sits underneath. Warning text sits underneath.", DateAdded: "17th Dec 2022",},
+		Warning{ID: 5, WarningType: "Warning type", WarningText: "Warning text sits underneath. Warning text sits underneath.", DateAdded: "1st Sept 2022",},
 	}
 
 	return cs, nil
