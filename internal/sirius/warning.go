dc7291d3862c8459c36e054510f0c7bc0255a884
Elliot Smith <elliot.smith@justice.digital.gov.uk>
2024-01-05 13:44:29 +0000

    Show cases warning is applied to

--- internal/sirius/warning.go
+++ internal/sirius/warning.go
@@ -1,9 +1,9 @@
 package sirius
 
 type Warning struct {
-	ID          int        `json:"id"`
-	WarningType string     `json:"warningType"`
-	WarningText string     `json:"warningText"`
-	DateAdded   DateString `json:"dateAdded"`
-	CaseItems   []Case     `json:"caseItems"`
+	ID          int    `json:"id"`
+	WarningType string `json:"warningType"`
+	WarningText string `json:"warningText"`
+	DateAdded   string `json:"dateAdded"`
+	CaseItems   []Case `json:"caseItems"`
 }
