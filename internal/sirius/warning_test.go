148970002a1f4bbd8514d9c86d07ac80bbcab9a6
Elliot Smith <elliot.smith@justice.digital.gov.uk>
2024-01-08 17:31:07 +0000

    Fill out remaining pact and unit tests to cover warnings fetches

--- internal/sirius/warning_test.go
+++ internal/sirius/warning_test.go
@@ -1,8 +1,11 @@
 package sirius
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"testing"
 
@@ -18,6 +21,38 @@ type mockWarningHttpClient struct {
 func (m *mockWarningHttpClient) Do(req *http.Request) (*http.Response, error) {
 	args := m.Called(req)
 	return args.Get(0).(*http.Response), args.Error(1)
+}
+
+func TestWarningsForCaseBadContext(t *testing.T) {
+	client := NewClient(http.DefaultClient, "http://localhost")
+	_, err := client.WarningsForCase(Context{Context: nil}, 990)
+	assert.Equal(t, "net/http: nil Context", err.Error())
+}
+
+func TestWarningsForCaseNetworkError(t *testing.T) {
+	mockWarningHttpClient := &mockWarningHttpClient{}
+
+	mockWarningHttpClient.On("Do", mock.Anything).Return(&http.Response{}, errors.New("Networking issue"))
+
+	client := NewClient(mockWarningHttpClient, "http://localhost")
+	_, err := client.TasksForCase(Context{Context: context.Background()}, 990)
+	assert.Equal(t, "Networking issue", err.Error())
+}
+
+func TestWarningsForCaseBadResponseJSON(t *testing.T) {
+	mockWarningHttpClient := &mockWarningHttpClient{}
+
+	badJsonResponse := http.Response{
+		StatusCode: 200,
+		Body:       io.NopCloser(bytes.NewBufferString("[bad json time")),
+	}
+
+	mockWarningHttpClient.On("Do", mock.Anything).Return(&badJsonResponse, nil)
+
+	client := NewClient(mockWarningHttpClient, "http://localhost")
+	_, err := client.TasksForCase(Context{Context: context.Background()}, 990)
+
+	assert.Equal(t, "invalid character 'b' looking for beginning of value", err.Error())
 }
 
 func TestWarningsForCase(t *testing.T) {
@@ -47,7 +82,7 @@ func TestWarningsForCase(t *testing.T) {
 						Status: http.StatusOK,
 						Body: dsl.EachLike(map[string]interface{}{
 							"id":           dsl.Like(9901),
-							"dateAdded":    dsl.String("08/01/2023"),
+							"dateAdded":    dsl.String("07/01/2023"),
 							"warningType":  dsl.String("Donor Deceased"),
 							"warningText":  dsl.String("Donor died"),
 							"caseItems":    dsl.EachLike(map[string]interface{}{
@@ -61,7 +96,7 @@ func TestWarningsForCase(t *testing.T) {
 			expectedResponse: []Warning{
 				Warning{
 					ID: 9901,
-					DateAdded: "08/01/2023",
+					DateAdded: "07/01/2023",
 					WarningType: "Donor Deceased",
 					WarningText: "Donor died",
 					CaseItems: []Case{
@@ -71,6 +106,30 @@ func TestWarningsForCase(t *testing.T) {
 						},
 					},
 				},
+			},
+		},
+		{
+			name: "404",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("There is no case with warnings with the specified ID").
+					UponReceiving("A request for the warnings for a non-existent case").
+					WithRequest(dsl.Request{
+						Method: http.MethodGet,
+						Path:   dsl.String("/lpa-api/v1/cases/990/warnings"),
+					}).
+					WillRespondWith(dsl.Response{
+						Status: http.StatusNotFound,
+					})
+			},
+			expectedError: func(port int) error {
+				return StatusError{
+					Code:          404,
+					URL:           fmt.Sprintf("http://localhost:%d/lpa-api/v1/cases/990/warnings", port),
+					Method:        "GET",
+					CorrelationId: "",
+				}
 			},
 		},
 	}
