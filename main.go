47ec128b5bddf96ad7c181b098f4dd77739b8edb
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2024-02-23 11:11:20 +0000

    Refactor telemetry functions into new package
    To minimise the amount of bootstrapping inside other files.
    
    #patch

--- main.go
+++ main.go
@@ -14,15 +14,10 @@ import (
 	"github.com/ministryofjustice/opg-go-common/template"
 	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/server"
 	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/sirius"
+	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/telemetry"
 	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/templatefn"
-	"go.opentelemetry.io/contrib/detectors/aws/ecs"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
-	"go.opentelemetry.io/contrib/propagators/aws/xray"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
-	"go.opentelemetry.io/otel/sdk/trace"
 	"golang.org/x/mod/sumdb/dirhash"
-	"google.golang.org/grpc"
 )
 
 func fatal(err error, logger *slog.Logger) {
@@ -30,69 +25,15 @@ func fatal(err error, logger *slog.Logger) {
 	os.Exit(1)
 }
 
-func initTracerProvider(ctx context.Context, logger *slog.Logger) func() {
-	resource, err := ecs.NewResourceDetector().Detect(ctx)
-	if err != nil {
-		logger.Error(err.Error())
-		os.Exit(1)
-	}
-
-	var traceExporter trace.SpanExporter
-	if env.Get("TRACING_ENABLED", "0") == "1" {
-		traceExporter, err = otlptracegrpc.New(ctx,
-			otlptracegrpc.WithInsecure(),
-			otlptracegrpc.WithEndpoint("0.0.0.0:4317"),
-			otlptracegrpc.WithDialOption(grpc.WithBlock()),
-		)
-	}
-	if err != nil {
-		fatal(err, logger)
-	}
-
-	idg := xray.NewIDGenerator()
-	tp := trace.NewTracerProvider(
-		trace.WithResource(resource),
-		trace.WithSampler(trace.AlwaysSample()),
-		trace.WithBatcher(traceExporter),
-		trace.WithIDGenerator(idg),
-	)
-
-	otel.SetTracerProvider(tp)
-	otel.SetTextMapPropagator(xray.Propagator{})
-
-	return func() {
-		if err := tp.Shutdown(ctx); err != nil {
-			fatal(err, logger)
-		}
-	}
-}
-
 func main() {
-	logger := slog.New(slog.
-		NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-			ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
-				switch a.Value.Kind() {
-				case slog.KindAny:
-					switch v := a.Value.Any().(type) {
-					case *http.Request:
-						return slog.Group(a.Key,
-							slog.String("method", v.Method),
-							slog.String("uri", v.URL.String()))
-					}
-				}
-
-				return a
-			},
-		}).
-		WithAttrs([]slog.Attr{
-			slog.String("service_name", "opg-sirius-lpa-frontend"),
-		}))
+	logger := telemetry.NewLogger()
 
 	port := env.Get("PORT", "8080")
 	webDir := env.Get("WEB_DIR", "web")
 	siriusURL := env.Get("SIRIUS_URL", "http://localhost:9001")
 	siriusPublicURL := env.Get("SIRIUS_PUBLIC_URL", "")
 	prefix := env.Get("PREFIX", "")
+	exportTraces := env.Get("TRACING_ENABLED", "0") == "1"
 
 	staticHash, err := dirhash.HashDir(webDir+"/static", webDir, dirhash.DefaultHash)
 	if err != nil {
@@ -104,7 +45,7 @@ func main() {
 		fatal(err, logger)
 	}
 
-	shutdown := initTracerProvider(context.Background(), logger)
+	shutdown := telemetry.InitTracerProvider(context.Background(), logger, exportTraces)
 	defer shutdown()
 
 	httpClient := http.DefaultClient
