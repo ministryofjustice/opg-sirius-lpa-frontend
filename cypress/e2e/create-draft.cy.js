f0c3f4db0a60893f91a9240b3e7ac3998001c5eb
Mabel Li <33980519+mabely@users.noreply.github.com>
2024-03-01 12:58:36 +0000

    vega-2358 - Amend form field names for donor and correspondent fields (#483)
    * Amend form field names for donor and correspondent fields
    vega-2358#minor
    
    * Update tests
    
    * Add to cypress readme
    
    * Add inset and warning text about creating an LPA
    
    * Update button name in cypress test

--- cypress/e2e/create-draft.cy.js
+++ cypress/e2e/create-draft.cy.js
@@ -16,8 +16,7 @@ describe("Create Digital LPA draft", () => {
     cy.contains("Personal welfare").click();
     cy.contains("Property and affairs").click();
 
-    cy.get("#f-donorFirstname").type("Coleen");
-    cy.get("#f-donorMiddlename").type("Stephanie");
+    cy.get("#f-donorFirstname").type("Coleen Stephanie");
     cy.get("#f-donorSurname").type("Morneault");
 
     cy.get("#f-dob-day").type("8");
@@ -53,7 +52,6 @@ describe("Create Digital LPA draft", () => {
 
     cy.contains("Another person").click();
     cy.get("#f-correspondentFirstname").type("Leon");
-    cy.get("#f-correspondentMiddlename").type("Marius");
     cy.get("#f-correspondentSurname").type("Selden");
 
     cy.get("#f-correspondentSurname")
@@ -72,7 +70,7 @@ describe("Create Digital LPA draft", () => {
     cy.get("#f-donorPhone").type("07893932118");
     cy.get("#f-donorEmail").type("c.morneault@somehost.example");
 
-    cy.contains("Save draft LPA").click();
+    cy.contains("Confirm and create draft LPA").click();
     cy.get(".govuk-panel").contains(
       "Draft Personal welfare and Property and affairs LPAs for the donor Coleen Stephanie Morneault have been saved",
     );
