71a9c214cead7c8a1d17a315be77e0757f0bbff6
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2024-04-24 17:02:10 +0100

    Add baseline cypress test for application progress and align with go test

---
+++ cypress/e2e/application-progress.cy.js
@@ -0,0 +1,82 @@
+describe("View the application progress for a digital LPA", () => {
+  const uid = "M-QEQE-EEEE-WERT";
+  const id = 113222;
+
+  beforeEach(() => {
+    cy.addMock(`/lpa-api/v1/digital-lpas/${uid}`, "GET", {
+      status: 200,
+      body: {
+        uId: uid,
+        "opg.poas.sirius": {
+          id: id,
+          uId: "M-QEQE-EEEE-WERT",
+          status: "Processing",
+          caseSubtype: "personal-welfare",
+          createdDate: "24/04/2024",
+          investigationCount: 2,
+          complaintCount: 1,
+          taskCount: 2,
+          warningCount: 0,
+          donor: {
+            id: 33,
+          },
+          application: {
+            donorFirstNames: "Peter",
+            donorLastName: "Maaaabbbb",
+            donorDob: "27/05/1968",
+            donorEmail: "peter@bbsssnssssss.org",
+            donorPhone: "073656249524",
+            donorAddress: {
+              addressLine1: "Flat 9999",
+              addressLine2: "Flaim House",
+              addressLine3: "33 Marb Road",
+              country: "GB",
+              postcode: "X15 3XX",
+            },
+            correspondentFirstNames: "Salty",
+            correspondentLastName: "McNab",
+            correspondentAddress: {
+              addressLine1: "Flat 3",
+              addressLine2: "Digital LPA Avenue",
+              addressLine3: "Noplace",
+              country: "GB",
+              postcode: "SW1 1AA",
+            },
+          },
+          linkedDigitalLpas: [],
+        },
+        "opg.poas.lpastore": null,
+      },
+    });
+
+    cy.addMock(`/lpa-api/v1/cases/${id}/warnings`, "GET", {
+      status: 200,
+      body: [],
+    });
+
+    cy.addMock(
+      `/lpa-api/v1/cases/${id}/tasks?filter=status%3ANot+started%2Cactive%3Atrue&limit=99&sort=duedate%3AASC`,
+      "GET",
+      {
+        status: 200,
+        body: {
+          tasks: [],
+        },
+      },
+    );
+
+    cy.addMock(`/lpa-api/v1/digital-lpas/${uid}/progress-indicators`, "GET", {
+      status: 200,
+      body: {
+        digitalLpaUid: uid,
+        progressIndicators: [
+          { "indicator": "FEES", "status": "Complete" },
+        ],
+      },
+    });
+  });
+
+  it("shows application progress", () => {
+    cy.visit(`/lpa/${uid}`);
+  });
+});
--- internal/sirius/progress_indicator_test.go
+++ internal/sirius/progress_indicator_test.go
@@ -28,12 +28,12 @@ func TestProgressIndicatorsForDigitalLpa(t *testing.T) {
 		UponReceiving("A request for the progress indicators for a digital LPA").
 		WithRequest(dsl.Request{
 			Method: http.MethodGet,
-			Path:   dsl.String("/lpa-api/v1/digital-lpas/M-QQQQ-EEEE-VVVV/progress-indicators"),
+			Path:   dsl.String("/lpa-api/v1/digital-lpas/M-QEQE-EEEE-WERT/progress-indicators"),
 		}).
 		WillRespondWith(dsl.Response{
 			Status: http.StatusOK,
 			Body: dsl.Like(map[string]interface{}{
-				"digitalLpaUid": dsl.Like("M-QQQQ-EEEE-VVVV"),
+				"digitalLpaUid": dsl.Like("M-QEQE-EEEE-WERT"),
 				"progressIndicators": dsl.EachLike(map[string]interface{}{
 					"indicator": dsl.Like("FEES"),
 					"status":    dsl.Like("IN_PROGRESS"),
@@ -45,7 +45,7 @@ func TestProgressIndicatorsForDigitalLpa(t *testing.T) {
 	assert.Nil(t, pact.Verify(func() error {
 		client := NewClient(http.DefaultClient, fmt.Sprintf("http://localhost:%d", pact.Server.Port))
 
-		progressIndicators, err := client.ProgressIndicatorsForDigitalLpa(Context{Context: context.Background()}, "M-QQQQ-EEEE-VVVV")
+		progressIndicators, err := client.ProgressIndicatorsForDigitalLpa(Context{Context: context.Background()}, "M-QEQE-EEEE-WERT")
 
 		assert.Equal(t, expectedResponse, progressIndicators)
 		assert.Nil(t, err)
