15706cc16674524524bb7ee40a29a91f3abd1843
Mishkat Najam <mishkat.najam@digital.justice.gov.uk>
2024-04-25 11:37:14 +0100

    WIP passing pact test

--- internal/sirius/get_events.go
+++ internal/sirius/get_events.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 )
 
-func (c *Client) GetEvents(ctx Context, id int, caseId int) (any, error) {
+func (c *Client) GetEvents(ctx Context, donorId int, caseId int) (any, error) {
 	var v struct {
 		Events any `json:"events"`
 	}
 
-	err := c.get(ctx, fmt.Sprintf("/lpa-api/v1/persons/%d/events?filter=case:%d&sort=id:desc", id, caseId), &v)
+	err := c.get(ctx, fmt.Sprintf("/lpa-api/v1/persons/%d/events?filter=case:%d&sort=id:desc", donorId, caseId), &v)
 
 	return v.Events, err
 }
