3369334a6cf3bec449cbee1576276ec4330e6070
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2024-04-29 13:30:46 +0100

    Add assertions to application progress cypress test

--- cypress/e2e/application-progress.cy.js
+++ cypress/e2e/application-progress.cy.js
@@ -80,5 +80,11 @@ describe("View the application progress for a digital LPA", () => {
 
   it("shows application progress", () => {
     cy.visit(`/lpa/${uid}`);
+
+    cy.get(".app-progress-indicator-summary").then((elts) => {
+      expect(Cypress.$(elts[0]).find("svg[data-progress-indicator=not-started]").length).to.equal(1);
+      expect(Cypress.$(elts[1]).find("svg[data-progress-indicator=complete]").length).to.equal(1);
+      expect(Cypress.$(elts[2]).find("svg[data-progress-indicator=in-progress]").length).to.equal(1);
+    })
   });
 });
