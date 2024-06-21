f75259d52629f25f05939bdea13b7819041166ac
MishNajam <61416092+MishNajam@users.noreply.github.com>
2024-06-21 09:47:38 +0100

    VEGA-2419 Cert provider redirects to paper id service (#548)

--- cypress/e2e/application-progress.cy.js
+++ cypress/e2e/application-progress.cy.js
@@ -45,7 +45,24 @@ describe("View the application progress for a digital LPA", () => {
           },
           linkedDigitalLpas: [],
         },
-        "opg.poas.lpastore": null,
+        "opg.poas.lpastore": {
+          attorneys: [
+            {
+              firstNames: "Esther",
+              lastName: "Greenwood",
+              status: "active",
+            },
+          ],
+          certificateProvider: {
+            channel: "paper",
+            firstNames: "Fake",
+            lastName: "Provider",
+          },
+          lpaType: "pf",
+          channel: "paper",
+          registrationDate: "2022-12-18",
+          peopleToNotify: [],
+        },
       },
     });
 
@@ -122,6 +139,11 @@ describe("View the application progress for a digital LPA", () => {
     cy.contains("Donor identity confirmation").click();
     cy.contains("Not started");
     cy.contains("Start donor identity check").should("not.exist");
+
+    cy.contains("Certificate provider identity confirmation").click();
+    cy.contains("Start certificate provider identity check").should(
+      "not.exist",
+    );
   });
 
   it("shows application progress in progress", () => {
@@ -180,6 +202,16 @@ describe("View the application progress for a digital LPA", () => {
     cy.contains("Donor identity confirmation").click();
     cy.contains("In progress");
     cy.contains("Start donor identity check").should("exist");
+    cy.get(
+      "a[href='/lpa/identity-check/start?personType=donor&lpas[]=M-QEQE-EEEE-WERT']",
+    ).contains("Start donor identity check");
+
+    cy.contains("Certificate provider identity confirmation").click();
+    cy.contains("Certificate provider: Fake Provider");
+    cy.contains("Start certificate provider identity check").should("exist");
+    cy.get(
+      "a[href='/lpa/identity-check/start?personType=certificate-provider&lpas[]=M-QEQE-EEEE-WERT']",
+    ).contains("Start certificate provider identity check");
   });
 
   it("shows application progress completed", () => {
@@ -233,5 +265,108 @@ describe("View the application progress for a digital LPA", () => {
     cy.contains("Donor identity confirmation").click();
     cy.contains("Complete");
     cy.contains("Start donor identity check").should("not.exist");
+
+    cy.contains("Certificate provider identity confirmation").click();
+    cy.contains("Certificate provider: Fake Provider");
+    cy.contains("Start certificate provider identity check").should(
+      "not.exist",
+    );
+  });
+
+  it("does not show link to id check when actor is online", () => {
+    cy.addMock(`/lpa-api/v1/digital-lpas/${uid}`, "GET", {
+      status: 200,
+      body: {
+        uId: uid,
+        "opg.poas.sirius": {
+          id: id,
+          uId: "M-QEQE-EEEE-WERT",
+          status: "Processing",
+          caseSubtype: "personal-welfare",
+          createdDate: "24/04/2024",
+          investigationCount: 2,
+          complaintCount: 1,
+          taskCount: 2,
+          warningCount: 0,
+          donor: {
+            id: 33,
+          },
+          application: {
+            donorFirstNames: "Peter",
+            donorLastName: "Maaaabbbb",
+            donorDob: "27/05/1968",
+            donorEmail: "peter@bbsssnssssss.org",
+            donorPhone: "073656249524",
+            donorAddress: {
+              addressLine1: "Flat 9999",
+              addressLine2: "Flaim House",
+              addressLine3: "33 Marb Road",
+              country: "GB",
+              postcode: "X15 3XX",
+            },
+            correspondentFirstNames: "Salty",
+            correspondentLastName: "McNab",
+            correspondentAddress: {
+              addressLine1: "Flat 3",
+              addressLine2: "Digital LPA Avenue",
+              addressLine3: "Noplace",
+              country: "GB",
+              postcode: "SW1 1AA",
+            },
+          },
+          linkedDigitalLpas: [],
+        },
+        "opg.poas.lpastore": {
+          attorneys: [
+            {
+              firstNames: "Esther",
+              lastName: "Greenwood",
+              status: "active",
+            },
+          ],
+          certificateProvider: {
+            channel: "online",
+            firstNames: "Fake",
+            lastName: "Provider",
+          },
+          lpaType: "pf",
+          channel: "online",
+          registrationDate: "2022-12-18",
+          peopleToNotify: [],
+        },
+      },
+    });
+
+    cy.addMock(`/lpa-api/v1/digital-lpas/${uid}/progress-indicators`, "GET", {
+      status: 200,
+      body: {
+        digitalLpaUid: uid,
+        progressIndicators: [
+          { indicator: "DONOR_ID", status: "IN_PROGRESS" },
+          { indicator: "CERTIFICATE_PROVIDER_ID", status: "IN_PROGRESS" },
+        ],
+      },
+    });
+
+    cy.visit(`/lpa/${uid}`);
+
+    cy.get(".app-progress-indicator-summary").then((elts) => {
+      expect(
+        Cypress.$(elts[0]).find("svg[data-progress-indicator=in-progress]")
+          .length,
+      ).to.equal(1);
+      expect(
+        Cypress.$(elts[1]).find("svg[data-progress-indicator=in-progress]")
+          .length,
+      ).to.equal(1);
+    });
+
+    cy.contains("Donor identity confirmation").click();
+    cy.contains("Start donor identity check").should("not.exist");
+
+    cy.contains("Certificate provider identity confirmation").click();
+    cy.contains("Start certificate provider identity check").should(
+      "not.exist",
+    );
   });
 });
