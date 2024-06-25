6268fa53398884334effbccdd55a8939cb141c85
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2024-06-25 10:32:06 +0100

    Fetch anomalies when getting data for LPA details page

--- internal/server/get_lpa_details.go
+++ internal/server/get_lpa_details.go
@@ -1,21 +1,23 @@
 package server
 
 import (
-	"github.com/go-chi/chi/v5"
-	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/sirius"
-	"golang.org/x/sync/errgroup"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/ministryofjustice/opg-go-common/template"
+	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/sirius"
+	"golang.org/x/sync/errgroup"
 )
 
 type GetLpaDetailsClient interface {
 	CaseSummary(ctx sirius.Context, uid string) (sirius.CaseSummary, error)
+	AnomaliesForDigitalLpa(ctx sirius.Context, uid string) ([]sirius.Anomaly, error)
 }
 
 type getLpaDetails struct {
 	CaseSummary             sirius.CaseSummary
 	DigitalLpa              sirius.DigitalLpa
+	AnomalyDisplay          sirius.AnomalyDisplay
 	ReplacementAttorneys    []sirius.LpaStoreAttorney
 	NonReplacementAttorneys []sirius.LpaStoreAttorney
 	FlashMessage            FlashNotification
@@ -28,6 +30,7 @@ func GetLpaDetails(client GetLpaDetailsClient, tmpl template.Template) Handler {
 
 		var err error
 		var data getLpaDetails
+		var anomalies []sirius.Anomaly
 
 		group, groupCtx := errgroup.WithContext(ctx.Context)
 
@@ -39,9 +42,20 @@ func GetLpaDetails(client GetLpaDetailsClient, tmpl template.Template) Handler {
 			return nil
 		})
 
+		group.Go(func() error {
+			anomalies, err = client.AnomaliesForDigitalLpa(ctx.With(groupCtx), uid)
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+
 		if err := group.Wait(); err != nil {
 			return err
 		}
+
+		data.AnomalyDisplay = sirius.AnomalyDisplay{}
+		data.AnomalyDisplay.Group(&data.CaseSummary.DigitalLpa.LpaStoreData, anomalies)
 
 		data.DigitalLpa = data.CaseSummary.DigitalLpa
 		data.FlashMessage, _ = GetFlash(w, r)
