30d8102d916b5c5ab3571e8d523d19639544b94f
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2024-06-25 15:40:57 +0100

    Add skeleton for anomaly display cypress test

---
+++ cypress/e2e/anomaly.cy.js
@@ -0,0 +1,81 @@
+describe("View and edit anomalies for a digital LPA", () => {
+    beforeEach(() => {
+        cy.addMock("/lpa-api/v1/digital-lpas/M-DIGI-QQQQ-1111", "GET", {
+            status: 200,
+            body: {
+                uId: "M-DIGI-QQQQ-1111",
+                "opg.poas.sirius": {
+                    id: 111,
+                    uId: "M-DIGI-QQQQ-1111",
+                    status: "Processing",
+                    caseSubtype: "property-and-affairs",
+                },
+                "opg.poas.lpastore": {
+                    attorneys: [
+                        {
+                            uid: "attorney-1-uid",
+                            status: "active",
+                        },
+                        {
+                            uid: "replacement-attorney-1-uid",
+                            status: "replacement",
+                        }
+                    ],
+                },
+            },
+        });
+
+        cy.addMock("/lpa-api/v1/cases/111/warnings", "GET", {
+            status: 200,
+            body: [],
+        });
+
+        cy.addMock("/lpa-api/v1/cases/111/tasks?filter=status%3ANot+started%2Cactive%3Atrue&limit=99&sort=duedate%3AASC", "GET", {
+            status: 200,
+            body: {
+                tasks: [],
+            },
+        });
+
+        cy.addMock("/lpa-api/v1/digital-lpas/M-DIGI-QQQQ-1111/anomalies", "GET", {
+            status: 200,
+            body: {
+                anomalies: [
+                    {
+                        id: 123,
+                        status: "detected",
+                        fieldName: "firstNames",
+                        ruleType: "empty",
+                        fieldOwnerUid: "attorney-1-uid",
+                    },
+                    {
+                        id: 124,
+                        status: "detected",
+                        fieldName: "lastName",
+                        ruleType: "empty",
+                        fieldOwnerUid: "attorney-1-uid",
+                    },
+                    {
+                        id: 125,
+                        status: "detected",
+                        fieldName: "firstNames",
+                        ruleType: "empty",
+                        fieldOwnerUid: "replacement-attorney-1-uid",
+                    },
+                    {
+                        id: 126,
+                        status: "detected",
+                        fieldName: "lastName",
+                        ruleType: "empty",
+                        fieldOwnerUid: "replacement-attorney-1-uid",
+                    },
+                ],
+            },
+        });
+    });
+
+    it("shows anomalies", () => {
+        cy.visit("/lpa/M-DIGI-QQQQ-1111/lpa-details");
+    });
+});
+
