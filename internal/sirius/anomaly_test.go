e2eec9226cd0ef59dd2310ebdea0a3b2024433f3
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2024-06-28 09:31:22 +0100

    Remove commented-out section

--- internal/sirius/anomaly_test.go
+++ internal/sirius/anomaly_test.go
@@ -57,43 +57,6 @@ func TestAnomaliesForDigitalLpaNotInStore(t *testing.T) {
 	}))
 }
 
-/*
-func TestAnomaliesForDigitalLpaSuccess(t *testing.T) {
-	pact := newPact()
-
-	defer pact.Teardown()
-
-	expectedResponse := []Anomaly
-
-	pact.
-		AddInteraction().
-		Given("A digital LPA with UID LPA M-XXXX-QTQT-WERT and anomalies exists").
-		UponReceiving("A request for the anomalies for a digital LPA").
-		WithRequest(dsl.Request{
-			Method: http.MethodGet,
-			Path:   dsl.String("/lpa-api/v1/digital-lpas/M-XXXX-QTQT-WERT/anomalies"),
-		}).
-		WillRespondWith(dsl.Response{
-			Status: http.StatusOK,
-			Body: dsl.Like(map[string]interface{}{
-				"uid":       dsl.Like("M-XXXX-QTQT-WERT"),
-				"anomalies": dsl.EachLike(map[string]interface{}{}, 0),
-			}),
-			Headers: dsl.MapMatcher{"Content-Type": dsl.String("application/json")},
-		})
-
-	assert.Nil(t, pact.Verify(func() error {
-		client := NewClient(http.DefaultClient, fmt.Sprintf("http://localhost:%d", pact.Server.Port))
-
-		anomalies, err := client.AnomaliesForDigitalLpa(Context{Context: context.Background()}, "M-XXXX-QTQT-WERT")
-
-		assert.Equal(t, expectedResponse, anomalies)
-		assert.Nil(t, err)
-
-		return nil
-	}))
-}*/
-
 func TestAnomaliesForDigitalLpaFail(t *testing.T) {
 	mockClient := &mockAnomaliesHttpClient{}
 	mockClient.On("Do", mock.Anything).Return(&http.Response{}, errors.New("Networking issue"))
