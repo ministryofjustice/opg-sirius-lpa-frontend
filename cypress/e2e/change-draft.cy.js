c3e0056856c0286c9ccb2a927186e390f86caeb8
MishNajam <61416092+MishNajam@users.noreply.github.com>
2025-02-28 15:04:11 +0000

    patch cypress tests

--- cypress/e2e/change-draft.cy.js
+++ cypress/e2e/change-draft.cy.js
@@ -1,12 +1,12 @@
 describe("Change draft form", () => {
     beforeEach(() => {
-        cy.addMock("/lpa-api/v1/digital-lpas/M-1111-2222-1110", "GET", {
+        cy.addMock("/lpa-api/v1/digital-lpas/M-1111-2222-1111", "GET", {
             status: 200,
             body: {
-                uId: "M-1111-2222-1110",
+                uId: "M-1111-2222-1111",
                 "opg.poas.sirius": {
                     id: 565,
-                    uId: "M-1111-2222-1110",
+                    uId: "M-1111-2222-1111",
                     status: "Draft",
                     caseSubtype: "personal-welfare",
                     createdDate: "24/04/2024",
@@ -15,24 +15,33 @@ describe("Change draft form", () => {
                     taskCount: 0,
                     warningCount: 0,
                     donor: {
-                        id: 33,
+                        id: 44,
                         firstname: "Peter",
                         surname: "MacPhearson",
+                        dob: "1971-11-27",
+                        addressLine1: "15 Cameron Approach",
+                        addressLine2: "Nether Collier",
+                        addressLine3: "",
+                        town: "Worcestershire",
+                        postcode: "BL2 6DI",
+                        country: "GB",
+                        phone: "0123456789",
+                        email: "peter@test.com",
                     },
                     application: {
                         source: "PHONE",
                         donorFirstNames: "Peter",
                         donorLastName: "MacPhearson",
                         donorDob: "27/05/1968",
-                        donorEmail: "peter@test",
-                        donorPhone: "073656249524",
+                        donorEmail: "peter@test.com",
+                        donorPhone: "0123456789",
                         donorAddress: {
-                            addressLine1: "Flat 9999",
-                            addressLine2: "Flaim House",
-                            addressLine3: "33 Marb Road",
-                            town: "Birmingham",
+                            addressLine1: "15 Cameron Approach",
+                            addressLine2: "Nether Collier",
+                            addressLine3: "",
+                            town: "Worcestershire",
                             country: "GB",
-                            postcode: "X15 3XX",
+                            postcode: "BL2 6DI",
                         },
                         correspondentFirstNames: "Salty",
                         correspondentLastName: "McNab",
@@ -53,10 +62,10 @@ describe("Change draft form", () => {
             status: 200,
             body: {
                 id: 565,
-                uId: "M-1111-2222-1110",
+                uId: "M-1111-2222-1111",
                 caseType: "DIGITAL_LPA",
                 donor: {
-                    id: 33,
+                    id: 44,
                 },
             },
         });
@@ -76,21 +85,23 @@ describe("Change draft form", () => {
                 },
             },
         );
-        cy.visit("/lpa/M-1111-2222-1110/change-draft");
+
     });
 
     it("can be visited from the LPA draft Change link", () => {
-        cy.visit("/lpa/M-1111-2222-1110/lpa-details").then(() => {
+        cy.visit("/lpa/M-1111-2222-1111/lpa-details").then(() => {
             cy.get("#f-change-draft").click();
             cy.contains("Change donor details");
             cy.url().should(
                 "contain",
-                "/lpa/M-1111-2222-1110/change-draft",
+                "/lpa/M-1111-2222-1111/change-draft",
             );
         });
     });
 
     it("populates draft details", () => {
+        cy.visit("/lpa/M-1111-2222-1111/change-draft");
+
         cy.get("#f-firstNames").should("have.value", "Peter");
         cy.get("#f-lastName").should("have.value", "MacPhearson");
 
@@ -106,16 +117,18 @@ describe("Change draft form", () => {
         cy.get("#f-address\\.Country").should("have.value", "GB");
 
         cy.get("#f-phoneNumber").should("have.value", "0123456789");
-        cy.get("#f-email").should("have.value", "j@example.com");
+        cy.get("#f-email").should("have.value", "peter@test.com");
     });
 
     it("can go Back to LPA details", () => {
+        cy.visit("/lpa/M-1111-2222-1111/change-draft");
         cy.contains("Back to LPA details").click();
-        cy.url().should("contain", "/lpa/M-1111-2222-1110/lpa-details");
+        cy.url().should("contain", "/lpa/M-1111-2222-1111/lpa-details");
     });
 
     it("can be cancelled, returning to the LPA details", () => {
+        cy.visit("/lpa/M-1111-2222-1111/change-draft");
         cy.contains("Cancel").click();
-        cy.url().should("contain", "/lpa/M-1111-2222-1110/lpa-details");
+        cy.url().should("contain", "/lpa/M-1111-2222-1111/lpa-details");
     });
 });
