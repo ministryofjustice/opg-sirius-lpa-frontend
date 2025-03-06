2a62597945579d647f0e62c56ed4a8a2865691f3
Joshua Hawxwell <m@hawx.me>
2025-03-06 10:01:22 +0000

    MLPAB-2862 Show restrictions and conditions images in details (#616)

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,17 @@
 import { addMock, reset } from "../mocks/wiremock";
 
 Cypress.Commands.add("addMock", async (url, method, response) => {
-  await addMock(url, method, response);
+  // if we need to mock this route there is a good chance the test hits
+  // /lpa-details, and will therefore need to also mock with query
+  // ?presignImages, but assign a lower priority so it can be overwritten
+  if (
+    method == "GET" &&
+    url.match(/^\/lpa-api\/v1\/digital-lpas\/M(-[A-Z0-9]{4}){3}$/)
+  ) {
+    await addMock(url + "?presignImages", method, response, 2);
+  }
+
+  await addMock(url, method, response, 1);
 });
 
 Cypress.Commands.add("resetMocks", async () => {
