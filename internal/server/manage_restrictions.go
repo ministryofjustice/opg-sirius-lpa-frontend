5d5c5ff4b1af24d3521ae2e56f9eaf6648a2087a
MishNajam <61416092+MishNajam@users.noreply.github.com>
2025-03-07 16:15:43 +0000

    Functioning dummy form

--- internal/server/manage_restrictions.go
+++ internal/server/manage_restrictions.go
@@ -1,11 +1,10 @@
 package server
 
 import (
+	"fmt"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-playground/form/v4"
 	"github.com/ministryofjustice/opg-go-common/template"
 	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/sirius"
-	"golang.org/x/sync/errgroup"
 	"net/http"
 )
 
@@ -14,41 +13,73 @@ type ManageRestrictionsClient interface {
 }
 
 type manageRestrictionsData struct {
-	XSRFToken   string
-	Success     bool
-	Error       sirius.ValidationError
-	CaseUID     string
-	CaseSummary sirius.CaseSummary
+	XSRFToken       string
+	Error           sirius.ValidationError
+	CaseUID         string
+	CaseSummary     sirius.CaseSummary
+	SeveranceAction string
 }
 
 func ManageRestrictions(client ManageRestrictionsClient, tmpl template.Template) Handler {
-	if decoder == nil {
-		decoder = form.NewDecoder()
-	}
+	//if decoder == nil {
+	//	decoder = form.NewDecoder()
+	//}
 
 	return func(w http.ResponseWriter, r *http.Request) error {
 		caseUID := chi.URLParam(r, "uid")
 		ctx := getContext(r)
 
-		var cs sirius.CaseSummary
-		var err error
-
-		group, groupCtx := errgroup.WithContext(ctx.Context)
-
-		group.Go(func() error {
-			cs, err = client.CaseSummary(ctx.With(groupCtx), caseUID)
-			if err != nil {
-				return err
-			}
-			return nil
-		})
-
-		if err := group.Wait(); err != nil {
+		cs, err := client.CaseSummary(ctx, caseUID)
+		if err != nil {
 			return err
 		}
 
+		//var cs sirius.CaseSummary
+		//var err error
+		//
+		//group, groupCtx := errgroup.WithContext(ctx.Context)
+		//
+		//group.Go(func() error {
+		//	cs, err = client.CaseSummary(ctx.With(groupCtx), caseUID)
+		//	if err != nil {
+		//		return err
+		//	}
+		//	return nil
+		//})
+		//
+		//if err := group.Wait(); err != nil {
+		//	return err
+		//}
+
 		data := manageRestrictionsData{
-			CaseSummary: cs,
+			CaseSummary:     cs,
+			SeveranceAction: postFormString(r, "severanceAction"),
+			XSRFToken:       ctx.XSRFToken,
+			Error:           sirius.ValidationError{Field: sirius.FieldErrors{}},
+			CaseUID:         caseUID,
+		}
+
+		if r.Method == http.MethodPost {
+			var redirectUrl string
+
+			switch data.SeveranceAction {
+			case "severance-application-not-required":
+				redirectUrl = fmt.Sprintf("/lpa/%s/remove-an-attorney", caseUID)
+
+			case "severance-application-required":
+				redirectUrl = fmt.Sprintf("/lpa/%s/enable-replacement-attorney", caseUID)
+
+			default:
+				w.WriteHeader(http.StatusBadRequest)
+
+				data.Error.Field["severanceAction"] = map[string]string{
+					"reason": "Please select an option",
+				}
+			}
+
+			if !data.Error.Any() && redirectUrl != "" {
+				return RedirectError(redirectUrl)
+			}
 		}
 
 		return tmpl(w, data)
