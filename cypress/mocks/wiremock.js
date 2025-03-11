ecb3d78e78ff8e5599b919f1ead338a83788dea5
Nick Pyett <nick.pyett@digital.justice.gov.uk>
2025-03-11 20:11:23 +0000

    Move logic to add an extra mocked response to wiremock script
    This is so the extra URL is created if we are creating the mocks via
    Cypress or with the mock methods.

--- cypress/mocks/wiremock.js
+++ cypress/mocks/wiremock.js
@@ -1,4 +1,14 @@
-async function addMock(url, method, response, priority) {
+async function addMock(url, method, response, priority = 1) {
+  if (
+    method === "GET" &&
+    url.match(/^\/lpa-api\/v1\/digital-lpas\/M(-[A-Z0-9]{4}){3}\/?$/)
+  ) {
+    // if we need to mock this route there is a good chance the test hits
+    // /lpa-details, and will therefore need to also mock with query
+    // ?presignImages, but assign a lower priority, so it can be overwritten
+    await addMock(url + "?presignImages", "GET", response, 2);
+  }
+
   if (typeof response.body !== "string") {
     response.body = JSON.stringify(response.body);
   }
