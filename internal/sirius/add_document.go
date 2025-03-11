b39e6f0d5dfa7e2f0bdbbf6e7cdeccf5cd33c40e
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2025-03-11 09:59:30 +0000

    Use reusable HTTP methods
    Reduce duplication in Sirius client code by introducing reusable put/post/delete methods.
    
    #patch

--- internal/sirius/add_document.go
+++ internal/sirius/add_document.go
@@ -1,10 +1,7 @@
 package sirius
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"net/http"
 )
 
 type addDocumentRequestData struct {
@@ -17,44 +14,21 @@ type addDocumentRequestData struct {
 }
 
 func (c *Client) AddDocument(ctx Context, caseID int, document Document, docType string) (Document, error) {
-	data, err := json.Marshal(addDocumentRequestData{
+	data := addDocumentRequestData{
 		CaseId:          caseID,
 		CorrespondentID: document.Correspondent.ID,
 		Type:            docType,
 		FileName:        document.FileName,
 		SystemType:      document.SystemType,
 		Content:         document.Content,
-	})
-	if err != nil {
-		return Document{}, err
-	}
-
-	req, err := c.newRequest(ctx, http.MethodPost, fmt.Sprintf("/lpa-api/v1/lpas/%d/documents", caseID), bytes.NewReader(data))
-	if err != nil {
-		return Document{}, err
-	}
-
-	resp, err := c.http.Do(req)
-	if err != nil {
-		return Document{}, err
-	}
-	defer resp.Body.Close() //#nosec G307 false positive
-
-	if resp.StatusCode == http.StatusBadRequest {
-		var v ValidationError
-		if err := json.NewDecoder(resp.Body).Decode(&v); err != nil {
-			return Document{}, err
-		}
-		return Document{}, v
-	}
-
-	if resp.StatusCode != http.StatusCreated {
-		return Document{}, newStatusError(resp)
 	}
 
 	var d Document
-	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+
+	err := c.post(ctx, fmt.Sprintf("/lpa-api/v1/lpas/%d/documents", caseID), data, &d)
+	if err != nil {
 		return Document{}, err
 	}
+
 	return d, nil
 }
