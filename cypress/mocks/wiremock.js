626098803097f30047929eabe1dbaf9f47452ea5
Nick Pyett <nick.pyett@digital.justice.gov.uk>
2025-03-12 10:08:47 +0000

    Await all fetch calls before unblocking function to avoid race conditions in Cypress setups

--- cypress/mocks/wiremock.js
+++ cypress/mocks/wiremock.js
@@ -1,4 +1,6 @@
 async function addMock(url, method, response, priority = 1) {
+  let presignMock = null;
+
   if (
     method === "GET" &&
     url.match(/^\/lpa-api\/v1\/digital-lpas\/M(-[A-Z0-9]{4}){3}\/?$/)
@@ -6,14 +8,14 @@ async function addMock(url, method, response, priority = 1) {
     // if we need to mock this route there is a good chance the test hits
     // /lpa-details, and will therefore need to also mock with query
     // ?presignImages, but assign a lower priority, so it can be overwritten
-    await addMock(url + "?presignImages", "GET", response, 2);
+    presignMock = addMock(url + "?presignImages", "GET", response, 2);
   }
 
   if (typeof response.body !== "string") {
     response.body = JSON.stringify(response.body);
   }
 
-  await fetch(`${Cypress.env("MOCK_SERVER_URI")}/__admin/mappings`, {
+  const mock = fetch(`${Cypress.env("MOCK_SERVER_URI")}/__admin/mappings`, {
     method: "POST",
     body: JSON.stringify({
       request: {
@@ -24,6 +26,10 @@ async function addMock(url, method, response, priority = 1) {
       priority,
     }),
   });
+
+  await Promise.allSettled([presignMock, mock]);
+
+  return mock;
 }
 
 async function reset() {
