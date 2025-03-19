4213057c369f752f21feb5f4f3739fbc7654b423
MishNajam <61416092+MishNajam@users.noreply.github.com>
2025-03-19 17:07:33 +0000

    refactor unit test

--- internal/server/manage_restrictions_test.go
+++ internal/server/manage_restrictions_test.go
@@ -153,52 +153,52 @@ func TestPostManageRestrictions(t *testing.T) {
 	}
 }
 
-func TestPostManageRestrictionsRedirect(t *testing.T) {
-	client := &mockManageRestrictionsClient{}
-	client.
-		On("CaseSummary", mock.Anything, "M-1111-2222-3333").
-		Return(restrictionsCaseSummary, nil)
-	client.On("ClearTask", mock.Anything, 1).Return(nil)
-
-	template := &mockTemplate{}
-
-	server := newMockServer("/lpa/{uid}/manage-restrictions", ManageRestrictions(client, template.Func))
-
-	form := url.Values{
-		"severanceAction": {"severance-application-not-required"},
+func TestPostManageRestrictionsRedirects(t *testing.T) {
+	tests := []struct {
+		name            string
+		severanceAction string
+		severanceStatus *sirius.SeveranceStatusData
+	}{
+		{
+			name:            "Severance application not required",
+			severanceAction: "severance-application-not-required",
+			severanceStatus: nil,
+		},
+		{
+			name:            "Severance application required",
+			severanceAction: "severance-application-required",
+			severanceStatus: &sirius.SeveranceStatusData{Status: "REQUIRED"},
+		},
 	}
 
-	req, _ := http.NewRequest(http.MethodPost, "/lpa/M-1111-2222-3333/manage-restrictions", strings.NewReader(form.Encode()))
-	req.Header.Add("Content-Type", formUrlEncoded)
-	_, err := server.serve(req)
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			client := &mockManageRestrictionsClient{}
+			client.
+				On("CaseSummary", mock.Anything, "M-1111-2222-3333").
+				Return(restrictionsCaseSummary, nil)
 
-	assert.Equal(t, RedirectError("/lpa/M-1111-2222-3333/lpa-details"), err)
-	mock.AssertExpectationsForObjects(t, client, template)
-}
+			if tc.severanceStatus == nil {
+				client.On("ClearTask", mock.Anything, 1).Return(nil)
+			} else {
+				client.
+					On("UpdateSeveranceStatus", mock.Anything, "M-1111-2222-3333", *tc.severanceStatus).
+					Return(nil)
+			}
 
-func TestPostManageRestrictionsSeveranceRequiredRedirect(t *testing.T) {
-	client := &mockManageRestrictionsClient{}
-	client.
-		On("CaseSummary", mock.Anything, "M-1111-2222-3333").
-		Return(restrictionsCaseSummary, nil)
-	client.
-		On("UpdateSeveranceStatus", mock.Anything, "M-1111-2222-3333", sirius.SeveranceStatusData{
-			Status: "REQUIRED",
-		}).
-		Return(nil)
+			template := &mockTemplate{}
+			server := newMockServer("/lpa/{uid}/manage-restrictions", ManageRestrictions(client, template.Func))
 
-	template := &mockTemplate{}
+			form := url.Values{
+				"severanceAction": {tc.severanceAction},
+			}
 
-	server := newMockServer("/lpa/{uid}/manage-restrictions", ManageRestrictions(client, template.Func))
+			req, _ := http.NewRequest(http.MethodPost, "/lpa/M-1111-2222-3333/manage-restrictions", strings.NewReader(form.Encode()))
+			req.Header.Add("Content-Type", formUrlEncoded)
+			_, err := server.serve(req)
 
-	form := url.Values{
-		"severanceAction": {"severance-application-required"},
+			assert.Equal(t, RedirectError("/lpa/M-1111-2222-3333/lpa-details"), err)
+			mock.AssertExpectationsForObjects(t, client, template)
+		})
 	}
-
-	req, _ := http.NewRequest(http.MethodPost, "/lpa/M-1111-2222-3333/manage-restrictions", strings.NewReader(form.Encode()))
-	req.Header.Add("Content-Type", formUrlEncoded)
-	_, err := server.serve(req)
-
-	assert.Equal(t, RedirectError("/lpa/M-1111-2222-3333/lpa-details"), err)
-	mock.AssertExpectationsForObjects(t, client, template)
 }
