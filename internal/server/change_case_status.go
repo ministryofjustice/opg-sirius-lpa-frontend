2d8079e15f51a513f2ab9c7123e022c6e229c7b7
Niladri Das <niladri.das@digital.justice.gov.uk>
2025-06-02 15:55:30 +0100

    VEGA-2913 : Changes for adding a  case status reason

--- internal/server/change_case_status.go
+++ internal/server/change_case_status.go
@@ -5,12 +5,20 @@ import (
 	"github.com/ministryofjustice/opg-go-common/template"
 	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/sirius"
 	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/templatefn"
+	"golang.org/x/sync/errgroup"
 	"net/http"
 )
 
 type ChangeCaseStatusClient interface {
+	RefDataByCategory(ctx sirius.Context, category string) ([]sirius.RefDataItem, error)
 	CaseSummary(sirius.Context, string) (sirius.CaseSummary, error)
 	EditDigitalLPAStatus(sirius.Context, string, sirius.CaseStatusData) error
+}
+
+type statusItem struct {
+	Value           string
+	Label           string
+	ConditionalItem bool
 }
 
 type changeCaseStatusData struct {
@@ -20,8 +28,11 @@ type changeCaseStatusData struct {
 	Success   bool
 	Error     sirius.ValidationError
 
-	OldStatus string
-	NewStatus string
+	StatusItems             []statusItem
+	CaseStatusChangeReasons []sirius.RefDataItem
+	OldStatus               string
+	NewStatus               string
+	StatusChangeReason      string
 }
 
 func ChangeCaseStatus(client ChangeCaseStatusClient, tmpl template.Template) Handler {
@@ -29,6 +40,7 @@ func ChangeCaseStatus(client ChangeCaseStatusClient, tmpl template.Template) Han
 		caseUID := r.FormValue("uid")
 
 		ctx := getContext(r)
+		group, groupCtx := errgroup.WithContext(ctx.Context)
 		cs, err := client.CaseSummary(ctx, caseUID)
 		if err != nil {
 			return err
@@ -41,16 +53,44 @@ func ChangeCaseStatus(client ChangeCaseStatusClient, tmpl template.Template) Han
 		}
 
 		data := changeCaseStatusData{
-			XSRFToken: ctx.XSRFToken,
-			Entity:    fmt.Sprintf("%s %s", cs.DigitalLpa.SiriusData.Subtype, caseUID),
-			CaseUID:   caseUID,
-			OldStatus: status,
-			NewStatus: postFormString(r, "status"),
+			XSRFToken:          ctx.XSRFToken,
+			Entity:             fmt.Sprintf("%s %s", cs.DigitalLpa.SiriusData.Subtype, caseUID),
+			CaseUID:            caseUID,
+			OldStatus:          status,
+			NewStatus:          postFormString(r, "status"),
+			StatusChangeReason: postFormString(r, "statusReason"),
+		}
+
+		data.StatusItems = []statusItem{
+			{Value: "draft", Label: "Draft", ConditionalItem: false},
+			{Value: "in-progress", Label: "In progress", ConditionalItem: false},
+			{Value: "statutory-waiting-period", Label: "Statutory waiting period", ConditionalItem: false},
+			{Value: "registered", Label: "Registered", ConditionalItem: false},
+			{Value: "suspended", Label: "Suspended", ConditionalItem: false},
+			{Value: "do-not-register", Label: "Do not register", ConditionalItem: false},
+			{Value: "expired", Label: "Expired", ConditionalItem: false},
+			{Value: "cannot-register", Label: "Cannot register", ConditionalItem: true},
+			{Value: "cancelled", Label: "Cancelled", ConditionalItem: true},
+			{Value: "de-registered", Label: "De-registered", ConditionalItem: false},
+		}
+
+		group.Go(func() error {
+			data.CaseStatusChangeReasons, err = client.RefDataByCategory(ctx.With(groupCtx), sirius.CaseStatusChangeReason)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		})
+
+		if err := group.Wait(); err != nil {
+			return err
 		}
 
 		if r.Method == http.MethodPost {
 			caseStatusData := sirius.CaseStatusData{
-				Status: data.NewStatus,
+				Status:           data.NewStatus,
+				CaseChangeReason: data.StatusChangeReason,
 			}
 
 			err = client.EditDigitalLPAStatus(ctx, caseUID, caseStatusData)
