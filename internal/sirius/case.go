1c6b76580fbbbeeee98908302344796e5f94a04f
Nick Pyett <contact@nickpyett.com>
2025-07-29 10:37:47 +0100

    Set expected payment amount from Case data, merge error group, fix tests

--- internal/sirius/case.go
+++ internal/sirius/case.go
@@ -5,25 +5,26 @@ import (
 )
 
 type Case struct {
-	ID                int        `json:"id,omitempty"`
-	UID               string     `json:"uId,omitempty"`
-	Status            string     `json:"status"`
-	CaseType          string     `json:"caseType,omitempty"`
-	SubType           string     `json:"caseSubtype,omitempty"`
-	CancellationDate  DateString `json:"cancellationDate,omitempty"`
-	DispatchDate      DateString `json:"dispatchDate,omitempty"`
-	DueDate           DateString `json:"dueDate,omitempty"`
-	InvalidDate       DateString `json:"invalidDate,omitempty"`
-	PaymentDate       DateString `json:"paymentDate,omitempty"`
-	ReceiptDate       DateString `json:"receiptDate,omitempty"`
-	RegistrationDate  DateString `json:"registrationDate,omitempty"`
-	RejectedDate      DateString `json:"rejectedDate,omitempty"`
-	RevokedDate       DateString `json:"revokedDate,omitempty"`
-	WithdrawnDate     DateString `json:"withdrawnDate,omitempty"`
-	Donor             *Person    `json:"donor,omitempty"`
-	TrustCorporations []Person   `json:"trustCorporations,omitempty"`
-	Attorneys         []Person   `json:"attorneys,omitempty"`
-	Correspondent     *Person    `json:"correspondent,omitempty"`
+	ID                   int        `json:"id,omitempty"`
+	UID                  string     `json:"uId,omitempty"`
+	Status               string     `json:"status"`
+	CaseType             string     `json:"caseType,omitempty"`
+	SubType              string     `json:"caseSubtype,omitempty"`
+	CancellationDate     DateString `json:"cancellationDate,omitempty"`
+	DispatchDate         DateString `json:"dispatchDate,omitempty"`
+	DueDate              DateString `json:"dueDate,omitempty"`
+	InvalidDate          DateString `json:"invalidDate,omitempty"`
+	PaymentDate          DateString `json:"paymentDate,omitempty"`
+	ReceiptDate          DateString `json:"receiptDate,omitempty"`
+	RegistrationDate     DateString `json:"registrationDate,omitempty"`
+	RejectedDate         DateString `json:"rejectedDate,omitempty"`
+	RevokedDate          DateString `json:"revokedDate,omitempty"`
+	WithdrawnDate        DateString `json:"withdrawnDate,omitempty"`
+	Donor                *Person    `json:"donor,omitempty"`
+	TrustCorporations    []Person   `json:"trustCorporations,omitempty"`
+	Attorneys            []Person   `json:"attorneys,omitempty"`
+	Correspondent        *Person    `json:"correspondent,omitempty"`
+	ExpectedPaymentTotal int        `json:"expectedPaymentTotal"`
 }
 
 func (c Case) Summary() string {
