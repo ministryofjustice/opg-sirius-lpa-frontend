9ae64b092257281554c65481c6fe47c41244909f
nicholassully <nicholas.sully@digital.justice.gov.uk>
2025-09-02 13:41:42 +0100

    VEGA-2673 adding in a tempelate for information warnings

--- cypress/e2e/anomaly.cy.js
+++ cypress/e2e/anomaly.cy.js
@@ -242,7 +242,6 @@ describe("View and edit anomalies for a digital LPA", () => {
     cy.contains("Review certificate provider's address");
   });
 
-
   it("shows anomalies for pa LPA", () => {
     cy.visit("/lpa/M-DIGI-SSSS-3333/lpa-details");
     cy.contains("Some LPA details have been identified for review.");
@@ -250,7 +249,7 @@ describe("View and edit anomalies for a digital LPA", () => {
     cy.contains("Review life sustaining treatment");
   });
 
-  it("shows anomalie for last name matching both donor and at least one attorney", () => {
+  it("shows anomalies for last name matching both donor and at least one attorney", () => {
     cy.visit("/lpa/M-DIGI-TTTT-3333/lpa-details");
     cy.contains(
       "Review last name - this matches the donor and at least one of the attorneys. Check certificate provider's eligibility",
