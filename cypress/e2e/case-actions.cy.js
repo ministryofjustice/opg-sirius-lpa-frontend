fa548304fb45be40ad257770a8c90cfc2cb83ca5
MishNajam <mishkat.najam@digital.justice.gov.uk>
2025-09-26 17:21:43 +0100

    VEGA-3223 split up digital lpa cypress tests into case actions

--- cypress/e2e/case-actions.cy.js
+++ cypress/e2e/case-actions.cy.js
@@ -1,28 +1,65 @@
 import * as digitalLpas from "../mocks/digitalLpas";
-import * as cases from "../mocks/cases";
 
 describe("Case actions drop down", () => {
     beforeEach(() => {
 
         const mocks = Promise.allSettled([
             digitalLpas.get("M-1111-1111-1111"),
-            cases.warnings.empty("1111"),
-            cases.tasks.empty("1111"),
             digitalLpas.objections.empty("M-1111-1111-1111"),
             digitalLpas.progressIndicators.feesInProgress("M-1111-1111-1111"),
         ]);
 
         cy.wrap(mocks);
 
-        cy.addMock("/lpa-api/v1/digital-lpas/M-1111-1111-1111/anomalies", "GET", {
+        cy.addMock(
+            "/lpa-api/v1/cases/1111/tasks?filter=status%3ANot+started%2Cactive%3Atrue&limit=99&sort=duedate%3AASC",
+            "GET",
+            {
+                status: 200,
+                body: {
+                    tasks: [
+                        {
+                            id: 1,
+                            name: "Review restrictions and conditions",
+                            duedate: "10/12/2023",
+                            status: "OPEN",
+                            assignee: { displayName: "Super Team" },
+                        },
+                    ],
+                },
+            },
+        );
+
+        cy.addMock("/lpa-api/v1/persons/1111/cases", "GET", {
             status: 200,
-            body: [],
+            body: {
+                cases: [
+                    {
+                        caseSubtype: "property-and-affairs",
+                        id: 1111,
+                        uId: "M-1111-1111-1111",
+                        status: "Draft",
+                    },
+                ],
+            },
         });
 
-        cy.addMock("/lpa-api/v1/cases/1111/tasks", "GET", {
+        cy.addMock("/lpa-api/v1/tasks/1", "GET", {
             status: 200,
-            body: { tasks: [] },
+            body: {
+                caseItems: [
+                    {
+                        caseType: "DIGITAL_LPA",
+                        uId: "M-1111-1111-1111",
+                    },
+                ],
+                dueDate: "10/01/2022",
+                id: 1,
+                name: "Create physical case file",
+                status: "Not Started",
+            },
         });
+
 
         cy.addMock("/lpa-api/v1/cases/1111/tasks", "POST", {
             status: 201,
@@ -43,22 +80,34 @@ describe("Case actions drop down", () => {
             },
         });
 
-        cy.addMock("/lpa-api/v1/cases/1111/payments", "GET", {
+        cy.addMock("/lpa-api/v1/cases/1111/warnings", "GET", {
             status: 200,
             body: [
                 {
-                    amount: 4100,
-                    case: {
-                        id: 1111,
-                    },
-                    id: 1234,
-                    paymentDate: "12/02/2025",
-                    source: "ONLINE",
-                    locked: true,
+                    id: 44,
+                    warningType: "Court application in progress",
+                    warningText: "Court notified",
+                    dateAdded: "24/08/2022 13:13:13",
+                    caseItems: [
+                        { uId: "M-1111-1111-1111", caseSubtype: "property-and-affairs" },
+                    ],
                 },
             ],
         });
 
+        cy.addMock("/lpa-api/v1/warnings", "POST", {
+            status: 201,
+            body: {
+                personId: 1111,
+                warningText: "Be warned!",
+                warningType: "Complaint Received",
+                caseIds: [1111],
+            },
+        });
+
+        cy.addMock("/lpa-api/v1/warnings", "POST", {
+            status: 201,
+        });
 
         cy.visit("/lpa/M-1111-1111-1111");
     });
@@ -82,15 +131,77 @@ describe("Case actions drop down", () => {
         cy.location("pathname").should("eq", "/lpa/M-1111-1111-1111");
     });
 
-    it("creates a warning via case actions", () => {
-        cy.addMock("/lpa-api/v1/warnings", "POST", {
-            status: 201,
-            body: {
-                personId: 1111,
-                warningText: "Be warned!",
-                warningType: "Complaint Received",
-            },
+    it("can cancel reassign task", () => {
+        cy.contains("Reassign task").click();
+        cy.url().should("include", "/assign-task?id=1");
+        cy.contains("Assign Task");
+
+        cy.contains("Cancel").click();
+        cy.url().should("include", "/lpa/M-1111-1111-1111");
+        cy.contains("Case summary");
+    });
+
+    it("can cancel clear task", () => {
+        cy.contains("Clear task").click();
+        cy.url().should("include", "/clear-task?id=1");
+        cy.contains("Save and clear task");
+
+        cy.contains("Cancel").click();
+        cy.url().should("include", "/lpa/M-1111-1111-1111");
+        cy.contains("Case summary");
+    });
+
+    it("can clear a task", () => {
+        cy.addMock("/lpa-api/v1/tasks/1/mark-as-completed", "PUT", {
+            status: 200,
         });
+
+        cy.contains("Clear task").click();
+        cy.url().should("include", "/clear-task?id=1");
+        cy.get("button[type=submit]").click();
+
+        cy.get(".moj-alert").should("exist");
+        cy.get(".moj-alert").contains("Task completed");
+
+        cy.url().should("contain", "/lpa/M-1111-1111-1111");
+        cy.contains("Case summary");
+    });
+
+    it("can cancel changing the status", () => {
+        cy.addMock("/lpa-api/v1/reference-data/caseChangeReason", "GET", {
+            status: 200,
+            body: [
+                {
+                    handle: "LPA_DOES_NOT_WORK",
+                    label: "The LPA does not work and cannot be changed",
+                    parentSources: ["cannot-register"],
+                },
+            ],
+        });
+        cy.contains("Case actions").click();
+        cy.contains("Change case status").click();
+
+        cy.url().should("include", "/change-case-status?uid=M-1111-1111-1111");
+        cy.contains("Change case status");
+        cy.get(".govuk-button-group").contains("Cancel").click();
+
+        cy.url().should("include", "/lpa/M-1111-1111-1111");
+        cy.contains("Case summary");
+    });
+
+    it("can cancel creating a warning", () => {
+        cy.contains("Case actions").click();
+        cy.contains("Create a warning").click();
+
+        cy.url().should("include", "/create-warning?id=1111");
+        cy.contains("Create Warning");
+        cy.contains("Cancel").click();
+
+        cy.url().should("include", "/lpa/M-1111-1111-1111");
+        cy.contains("Case summary");
+    });
+
+    it("creates a warning via case actions", () => {
         cy.contains(".govuk-button", "Case actions").click();
         cy.contains("Create a warning").click();
         cy.url().should("include", "/create-warning?id=1111");
@@ -100,7 +211,7 @@ describe("Case actions drop down", () => {
 
         cy.get(".moj-alert").should("exist");
         cy.get(".moj-alert").contains("Warning created");
-        cy.get("h1").contains("Agnes Hartley");
+        cy.get("h1").contains("Steven Munnell");
         cy.location("pathname").should("eq", "/lpa/M-1111-1111-1111");
     });
 });
