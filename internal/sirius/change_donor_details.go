436c4decb84576971966efe984b8ec3d85b8ea2a
Nick Pyett <contact@nickpyett.com>
2025-10-08 16:07:43 +0100

    Add fields for detail of person who signed on behalf of donor

--- internal/sirius/change_donor_details.go
+++ internal/sirius/change_donor_details.go
@@ -2,17 +2,23 @@ package sirius
 
 import (
 	"fmt"
+	"time"
 )
 
 type ChangeDonorDetails struct {
-	FirstNames        string     `json:"firstNames"`
-	LastName          string     `json:"lastName"`
-	OtherNamesKnownBy string     `json:"otherNamesKnownBy"`
-	DateOfBirth       DateString `json:"dateOfBirth"`
-	Address           Address    `json:"address"`
-	Phone             string     `json:"phoneNumber"`
-	Email             string     `json:"email"`
-	LpaSignedOn       DateString `json:"lpaSignedOn"`
+	FirstNames                       string     `json:"firstNames"`
+	LastName                         string     `json:"lastName"`
+	OtherNamesKnownBy                string     `json:"otherNamesKnownBy"`
+	DateOfBirth                      DateString `json:"dateOfBirth"`
+	Address                          Address    `json:"address"`
+	Phone                            string     `json:"phoneNumber"`
+	Email                            string     `json:"email"`
+	LpaSignedOn                      DateString `json:"lpaSignedOn"`
+	AuthorisedSignatory              string     `form:"authorisedSignatory"`
+	WitnessedByCertificateProviderAt time.Time  `form:"witnessedByCertificateProviderAt"`
+	WitnessedByIndependentWitnessAt  *time.Time `form:"witnessedByIndependentWitnessAt"`
+	IndependentWitnessName           string     `form:"independentWitnessName"`
+	IndependentWitnessAddress        Address    `form:"independentWitnessAddress"`
 }
 
 func (c *Client) ChangeDonorDetails(ctx Context, caseUID string, donorDetails ChangeDonorDetails) error {
