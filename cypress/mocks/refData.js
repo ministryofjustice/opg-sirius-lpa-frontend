0c59f478b54f54bd5dbf0904d4bd282bacd70e0f
Nick Pyett <contact@nickpyett.com>
2025-10-17 16:59:58 +0100

    Add mock for refdata

---
+++ cypress/mocks/refData.js
@@ -0,0 +1,28 @@
+import { addMock } from "./wiremock";
+
+const countries = {
+  async get(countriesArray, priority = 1) {
+    await addMock(
+      `/lpa-api/v1/reference-data/country`,
+      "GET",
+      {
+        status: 200,
+        body: countriesArray,
+      },
+      priority,
+    );
+  },
+  async gbOnly(priority = 1) {
+    await addMock(
+      `/lpa-api/v1/reference-data/country`,
+      "GET",
+      {
+        status: 200,
+        body: [{ handle: "GB", label : "Great Britain" }],
+      },
+      priority,
+    );
+  },
+};
+
+export { countries };
