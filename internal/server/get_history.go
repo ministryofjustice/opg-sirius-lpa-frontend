e54a9e6d41ff45d6440c9340774c5c01e8aae8c1
nicholassully <nicholas.sully@digital.justice.gov.uk>
2025-10-20 14:30:18 +0100

    VEGA-3332 adding uuid to event and added type

--- internal/server/get_history.go
+++ internal/server/get_history.go
@@ -1,7 +1,12 @@
 package server
 
 import (
+	"encoding/base32"
+	"encoding/hex"
+	"fmt"
+	"log"
 	"net/http"
+	"strings"
 
 	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/sirius"
 
@@ -10,7 +15,7 @@ import (
 
 type GetHistoryClient interface {
 	GetEvents(ctx sirius.Context, donorId int, caseId int) (any, error)
-	GetCombinedEvents(ctx sirius.Context, uid string) (any, error)
+	GetCombinedEvents(ctx sirius.Context, uid string) (sirius.APIEvent, error)
 	CaseSummary(ctx sirius.Context, uid string) (sirius.CaseSummary, error)
 }
 
@@ -29,16 +34,25 @@ func GetHistory(client GetHistoryClient, tmpl template.Template) Handler {
 			return err
 		}
 
-		var eventDetails any
+		var eventDetails sirius.APIEvent
+		var traditionalEventDetails any
 
 		if caseSummary.DigitalLpa.LpaStoreData.Status.ReadableString() != "" {
 			// Digital LPA - use combined events
 			eventDetails, err = client.GetCombinedEvents(ctx, uid)
+			for i := range eventDetails {
+				formattedUUID, err := LPAEventIDFromUUID(eventDetails[i].UUID)
+				if err != nil {
+					log.Println("Error generating formattedUUID:", err)
+					continue
+				}
+				eventDetails[i].FormattedUUID = formattedUUID
+			}
 		} else {
 			// Traditional LPA - use Sirius events only
 			donorId := caseSummary.DigitalLpa.SiriusData.Donor.ID
 			caseId := caseSummary.DigitalLpa.SiriusData.ID
-			eventDetails, err = client.GetEvents(ctx, donorId, caseId)
+			traditionalEventDetails, err = client.GetEvents(ctx, donorId, caseId)
 		}
 
 		if err != nil {
@@ -47,9 +61,30 @@ func GetHistory(client GetHistoryClient, tmpl template.Template) Handler {
 
 		data := getHistory{
 			CaseSummary: caseSummary,
-			EventData:   eventDetails,
+		}
+		if eventDetails != nil {
+			data.EventData = eventDetails
+		} else if traditionalEventDetails != nil {
+			data.EventData = traditionalEventDetails
 		}
 
 		return tmpl(w, data)
 	}
+}
+
+func LPAEventIDFromUUID(uuidStr string) (string, error) {
+	clean := strings.ReplaceAll(uuidStr, "-", "")
+
+	idBytes, err := hex.DecodeString(clean)
+	if err != nil {
+		return "", err
+	}
+
+	encoder := base32.StdEncoding.WithPadding(base32.NoPadding)
+	base32Str := encoder.EncodeToString(idBytes)
+
+	if len(base32Str) < 8 {
+		return "", fmt.Errorf("unexpected Base32 length for UUID")
+	}
+	return base32Str[:8], nil
 }
