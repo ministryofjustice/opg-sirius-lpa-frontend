8c56b0c5424826c47fafc687771298108584e573
MishNajam <mishkat.najam@digital.justice.gov.uk>
2025-11-26 08:46:29 +0000

    uncomment pact test

--- internal/sirius/apply_fee_reduction_test.go
+++ internal/sirius/apply_fee_reduction_test.go
@@ -1,60 +1,71 @@
 package sirius
 
-//func TestApplyFeeReduction(t *testing.T) {
-//	t.Parallel()
-//
-//	pact, err := newPact()
-//	assert.NoError(t, err)
-//
-//	testCases := []struct {
-//		name          string
-//		setup         func()
-//		expectedError func(int) error
-//	}{
-//		{
-//			name: "OK",
-//			setup: func() {
-//				pact.
-//					AddInteraction().
-//					Given("I have a pending case with no payment assigned").
-//					UponReceiving("A request to create a fee reduction").
-//					WithCompleteRequest(consumer.Request{
-//						Method: http.MethodPost,
-//						Path:   matchers.String("/lpa-api/v1/cases/801/payments/reduction"),
-//						Headers: matchers.MapMatcher{
-//							"Content-Type": matchers.String("application/json"),
-//						},
-//						Body: map[string]interface{}{
-//							"feeReductionType": "REMISSION",
-//							"source":           FeeReductionSource,
-//							"paymentDate":      "25/04/2022",
-//							"paymentEvidence":  "Test evidence",
-//						},
-//					}).
-//					WithCompleteResponse(consumer.Response{
-//						Status:  http.StatusCreated,
-//						Headers: matchers.MapMatcher{"Content-Type": matchers.String("application/json")},
-//					})
-//			},
-//		},
-//	}
-//
-//	for _, tc := range testCases {
-//		t.Run(tc.name, func(t *testing.T) {
-//			tc.setup()
-//
-//			assert.Nil(t, pact.ExecuteTest(t, func(config consumer.MockServerConfig) error {
-//				client := NewClient(http.DefaultClient, fmt.Sprintf("http://127.0.0.1:%d", config.Port))
-//
-//				err := client.ApplyFeeReduction(Context{Context: context.Background()}, 801, "REMISSION", "Test evidence", DateString("2022-04-25"))
-//
-//				if tc.expectedError == nil {
-//					assert.Nil(t, err)
-//				} else {
-//					assert.Equal(t, tc.expectedError(config.Port), err)
-//				}
-//				return nil
-//			}))
-//		})
-//	}
-//}
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/pact-foundation/pact-go/v2/consumer"
+	"github.com/pact-foundation/pact-go/v2/matchers"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestApplyFeeReduction(t *testing.T) {
+	t.Parallel()
+
+	pact, err := newPact()
+	assert.NoError(t, err)
+
+	testCases := []struct {
+		name          string
+		setup         func()
+		expectedError func(int) error
+	}{
+		{
+			name: "OK",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("I have a pending case with no payment assigned").
+					UponReceiving("A request to create a fee reduction").
+					WithCompleteRequest(consumer.Request{
+						Method: http.MethodPost,
+						Path:   matchers.String("/lpa-api/v1/cases/801/payments/reduction"),
+						Headers: matchers.MapMatcher{
+							"Content-Type": matchers.String("application/json"),
+						},
+						Body: map[string]interface{}{
+							"feeReductionType": "REMISSION",
+							"source":           FeeReductionSource,
+							"paymentDate":      "25/04/2022",
+							"paymentEvidence":  "Test evidence",
+						},
+					}).
+					WithCompleteResponse(consumer.Response{
+						Status:  http.StatusCreated,
+						Headers: matchers.MapMatcher{"Content-Type": matchers.String("application/json")},
+					})
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup()
+
+			assert.Nil(t, pact.ExecuteTest(t, func(config consumer.MockServerConfig) error {
+				client := NewClient(http.DefaultClient, fmt.Sprintf("http://127.0.0.1:%d", config.Port))
+
+				err := client.ApplyFeeReduction(Context{Context: context.Background()}, 801, "REMISSION", "Test evidence", DateString("2022-04-25"))
+
+				if tc.expectedError == nil {
+					assert.Nil(t, err)
+				} else {
+					assert.Equal(t, tc.expectedError(config.Port), err)
+				}
+				return nil
+			}))
+		})
+	}
+}
--- internal/sirius/payment_test.go
+++ internal/sirius/payment_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/shared"
 	"github.com/pact-foundation/pact-go/v2/consumer"
 	"github.com/pact-foundation/pact-go/v2/matchers"
 	"github.com/stretchr/testify/assert"
@@ -467,82 +468,82 @@ func TestEditPayment(t *testing.T) {
 	}
 }
 
-//func TestEditFeeReduction(t *testing.T) {
-//	t.Parallel()
-//
-//	pact, err := newPact()
-//	assert.NoError(t, err)
-//
-//	testCases := []struct {
-//		name          string
-//		setup         func()
-//		expectedError func(int) error
-//	}{
-//		{
-//			name: "OK",
-//			setup: func() {
-//				pact.
-//					AddInteraction().
-//					Given("I have an lpa which has a fee reduction").
-//					UponReceiving("A request to edit a fee reduction").
-//					WithCompleteRequest(consumer.Request{
-//						Method: http.MethodPut,
-//						Path:   matchers.String("/lpa-api/v1/payments/124/reduction"),
-//						Headers: matchers.MapMatcher{
-//							"Content-Type": matchers.String("application/json"),
-//						},
-//						Body: map[string]interface{}{
-//							"id":     124,
-//							"amount": 4100,
-//							"case": map[string]interface{}{
-//								"id":                   802,
-//								"status":               "Draft",
-//								"expectedPaymentTotal": 8000,
-//							},
-//							"paymentEvidence":  matchers.String("Edited test evidence"),
-//							"feeReductionType": matchers.String("REMISSION"),
-//							"paymentDate":      matchers.Term("28/04/2022", `^\d{1,2}/\d{1,2}/\d{4}$`),
-//							"source":           matchers.String(FeeReductionSource),
-//						},
-//					}).
-//					WithCompleteResponse(consumer.Response{
-//						Status:  http.StatusOK,
-//						Headers: matchers.MapMatcher{"Content-Type": matchers.String("application/json")},
-//					})
-//			},
-//		},
-//	}
-//
-//	for _, tc := range testCases {
-//		t.Run(tc.name, func(t *testing.T) {
-//			tc.setup()
-//
-//			assert.Nil(t, pact.ExecuteTest(t, func(config consumer.MockServerConfig) error {
-//				client := NewClient(http.DefaultClient, fmt.Sprintf("http://127.0.0.1:%d", config.Port))
-//
-//				err := client.EditPayment(Context{Context: context.Background()},
-//					124,
-//					Payment{
-//						ID:               124,
-//						Amount:           4100,
-//						Case:             &Case{ID: 802, Status: shared.CaseStatusTypeDraft, ExpectedPaymentTotal: 8000},
-//						PaymentEvidence:  "Edited test evidence",
-//						FeeReductionType: "REMISSION",
-//						PaymentDate:      "2022-04-28",
-//						Source:           FeeReductionSource,
-//					},
-//				)
-//
-//				if tc.expectedError == nil {
-//					assert.Nil(t, err)
-//				} else {
-//					assert.Equal(t, tc.expectedError(config.Port), err)
-//				}
-//				return nil
-//			}))
-//		})
-//	}
-//}
+func TestEditFeeReduction(t *testing.T) {
+	t.Parallel()
+
+	pact, err := newPact()
+	assert.NoError(t, err)
+
+	testCases := []struct {
+		name          string
+		setup         func()
+		expectedError func(int) error
+	}{
+		{
+			name: "OK",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("I have an lpa which has a fee reduction").
+					UponReceiving("A request to edit a fee reduction").
+					WithCompleteRequest(consumer.Request{
+						Method: http.MethodPut,
+						Path:   matchers.String("/lpa-api/v1/payments/124/reduction"),
+						Headers: matchers.MapMatcher{
+							"Content-Type": matchers.String("application/json"),
+						},
+						Body: map[string]interface{}{
+							"id":     124,
+							"amount": 4100,
+							"case": map[string]interface{}{
+								"id":                   802,
+								"status":               "Draft",
+								"expectedPaymentTotal": 8000,
+							},
+							"paymentEvidence":  matchers.String("Edited test evidence"),
+							"feeReductionType": matchers.String("REMISSION"),
+							"paymentDate":      matchers.Term("28/04/2022", `^\d{1,2}/\d{1,2}/\d{4}$`),
+							"source":           matchers.String(FeeReductionSource),
+						},
+					}).
+					WithCompleteResponse(consumer.Response{
+						Status:  http.StatusOK,
+						Headers: matchers.MapMatcher{"Content-Type": matchers.String("application/json")},
+					})
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup()
+
+			assert.Nil(t, pact.ExecuteTest(t, func(config consumer.MockServerConfig) error {
+				client := NewClient(http.DefaultClient, fmt.Sprintf("http://127.0.0.1:%d", config.Port))
+
+				err := client.EditPayment(Context{Context: context.Background()},
+					124,
+					Payment{
+						ID:               124,
+						Amount:           4100,
+						Case:             &Case{ID: 802, Status: shared.CaseStatusTypeDraft, ExpectedPaymentTotal: 8000},
+						PaymentEvidence:  "Edited test evidence",
+						FeeReductionType: "REMISSION",
+						PaymentDate:      "2022-04-28",
+						Source:           FeeReductionSource,
+					},
+				)
+
+				if tc.expectedError == nil {
+					assert.Nil(t, err)
+				} else {
+					assert.Equal(t, tc.expectedError(config.Port), err)
+				}
+				return nil
+			}))
+		})
+	}
+}
 
 func TestDeletePayment(t *testing.T) {
 	t.Parallel()
