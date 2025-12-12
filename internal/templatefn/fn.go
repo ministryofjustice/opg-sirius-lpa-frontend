4a17a1faae9d8b0dbf6f87284c386891af3a81fc
MishNajam <mishkat.najam@digital.justice.gov.uk>
2025-12-12 16:51:54 +0000

    move case label to enum

--- internal/templatefn/fn.go
+++ internal/templatefn/fn.go
@@ -302,20 +302,6 @@ func All(siriusPublicURL, prefix, staticHash string) map[string]interface{} {
 				return "resolution outcome NOT RECOGNISED: " + s
 			}
 		},
-		"caseLabel": func(s string) string {
-			switch s {
-			case "EPA":
-				return "colour-govuk-brown"
-			case "pfa":
-				return "colour-govuk-turquoise"
-			case "hw":
-				return "colour-govuk-grass-green"
-			case "":
-				return "Not specified"
-			default:
-				return "case label colour NOT RECOGNISED: " + s
-			}
-		},
 		"compareBoolPointers": func(i *bool, j bool) bool {
 			return *i == j
 		},
