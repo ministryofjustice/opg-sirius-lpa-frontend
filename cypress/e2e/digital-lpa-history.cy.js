ec25a350df6378075b2f7aae217aefa5bcc245af
Nick Pyett <contact@nickpyett.com>
2025-12-17 15:22:50 +0000

    Lint fix

--- cypress/e2e/digital-lpa-history.cy.js
+++ cypress/e2e/digital-lpa-history.cy.js
@@ -6,161 +6,177 @@ describe("View digital LPA history", () => {
     const uid = "M-1234-1234-1234";
     const id = "1111";
 
-    const events = [{
-      "id":"607631d0-586a-40a5-ae5f-cc9121b6eef1",
-      "uid":"M-1234-1234-1234",
-      "applied":"2025-12-16T12:06:50Z",
-      "author":"urn:opg:sirius:users:1",
-      "type":"CORRECTION",
-      "source":"lpa_store",
-      "changes":[{
-        "key":"/donor/firstNames",
-        "old":"Anne",
-        "new":"Anna"
-      },{
-        "key":"/donor/address/postcode",
-        "old":"M1 1AB",
-        "new":"M1 1AC"
-      }]
-    },{
-      "id":"43e3b449-909a-44a8-9fc9-37c90c3aab4c",
-      "uid":"M-1234-1234-1234",
-      "applied":"2025-12-16T12:19:16Z",
-      "author":"urn:opg:sirius:users:1",
-      "type":"CORRECTION",
-      "source":"lpa_store",
-      "changes":[{
-        "key":"/attorneys/0/lastName",
-        "old":"Dent",
-        "new":"Branch"
-      },{
-        "key":"/attorneys/0/address/line1",
-        "old":"1 Oak Road",
-        "new":"1 Willow Avenue"
-      },{
-        "key":"/attorneys/0/mobile",
-        "old":"07123 456789",
-        "new":"07123 456710"
-      }]
-    },{
-      "uuid": "c4b4dff2-e2e8-4b71-af0a-9fabdf648edc",
-      "owningCase": {
-        "id": 111,
-        "uId": "M-NN8A-XMHL-GF69",
-        "caseSubtype": "personal-welfare",
-        "caseType": "DIGITAL_LPA"
+    const events = [
+      {
+        id: "607631d0-586a-40a5-ae5f-cc9121b6eef1",
+        uid: "M-1234-1234-1234",
+        applied: "2025-12-16T12:06:50Z",
+        author: "urn:opg:sirius:users:1",
+        type: "CORRECTION",
+        source: "lpa_store",
+        changes: [
+          {
+            key: "/donor/firstNames",
+            old: "Anne",
+            new: "Anna",
+          },
+          {
+            key: "/donor/address/postcode",
+            old: "M1 1AB",
+            new: "M1 1AC",
+          },
+        ],
       },
-      "user": {
-        "id": 51,
-        "phoneNumber": "12345678",
-        "teams": [],
-        "displayName": "system admin",
-        "deleted": false,
-        "email": "system.admin@opgtest.com"
+      {
+        id: "43e3b449-909a-44a8-9fc9-37c90c3aab4c",
+        uid: "M-1234-1234-1234",
+        applied: "2025-12-16T12:19:16Z",
+        author: "urn:opg:sirius:users:1",
+        type: "CORRECTION",
+        source: "lpa_store",
+        changes: [
+          {
+            key: "/attorneys/0/lastName",
+            old: "Dent",
+            new: "Branch",
+          },
+          {
+            key: "/attorneys/0/address/line1",
+            old: "1 Oak Road",
+            new: "1 Willow Avenue",
+          },
+          {
+            key: "/attorneys/0/mobile",
+            old: "07123 456789",
+            new: "07123 456710",
+          },
+        ],
       },
-      "sourceType": "OutgoingDocument",
-      "sourceDocument": {
-        "id": 9,
-        "uuid": "ac488874-4e74-49f3-bc02-d95741b96aa3",
-        "friendlyDescription": "Anne Barlow - TEMP - Digital LPA Registration Form",
-        "createdDate": "17\/12\/2025 09:27:12",
-        "filename": "DLPA-form.pdf",
-        "mimeType": "application\/pdf"
-      },
-      "type": "INS",
-      "changeSet": [],
-      "entity": {
-        "_class": "Opg\\Core\\Model\\Entity\\Document\\OutgoingDocument",
-        "correspondent": {
-          "firstname": "Anne",
-          "surname": "Barlow"
+      {
+        uuid: "c4b4dff2-e2e8-4b71-af0a-9fabdf648edc",
+        owningCase: {
+          id: 111,
+          uId: "M-NN8A-XMHL-GF69",
+          caseSubtype: "personal-welfare",
+          caseType: "DIGITAL_LPA",
         },
-        "direction": "OUTGOING",
-        "friendlyDescription": "Anne Barlow - TEMP - Digital LPA Registration Form",
-        "id": 9,
-        "type": "Save",
-        "subType": ""
-      },
-      "createdOn": "2025-12-17T09:27:13+00:00",
-      "hash": "IK",
-      "source": "sirius"
-    },{
-      "uuid": "055679b3-9874-43f1-9c8c-fd0851c0aa55",
-      "owningCase": {
-        "id": 111,
-        "uId": "M-NN8A-XMHL-GF69",
-        "caseSubtype": "personal-welfare",
-        "caseType": "DIGITAL_LPA"
-      },
-      "user": {
-        "id": 51,
-        "phoneNumber": "12345678",
-        "teams": [],
-        "displayName": "system admin",
-        "deleted": false,
-        "email": "system.admin@opgtest.com"
-      },
-      "sourceType": "Task",
-      "sourceTask": {
-        "id": 251,
-        "assignee": {
-          "id": 0,
-          "phoneNumber": "03004560300",
-          "displayName": "Unassigned",
-          "deleted": false
-        }
-      },
-      "type": "INS",
-      "changeSet": [],
-      "entity": {
-        "_class": "Opg\\Core\\Model\\Entity\\Task\\Task",
-        "assignee": {
-          "displayName": "Unassigned"
+        user: {
+          id: 51,
+          phoneNumber: "12345678",
+          teams: [],
+          displayName: "system admin",
+          deleted: false,
+          email: "system.admin@opgtest.com",
         },
-        "name": "Print and post donor form"
+        sourceType: "OutgoingDocument",
+        sourceDocument: {
+          id: 9,
+          uuid: "ac488874-4e74-49f3-bc02-d95741b96aa3",
+          friendlyDescription:
+            "Anne Barlow - TEMP - Digital LPA Registration Form",
+          createdDate: "17\/12\/2025 09:27:12",
+          filename: "DLPA-form.pdf",
+          mimeType: "application\/pdf",
+        },
+        type: "INS",
+        changeSet: [],
+        entity: {
+          _class: "Opg\\Core\\Model\\Entity\\Document\\OutgoingDocument",
+          correspondent: {
+            firstname: "Anne",
+            surname: "Barlow",
+          },
+          direction: "OUTGOING",
+          friendlyDescription:
+            "Anne Barlow - TEMP - Digital LPA Registration Form",
+          id: 9,
+          type: "Save",
+          subType: "",
+        },
+        createdOn: "2025-12-17T09:27:13+00:00",
+        hash: "IK",
+        source: "sirius",
       },
-      "createdOn": "2025-12-17T09:27:13+00:00",
-      "hash": "II",
-      "source": "sirius"
-    },{
-      "uuid": "458ef72a-1d43-4c69-8c6c-dac2d138d1d3",
-      "owningCase": {
-        "id": 111,
-        "uId": "M-NN8A-XMHL-GF69",
-        "caseSubtype": "personal-welfare",
-        "caseType": "DIGITAL_LPA"
+      {
+        uuid: "055679b3-9874-43f1-9c8c-fd0851c0aa55",
+        owningCase: {
+          id: 111,
+          uId: "M-NN8A-XMHL-GF69",
+          caseSubtype: "personal-welfare",
+          caseType: "DIGITAL_LPA",
+        },
+        user: {
+          id: 51,
+          phoneNumber: "12345678",
+          teams: [],
+          displayName: "system admin",
+          deleted: false,
+          email: "system.admin@opgtest.com",
+        },
+        sourceType: "Task",
+        sourceTask: {
+          id: 251,
+          assignee: {
+            id: 0,
+            phoneNumber: "03004560300",
+            displayName: "Unassigned",
+            deleted: false,
+          },
+        },
+        type: "INS",
+        changeSet: [],
+        entity: {
+          _class: "Opg\\Core\\Model\\Entity\\Task\\Task",
+          assignee: {
+            displayName: "Unassigned",
+          },
+          name: "Print and post donor form",
+        },
+        createdOn: "2025-12-17T09:27:13+00:00",
+        hash: "II",
+        source: "sirius",
       },
-      "user": {
-        "id": 51,
-        "phoneNumber": "12345678",
-        "teams": [],
-        "displayName": "system admin",
-        "deleted": false,
-        "email": "system.admin@opgtest.com"
+      {
+        uuid: "458ef72a-1d43-4c69-8c6c-dac2d138d1d3",
+        owningCase: {
+          id: 111,
+          uId: "M-NN8A-XMHL-GF69",
+          caseSubtype: "personal-welfare",
+          caseType: "DIGITAL_LPA",
+        },
+        user: {
+          id: 51,
+          phoneNumber: "12345678",
+          teams: [],
+          displayName: "system admin",
+          deleted: false,
+          email: "system.admin@opgtest.com",
+        },
+        sourceType: "DigitalLpa",
+        sourceCase: {
+          id: 111,
+          uId: "M-NN8A-XMHL-GF69",
+          caseSubtype: "personal-welfare",
+          caseType: "DIGITAL_LPA",
+        },
+        type: "INS",
+        changeSet: [],
+        entity: {
+          _class:
+            "Opg\\Core\\Model\\Entity\\CaseItem\\PowerOfAttorney\\DigitalLpa",
+        },
+        createdOn: "2025-12-17T09:27:13+00:00",
+        hash: "IE",
+        source: "sirius",
       },
-      "sourceType": "DigitalLpa",
-      "sourceCase": {
-        "id": 111,
-        "uId": "M-NN8A-XMHL-GF69",
-        "caseSubtype": "personal-welfare",
-        "caseType": "DIGITAL_LPA"
-      },
-      "type": "INS",
-      "changeSet": [],
-      "entity": {
-        "_class": "Opg\\Core\\Model\\Entity\\CaseItem\\PowerOfAttorney\\DigitalLpa"
-      },
-      "createdOn": "2025-12-17T09:27:13+00:00",
-      "hash": "IE",
-      "source": "sirius"
-    }];
+    ];
 
     const mocks = Promise.allSettled([
       digitalLpas.get(uid),
       cases.warnings.empty(id),
       cases.tasks.empty(id),
       digitalLpas.objections.empty(uid),
-      digitalLpas.events.get(uid, events)
+      digitalLpas.events.get(uid, events),
     ]);
 
     cy.wrap(mocks);
@@ -185,5 +201,3 @@ describe("View digital LPA history", () => {
     cy.contains("Name: Print and post donor form");
   });
 });
-
-
