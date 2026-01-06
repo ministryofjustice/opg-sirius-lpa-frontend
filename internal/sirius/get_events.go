d8310e7b58ee1d5ac2548ed04227da28124f8306
Nick Pyett <contact@nickpyett.com>
2026-01-06 12:43:43 +0000

    Revert "VEGA-3469 LPA history category and content (#800)" (#814)
    This reverts commit 0b7ea0225ff2d1246a09145c96573a950ce14f0f.

--- internal/sirius/get_events.go
+++ internal/sirius/get_events.go
@@ -2,31 +2,30 @@ package sirius
 
 import (
 	"fmt"
-
-	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/shared"
 )
 
 type APIEvents []Event
 
 type Event struct {
-	Changes             []shared.LpaStoreChange `json:"changes"`
-	CreatedOn           string                  `json:"createdOn"`
-	Entity              any                     `json:"entity"`
-	Hash                string                  `json:"hash"`
-	ID                  string                  `json:"id"`
-	Source              string                  `json:"source"`
-	SourceType          string                  `json:"sourceType"`
-	Type                string                  `json:"type"`
-	User                EventUser               `json:"user"`
-	UUID                string                  `json:"uuid"`
-	Applied             string                  `json:"applied,omitempty"`
-	DateTime            string                  `json:"dateTime,omitempty"`
+	Changes             []LpaStoreEvent `json:"changes"`
+	CreatedOn           string          `json:"createdOn"`
+	Entity              any             `json:"entity"`
+	Hash                string          `json:"hash"`
+	ID                  string          `json:"id"`
+	Source              string          `json:"source"`
+	SourceType          string          `json:"sourceType"`
+	Type                string          `json:"type"`
+	User                EventUser       `json:"user"`
+	UUID                string          `json:"uuid"`
+	Applied             string          `json:"applied,omitempty"`
+	DateTime            string          `json:"dateTime,omitempty"`
 	FormattedLpaStoreId string
-	Category            string
 }
 
-func (e Event) IsLpaStore() bool {
-	return e.Source == "lpa_store"
+type LpaStoreEvent struct {
+	Key string `json:"key"`
+	New any    `json:"new"`
+	Old any    `json:"old"`
 }
 
 type EventUser struct {
