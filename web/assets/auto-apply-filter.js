5d108719669fdb8184283284a6f20c8bfe01c66a
Nick Pyett <contact@nickpyett.com>
2026-01-06 11:45:26 +0000

    Hide search page filter button with js-enabled CSS
    Signed-off-by: Nick Pyett <contact@nickpyett.com>

--- web/assets/auto-apply-filter.js
+++ web/assets/auto-apply-filter.js
@@ -1,12 +1,4 @@
 export default function autoApplyFilter() {
-  if (document.body.className.includes("js-enabled")) {
-    if (document.querySelector(".moj-filter")) {
-      document
-        .querySelector(".moj-filter form button")
-        .classList.add("govuk-!-display-none");
-    }
-  }
-
   const filters = document.querySelectorAll(
     '[data-module="app-auto-apply-filter"]',
   );
