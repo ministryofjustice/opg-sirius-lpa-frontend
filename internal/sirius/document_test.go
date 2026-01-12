3d49c1b41216514c8fbe39187d58e2d43a5c6261
David Bridgwood <david.bridgwood@digital.justice.gov.uk>
2026-01-12 12:18:07 +0000

    VEGA-3429: update pact-test

--- internal/sirius/document_test.go
+++ internal/sirius/document_test.go
@@ -394,9 +394,9 @@ func TestDownloadMultiple(t *testing.T) {
 					}).
 					WithCompleteResponse(consumer.Response{
 						Status: http.StatusOK,
-						Body:   matchers.String("this is a pdf"),
+						Body:   matchers.Term("fake-binary-file", ".+"),
 						Headers: matchers.MapMatcher{
-							"Content-Type": matchers.String("application/pdf"),
+							"Content-Type": matchers.String("application/octet-stream"),
 						},
 					})
 			},
@@ -406,11 +406,11 @@ func TestDownloadMultiple(t *testing.T) {
 				}
 
 				assert.Equal(t, http.StatusOK, resp.StatusCode)
-				assert.Equal(t, "application/pdf", resp.Header.Get("Content-Type"))
+				assert.Equal(t, "application/octet-stream", resp.Header.Get("Content-Type"))
 
 				body, readErr := io.ReadAll(resp.Body)
 				assert.NoError(t, readErr)
-				assert.Equal(t, "this is a pdf", string(body))
+				assert.True(t, len(body) > 0)
 			},
 		},
 		{
