1dae5b922f315fdaaec3dbdb5f7df2a15bc43e2a
Nick Pyett <contact@nickpyett.com>
2026-01-13 15:36:50 +0000

    Log Wiremock mappings to console when Cypress test fails

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,5 +1,26 @@
 import "./commands";
+import { mappings } from "../mocks/wiremock"
 
 afterEach(() => {
   cy.resetMocks();
 });
+
+Cypress.on('fail', (error, runnable) => {
+  debugger
+
+  let wiremockMappings = mappings();
+
+  wiremockMappings.then((response) => response.json())
+    .then((json) => {
+      let output = "Wiremock mappings: \n\n"
+
+      json.mappings.forEach((mapping) => {
+        output += mapping.request.method + " " + mapping.request.url + " " + mapping.response.status + "\n"
+        output += mapping.response.body + "\n\n"
+      })
+
+      console.log(output)
+    })
+
+  throw error
+})
