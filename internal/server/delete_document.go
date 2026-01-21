36eb47c8ed30ead23b2fc2ba0b421b20fa578cd9
nicholassully <nicholas.sully@digital.justice.gov.uk>
2026-01-21 09:12:08 +0000

    VEGA-3033 delete a doc with notification

--- internal/server/delete_document.go
+++ internal/server/delete_document.go
@@ -1,24 +1,27 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/ministryofjustice/opg-go-common/template"
 	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/sirius"
 )
 
-type ViewDocumentClient interface {
+type DeleteDocumentClient interface {
 	DocumentByUUID(ctx sirius.Context, uuid string) (sirius.Document, error)
 	GetUserDetails(sirius.Context) (sirius.User, error)
+	DeleteDocument(ctx sirius.Context, uuid string) error
 }
 
-type viewDocumentData struct {
+type deleteDocumentData struct {
 	XSRFToken      string
 	Document       sirius.Document
 	IsSysAdminUser bool
+	DonorId        int
 }
 
-func ViewDocument(client ViewDocumentClient, tmpl template.Template) Handler {
+func DeleteDocument(client DeleteDocumentClient, tmpl template.Template) Handler {
 	return func(w http.ResponseWriter, r *http.Request) error {
 		uuid := r.PathValue("uuid")
 		ctx := getContext(r)
@@ -33,11 +36,22 @@ func ViewDocument(client ViewDocumentClient, tmpl template.Template) Handler {
 			return err
 		}
 		isSysAdminUser := user.HasRole("System Admin")
+		donorId := documentData.CaseItems[0].Donor.ID
 
-		data := viewDocumentData{
+		data := deleteDocumentData{
 			XSRFToken:      ctx.XSRFToken,
 			Document:       documentData,
 			IsSysAdminUser: isSysAdminUser,
+			DonorId:        donorId,
+		}
+
+		if r.Method == http.MethodPost {
+			err = client.DeleteDocument(ctx, uuid)
+			if err != nil {
+				return err
+			}
+
+			return RedirectError(fmt.Sprintf("/donor/%d/documents?success=true&documentFriendlyName=%s&documentCreatedTime=%s", donorId, documentData.FriendlyDescription, documentData.CreatedDate))
 		}
 
 		return tmpl(w, data)
