bf4625e9e64a39ad700f84d402d68faaf8d0afce
allan.morris <allan.morris@justice.gov.uk>
2026-02-03 20:27:53 +0000

    VEGA-3574: Refactor and expand Cypress tests for search pagination logic and edge cases

--- cypress/e2e/search-pagination.cy.js
+++ cypress/e2e/search-pagination.cy.js
@@ -1,210 +1,174 @@
-describe("Search pagination", () => {
-  describe("Next and Previous links", () => {
+describe("Search pagination - Next and Previous link hrefs", () => {
+  describe("With paginated results", () => {
     beforeEach(() => {
-      // We have to mock a search with enough results to trigger the pagination (currently > 25)
-      const mockResults = Array.from({ length: 80 }, (_, i) => ({
-        id: i + 1,
-        uId: `7000-0000-${String(i + 1).padStart(4, "0")}`,
-        personType: i % 3 === 0 ? "Donor" : i % 3 === 1 ? "Attorney" : "Deputy",
-        firstname: "Test",
-        surname: `Person${i + 1}`,
-        dob: "01/01/1980",
-        addressLine1: `${i + 1} Test Road`,
-        cases: [
-          {
-            id: i + 100,
-            uId: `7000-1000-${String(i + 1).padStart(4, "0")}`,
-            caseType: "LPA",
-            caseSubtype: "pfa",
-            status: "Pending",
-          },
-        ],
-      }));
+      // Create a mock with enough results to trigger pagination
+      const generateResults = (count) => {
+        return Array.from({ length: count }, (_, i) => ({
+          id: i + 1,
+          uId: `7000-0000-${String(i + 1).padStart(4, "0")}`,
+          personType: "Donor",
+          firstname: "Test",
+          surname: `Person${i + 1}`,
+          dob: "01/01/1980",
+          addressLine1: `${i + 1} Test Road`,
+          cases: [
+            {
+              id: i + 100,
+              uId: `7000-1000-${String(i + 1).padStart(4, "0")}`,
+              caseType: "LPA",
+              caseSubtype: "pfa",
+              status: "Pending",
+            },
+          ],
+        }));
+      };
 
       cy.addMock("/lpa-api/v1/search/persons", "POST", {
         status: 200,
         body: {
-          aggregations: {
-            personType: {
-              Donor: 27,
-              Attorney: 27,
-              Deputy: 26,
-            },
-          },
-          results: mockResults,
-          total: {
-            count: 80,
-          },
+          aggregations: { personType: { Donor: 80 } },
+          results: generateResults(80),
+          total: { count: 80 },
         },
       });
     });
 
-    it("displays pagination controls when results exceed page size", () => {
+    it("next link on page 1 has correct href with page=2", () => {
       cy.visit("/search?term=test");
 
-      cy.contains("Showing 1 to 25");
-      cy.get(".govuk-pagination").should("exist");
-      cy.get(".govuk-pagination__next").should("exist");
-      cy.get(".govuk-pagination__prev").should("not.exist"); // Not on page 1
+      cy.get(".govuk-pagination__next a")
+        .should("exist")
+        .and("have.attr", "href")
+        .and("include", "term=test")
+        .and("include", "page=2")
+        .and("not.equal", "#");
     });
 
-    it("next link has correct href and navigates to page 2", () => {
+    it("previous link on page 2 has correct href with page=1", () => {
+      cy.visit("/search?term=test&page=2");
+
+      cy.get(".govuk-pagination__prev a")
+        .should("exist")
+        .and("have.attr", "href")
+        .and("include", "term=test")
+        .and("include", "page=1")
+        .and("not.equal", "#");
+    });
+
+    it("next link on page 2 has correct href with page=3", () => {
+      cy.visit("/search?term=test&page=2");
+
+      cy.get(".govuk-pagination__next a")
+        .should("exist")
+        .and("have.attr", "href")
+        .and("include", "term=test")
+        .and("include", "page=3")
+        .and("not.equal", "#");
+    });
+
+    it("preserves search term and filters in pagination hrefs", () => {
+      cy.visit("/search?term=test&person-type=Donor");
+
+      cy.get(".govuk-pagination__next a")
+        .should("have.attr", "href")
+        .and("include", "term=test")
+        .and("include", "person-type=Donor")
+        .and("include", "page=2");
+    });
+
+    it("numbered page links have same format as next/previous links", () => {
       cy.visit("/search?term=test");
+
+      cy.get(".govuk-pagination__item a")
+        .first()
+        .should("have.attr", "href")
+        .and("include", "term=test")
+        .and("include", "page=");
 
       cy.get(".govuk-pagination__next a")
         .should("have.attr", "href")
         .and("include", "term=test")
         .and("include", "page=2");
-
-      cy.get(".govuk-pagination__next a").click();
-
-      cy.url().should("include", "page=2");
-      cy.contains("Showing 26 to 50");
     });
+  });
 
-    it("previous link has correct href and navigates to page 1", () => {
+  describe("Edge cases", () => {
+    it("no next link on last page", () => {
+      cy.addMock("/lpa-api/v1/search/persons", "POST", {
+        status: 200,
+        body: {
+          aggregations: { personType: { Donor: 30 } },
+          results: Array.from({ length: 30 }, (_, i) => ({
+            id: i + 1,
+            uId: `7000-${i + 1}`,
+            personType: "Donor",
+            firstname: "Test",
+            surname: `Person${i}`,
+            cases: [],
+          })),
+          total: { count: 30 },
+        },
+      });
+
       cy.visit("/search?term=test&page=2");
-
-      cy.contains("Showing 26 to 50");
-
-      cy.get(".govuk-pagination__prev a")
-        .should("have.attr", "href")
-        .and("include", "term=test")
-        .and("include", "page=1");
-
-      cy.get(".govuk-pagination__prev a").click();
-
-      cy.url().should("not.include", "page=2");
-      cy.contains("Showing 1 to 25");
-    });
-
-    it("next and previous links work correctly on middle pages", () => {
-      cy.visit("/search?term=test&page=2");
-
-      cy.get(".govuk-pagination__next").should("exist");
-      cy.get(".govuk-pagination__prev").should("exist");
-
-      cy.get(".govuk-pagination__next a")
-        .should("have.attr", "href")
-        .and("include", "page=3");
-
-      cy.get(".govuk-pagination__prev a")
-        .should("have.attr", "href")
-        .and("include", "page=1");
-    });
-
-    it("hides next link on last page", () => {
-      cy.visit("/search?term=test&page=4");
-
-      cy.contains("Showing 76 to 80 of 80 results");
 
       cy.get(".govuk-pagination__next").should("not.exist");
 
-      cy.get(".govuk-pagination__prev").should("exist");
       cy.get(".govuk-pagination__prev a")
-        .should("have.attr", "href")
-        .and("include", "page=3");
+        .should("exist")
+        .and("have.attr", "href")
+        .and("not.equal", "#");
     });
 
-    it("preserves filters in next/previous links", () => {
-      cy.visit("/search?term=test");
-
-      cy.contains("label", "Donor").click();
-      cy.contains(".moj-filter__tag", "Donor");
-
-      cy.get(".govuk-pagination__next a")
-        .should("have.attr", "href")
-        .and("include", "term=test")
-        .and("include", "person-type=Donor")
-        .and("include", "page=2");
-
-      cy.get(".govuk-pagination__next a").click();
-
-      cy.url().should("include", "person-type=Donor");
-      cy.contains(".moj-filter__tag", "Donor");
-
-      cy.get(".govuk-pagination__prev a")
-        .should("have.attr", "href")
-        .and("include", "term=test")
-        .and("include", "person-type=Donor")
-        .and("include", "page=1");
-    });
-
-    it("numbered page links work alongside next/previous", () => {
-      cy.visit("/search?term=test&page=2");
-
-      cy.get(".govuk-pagination__item a").contains("3").click();
-
-      cy.url().should("include", "page=3");
-      cy.contains("Showing 51 to 75");
-
-      cy.get(".govuk-pagination__next").should("exist");
-      cy.get(".govuk-pagination__prev").should("exist");
-    });
-  });
-
-  describe("Pagination with no results", () => {
-    beforeEach(() => {
+    it("no previous link on first page", () => {
       cy.addMock("/lpa-api/v1/search/persons", "POST", {
         status: 200,
         body: {
-          aggregations: {},
-          results: [],
-          total: {
-            count: 0,
-          },
+          aggregations: { personType: { Donor: 30 } },
+          results: Array.from({ length: 30 }, (_, i) => ({
+            id: i + 1,
+            uId: `7000-${i + 1}`,
+            personType: "Donor",
+            firstname: "Test",
+            surname: `Person${i}`,
+            cases: [],
+          })),
+          total: { count: 30 },
         },
       });
 
-      cy.visit("/search?term=nonexistent");
+      cy.visit("/search?term=test");
+
+      cy.get(".govuk-pagination__prev").should("not.exist");
+
+      cy.get(".govuk-pagination__next a")
+        .should("exist")
+        .and("have.attr", "href")
+        .and("not.equal", "#");
     });
 
-    it("does not show pagination controls", () => {
-      cy.contains("Showing 1 to 0");
-      cy.get(".govuk-pagination").should("not.exist");
-    });
-  });
-
-  describe("Pagination with single page", () => {
-    beforeEach(() => {
-      const mockResults = Array.from({ length: 10 }, (_, i) => ({
-        id: i + 1,
-        uId: `7000-0000-${String(i + 1).padStart(4, "0")}`,
-        personType: "Donor",
-        firstname: "Test",
-        surname: `Person${i + 1}`,
-        dob: "01/01/1980",
-        addressLine1: `${i + 1} Test Road`,
-        cases: [
-          {
-            id: i + 100,
-            uId: `7000-1000-${String(i + 1).padStart(4, "0")}`,
-            caseType: "LPA",
-            caseSubtype: "pfa",
-            status: "Pending",
-          },
-        ],
-      }));
-
+    it("no pagination controls with single page of results", () => {
       cy.addMock("/lpa-api/v1/search/persons", "POST", {
         status: 200,
         body: {
           aggregations: { personType: { Donor: 10 } },
-          results: mockResults,
-          total: {
-            count: 10,
-          },
+          results: Array.from({ length: 10 }, (_, i) => ({
+            id: i + 1,
+            uId: `7000-${i + 1}`,
+            personType: "Donor",
+            firstname: "Test",
+            surname: `Person${i}`,
+            cases: [],
+          })),
+          total: { count: 10 },
         },
       });
 
       cy.visit("/search?term=test");
-    });
 
-    it("does not show pagination controls with results under page size", () => {
-      cy.contains("Showing 1 to 10 of 10 results");
+      // With only 10 results (< 25), no pagination should render
       cy.get(".govuk-pagination__next").should("not.exist");
       cy.get(".govuk-pagination__prev").should("not.exist");
-      cy.get(".govuk-pagination__list").should("not.exist");
     });
   });
 });
