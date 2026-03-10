30ebc3cf5358c871e9d852eb8176cde1620ebcc8
allan.morris <allan.morris@justice.gov.uk>
2026-03-10 21:26:32 +0000

    VEGA-3702: Refactor for lint

--- cypress/e2e/get-lpa-history-content.cy.js
+++ cypress/e2e/get-lpa-history-content.cy.js
@@ -297,15 +297,15 @@ describe("Show correct event content", () => {
       entity: {
         name: "Some Task",
       },
-        changeSet: {
-            completedDate: [
-                null,
-                {
-                    date: "2026-03-16 15:00:00.000000",
-                },
-            ],
-            status: ["Not Started", "Completed"],
-        },
+      changeSet: {
+        completedDate: [
+          null,
+          {
+            date: "2026-03-16 15:00:00.000000",
+          },
+        ],
+        status: ["Not Started", "Completed"],
+      },
     });
     cy.visit("/donor/1/history");
     cy.get(".moj-timeline__item")
