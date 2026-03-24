c9fc564d9e45f0d231ce9384383fae8e8f816df5
Michael.Reckless <Michael.Reckless@justice.gov.uk>
2026-03-24 15:03:10 +0000

    VEGA-3541 - Update formatting

--- web/assets/show-hide-filter.js
+++ web/assets/show-hide-filter.js
@@ -2,7 +2,7 @@ export default function showHideFilter(prefix) {
   const button = document.querySelector("button[data-filter-toggle]");
   const filters = document.querySelector(".moj-filter-layout__filter");
   const timeline = document.querySelector("div[data-filter-timeline]");
-  const summary = document.querySelector("div[data-filter-summary]")
+  const summary = document.querySelector("div[data-filter-summary]");
 
   if (button && filters) {
     button.onclick = () => {
