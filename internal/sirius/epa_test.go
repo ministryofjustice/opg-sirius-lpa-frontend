af1cf2bb02d0273bef49070a039f8e0c384e71f2
Mike Reckless <Michael.Reckless@justice.gov.uk>
2026-04-13 15:39:07 +0100

    VEGA-3766 - Implement new create EPA form (#927)
    * VEGA-3766 - Implement new create EPA form server logic and template. Add missing fields from Case struct
    
    Signed-off-by: Michael.Reckless <Michael.Reckless@justice.gov.uk>
    
    * VEGA-3766 - Add unit and pact tests
    
    Signed-off-by: Michael.Reckless <Michael.Reckless@justice.gov.uk>
    
    * VEGA-3766 - Move date string out into variable
    
    Signed-off-by: Michael.Reckless <Michael.Reckless@justice.gov.uk>
    
    * VEGA-3766 - Update pact
    
    Signed-off-by: Michael.Reckless <Michael.Reckless@justice.gov.uk>
    
    * VEGA-3766 - Prevent the create EPA form from submitting multiple times
    
    Signed-off-by: Michael.Reckless <Michael.Reckless@justice.gov.uk>
    
    * VEGA-3766 - Fix receipt date error message not displaying
    
    Signed-off-by: Michael.Reckless <Michael.Reckless@justice.gov.uk>
    
    ---------
    
    Signed-off-by: Michael.Reckless <Michael.Reckless@justice.gov.uk>

---
+++ internal/sirius/epa_test.go
@@ -0,0 +1,76 @@
+package sirius
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/shared"
+	"github.com/pact-foundation/pact-go/v2/consumer"
+	"github.com/pact-foundation/pact-go/v2/matchers"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEpa(t *testing.T) {
+	t.Parallel()
+
+	pact, err := newPact()
+	assert.NoError(t, err)
+
+	testCases := []struct {
+		name          string
+		setup         func()
+		expectedError func(int) error
+		Epa           Case
+	}{
+		{
+			name: "OK",
+			Epa: Case{
+				ReceiptDate:          DateString("2015-03-05"),
+				ExpectedPaymentTotal: 0,
+				Status:               shared.CaseStatusTypePending,
+			},
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("A donor exists").
+					UponReceiving("A request to create an EPA").
+					WithCompleteRequest(consumer.Request{
+						Method: http.MethodPost,
+						Path:   matchers.String("/lpa-api/v1/donors/189/epas"),
+						Headers: matchers.MapMatcher{
+							"Content-Type": matchers.String("application/json"),
+						},
+						Body: map[string]interface{}{
+							"receiptDate":          matchers.Term("05/03/2015", `^\d{1,2}/\d{1,2}/\d{4}$`),
+							"expectedPaymentTotal": matchers.Integer(0),
+							"status":               matchers.String("Pending"),
+						},
+					}).
+					WithCompleteResponse(consumer.Response{
+						Status:  http.StatusCreated,
+						Headers: matchers.MapMatcher{"Content-Type": matchers.String("application/json")},
+					})
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup()
+
+			assert.Nil(t, pact.ExecuteTest(t, func(config consumer.MockServerConfig) error {
+				client := NewClient(http.DefaultClient, fmt.Sprintf("http://127.0.0.1:%d", config.Port))
+
+				err := client.CreateEpa(Context{Context: context.Background()}, 189, tc.Epa)
+				if (tc.expectedError) == nil {
+					assert.Nil(t, err)
+				} else {
+					assert.Equal(t, tc.expectedError(config.Port), err)
+				}
+				return nil
+			}))
+		})
+	}
+}
