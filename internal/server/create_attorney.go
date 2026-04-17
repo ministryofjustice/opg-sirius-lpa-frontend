56200a087f4cb1a30a6dfccec60e90d434406f2c
Michael.Reckless <Michael.Reckless@justice.gov.uk>
2026-04-17 15:46:13 +0100

    VEGA-3774 - Implement create attorney form and add to create epa process

---
+++ internal/server/create_attorney.go
@@ -0,0 +1,93 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ministryofjustice/opg-go-common/template"
+	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/shared"
+	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/sirius"
+)
+
+type CreateAttorneyClient interface {
+	CreateAttorney(ctx sirius.Context, caseId int, attorney sirius.Attorney) error
+	RefDataByCategory(ctx sirius.Context, category string) ([]sirius.RefDataItem, error)
+}
+
+type createAttorneyData struct {
+	XSRFToken            string
+	Attorney             sirius.Attorney
+	Error                sirius.ValidationError
+	RelationshipToDonors []sirius.RefDataItem
+	DonorId              int
+	CaseId               int
+}
+
+func CreateAttorney(client CreateAttorneyClient, tmpl template.Template) Handler {
+	return func(w http.ResponseWriter, r *http.Request) error {
+		ctx := getContext(r)
+
+		donorId, err := strToIntOrStatusError(r.FormValue("id"))
+		if err != nil {
+			return err
+		}
+
+		caseId, err := strToIntOrStatusError(r.FormValue("caseId"))
+		if err != nil {
+			return err
+		}
+
+		data := createAttorneyData{
+			XSRFToken: ctx.XSRFToken,
+			DonorId:   donorId,
+			CaseId:    caseId,
+		}
+
+		data.RelationshipToDonors, err = client.RefDataByCategory(ctx, sirius.RelationshipToDonorCategory)
+		if err != nil {
+			return err
+		}
+
+		if r.Method == http.MethodPost {
+			attorney := sirius.Attorney{
+				Person: sirius.Person{
+					Salutation:        postFormString(r, "salutation"),
+					Firstname:         postFormString(r, "firstname"),
+					Middlenames:       postFormString(r, "middlenames"),
+					Surname:           postFormString(r, "surname"),
+					AlsoKnownAs:       postFormString(r, "otherNames"),
+					DateOfBirth:       postFormDateString(r, "dob"),
+					PhoneNumber:       postFormString(r, "phoneNumber"),
+					Email:             postFormString(r, "email"),
+					AddressLine1:      postFormString(r, "addressLine1"),
+					AddressLine2:      postFormString(r, "addressLine2"),
+					AddressLine3:      postFormString(r, "addressLine3"),
+					Town:              postFormString(r, "town"),
+					County:            postFormString(r, "county"),
+					Country:           postFormString(r, "country"),
+					Postcode:          postFormString(r, "postcode"),
+					CompanyName:       postFormString(r, "companyName"),
+					IsAirmailRequired: postFormString(r, "isAirmailRequired") == "true",
+				},
+				RelationshipToDonor: postFormString(r, "relationshipToDonor"),
+				SystemStatus:        shared.BoolPtr(postFormString(r, "isAttorneyActive") == "true"),
+			}
+			data.Attorney = attorney
+
+			err = client.CreateAttorney(ctx, caseId, attorney)
+
+			if ve, ok := err.(sirius.ValidationError); ok {
+				w.WriteHeader(http.StatusBadRequest)
+				data.Error = ve
+				return tmpl(w, data)
+			} else if err != nil {
+				return err
+			}
+			
+			return RedirectError(fmt.Sprintf("/create-epa?id=%d&caseId=%d", donorId, caseId))
+
+		}
+
+		return tmpl(w, data)
+	}
+}
---
+++ internal/sirius/attorney_test.go
@@ -0,0 +1,120 @@
+package sirius
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/pact-foundation/pact-go/v2/consumer"
+	"github.com/pact-foundation/pact-go/v2/matchers"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateAttorney(t *testing.T) {
+	t.Parallel()
+
+	pact, err := newPact()
+	assert.NoError(t, err)
+
+	testCases := []struct {
+		name          string
+		attorney      Attorney
+		setup         func()
+		expectedError func(int) error
+	}{
+		{
+			name: "OK",
+			attorney: Attorney{
+				RelationshipToDonor: "no relation",
+				Person: Person{
+					Salutation:            "Prof",
+					Firstname:             "Melanie",
+					Middlenames:           "Josefina",
+					Surname:               "Vanvolkenburg",
+					DateOfBirth:           DateString("1978-04-19"),
+					PreviouslyKnownAs:     "Colton Bacman",
+					AlsoKnownAs:           "Mel",
+					AddressLine1:          "29737 Andrew Plaza",
+					AddressLine2:          "Apt. 814",
+					AddressLine3:          "Gislasonside",
+					Town:                  "Hirthehaven",
+					County:                "Saskatchewan",
+					Postcode:              "S7R 9F9",
+					Country:               "Canada",
+					IsAirmailRequired:     true,
+					PhoneNumber:           "072345678",
+					Email:                 "m.vancolkenburg@ca.test",
+					CorrespondenceByPost:  false,
+					CorrespondenceByEmail: true,
+					CorrespondenceByPhone: true,
+					CorrespondenceByWelsh: false,
+					ResearchOptOut:        true,
+				}},
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("I have a pending EPA assigned").
+					UponReceiving("A request to create an attorney").
+					WithCompleteRequest(consumer.Request{
+						Method: http.MethodPost,
+						Path:   matchers.String("/lpa-api/v1/epas/800/attorneys"),
+						Headers: matchers.MapMatcher{
+							"Content-Type": matchers.String("application/json"),
+						},
+						Body: map[string]interface{}{
+							"salutation":            "Prof",
+							"firstname":             "Melanie",
+							"middlenames":           "Josefina",
+							"surname":               "Vanvolkenburg",
+							"dob":                   "19/04/1978",
+							"previousNames":         "Colton Bacman",
+							"otherNames":            "Mel",
+							"addressLine1":          "29737 Andrew Plaza",
+							"addressLine2":          "Apt. 814",
+							"addressLine3":          "Gislasonside",
+							"town":                  "Hirthehaven",
+							"county":                "Saskatchewan",
+							"postcode":              "S7R 9F9",
+							"country":               "Canada",
+							"sageId":                "",
+							"isAirmailRequired":     true,
+							"phoneNumber":           "072345678",
+							"email":                 "m.vancolkenburg@ca.test",
+							"correspondenceByPost":  false,
+							"correspondenceByEmail": true,
+							"correspondenceByPhone": true,
+							"correspondenceByWelsh": false,
+							"researchOptOut":        true,
+							"companyName":           "",
+							"companyReference":      "",
+							"relationshipToDonor":   "no relation",
+						},
+					}).
+					WithCompleteResponse(consumer.Response{
+						Status:  http.StatusCreated,
+						Headers: matchers.MapMatcher{"Content-Type": matchers.String("application/json")},
+					})
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup()
+
+			assert.Nil(t, pact.ExecuteTest(t, func(config consumer.MockServerConfig) error {
+				client := NewClient(http.DefaultClient, fmt.Sprintf("http://127.0.0.1:%d", config.Port))
+
+				err := client.CreateAttorney(Context{Context: context.Background()}, 800, tc.attorney)
+				if (tc.expectedError) == nil {
+					assert.Nil(t, err)
+				} else {
+					assert.Equal(t, tc.expectedError(config.Port), err)
+				}
+				return nil
+			}))
+		})
+	}
+}
