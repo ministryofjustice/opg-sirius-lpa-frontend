7732240c94805342337555341fd77a8cfdb8b99f
David Bridgwood <david.bridgwood@digital.justice.gov.uk>
2026-05-01 08:57:32 +0100

    VEGA-3777 pact-test update attorney/correspondent details

--- internal/sirius/correspondent_test.go
+++ internal/sirius/correspondent_test.go
@@ -119,6 +119,114 @@ func TestCreateCorrespondent(t *testing.T) {
 	}
 }
 
+func TestUpdateCorrespondent(t *testing.T) {
+	t.Parallel()
+
+	pact, err := newPact()
+	assert.NoError(t, err)
+
+	testCases := []struct {
+		name          string
+		correspondent Correspondent
+		setup         func()
+		expectedError func(int) error
+	}{
+		{
+			name: "OK",
+			correspondent: Correspondent{
+				Person: Person{
+					ID:                    654,
+					Salutation:            "Prof",
+					Firstname:             "Melanie",
+					Middlenames:           "Josefina",
+					Surname:               "Vanvolkenburg",
+					DateOfBirth:           DateString("1978-04-19"),
+					PreviouslyKnownAs:     "Colton Bacman",
+					AlsoKnownAs:           "Mel",
+					AddressLine1:          "29737 Andrew Plaza",
+					AddressLine2:          "Apt. 814",
+					AddressLine3:          "Gislasonside",
+					Town:                  "Hirthehaven",
+					County:                "Saskatchewan",
+					Postcode:              "S7R 9F9",
+					Country:               "Canada",
+					IsAirmailRequired:     true,
+					PhoneNumber:           "072345678",
+					Email:                 "m.vancolkenburg@ca.test",
+					CorrespondenceByPost:  false,
+					CorrespondenceByEmail: true,
+					CorrespondenceByPhone: true,
+					CorrespondenceByWelsh: false,
+					ResearchOptOut:        true,
+				},
+			},
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("A correspondent exists").
+					UponReceiving("A request to update a correspondent").
+					WithCompleteRequest(consumer.Request{
+						Method: http.MethodPut,
+						Path:   matchers.String("/lpa-api/v1/persons/654"),
+						Headers: matchers.MapMatcher{
+							"Content-Type": matchers.String("application/json"),
+						},
+						Body: map[string]interface{}{
+							"id":                    654,
+							"salutation":            "Prof",
+							"firstname":             "Melanie",
+							"middlenames":           "Josefina",
+							"surname":               "Vanvolkenburg",
+							"dob":                   "19/04/1978",
+							"previousNames":         "Colton Bacman",
+							"otherNames":            "Mel",
+							"addressLine1":          "29737 Andrew Plaza",
+							"addressLine2":          "Apt. 814",
+							"addressLine3":          "Gislasonside",
+							"town":                  "Hirthehaven",
+							"county":                "Saskatchewan",
+							"postcode":              "S7R 9F9",
+							"country":               "Canada",
+							"sageId":                "",
+							"isAirmailRequired":     true,
+							"phoneNumber":           "072345678",
+							"email":                 "m.vancolkenburg@ca.test",
+							"correspondenceByPost":  false,
+							"correspondenceByEmail": true,
+							"correspondenceByPhone": true,
+							"correspondenceByWelsh": false,
+							"researchOptOut":        true,
+							"companyName":           "",
+							"companyReference":      "",
+						},
+					}).
+					WithCompleteResponse(consumer.Response{
+						Status:  http.StatusOK,
+						Headers: matchers.MapMatcher{"Content-Type": matchers.String("application/json")},
+					})
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup()
+
+			assert.Nil(t, pact.ExecuteTest(t, func(config consumer.MockServerConfig) error {
+				client := NewClient(http.DefaultClient, fmt.Sprintf("http://127.0.0.1:%d", config.Port))
+
+				err := client.UpdateCorrespondent(Context{Context: context.Background()}, 654, tc.correspondent)
+				if tc.expectedError == nil {
+					assert.Nil(t, err)
+				} else {
+					assert.Equal(t, tc.expectedError(config.Port), err)
+				}
+				return nil
+			}))
+		})
+	}
+}
+
 func TestCorrespondentSummary(t *testing.T) {
 	correspondent := Correspondent{
 		Person: Person{
