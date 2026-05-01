cd6ab8c72a5be255b971e93b935e11152d8b8f59
David Bridgwood <david.bridgwood@digital.justice.gov.uk>
2026-05-01 08:57:32 +0100

    VEGA-3777 pact-test update attorney/correspondent details

--- internal/sirius/attorney_test.go
+++ internal/sirius/attorney_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/shared"
 	"github.com/pact-foundation/pact-go/v2/consumer"
 	"github.com/pact-foundation/pact-go/v2/matchers"
 
@@ -109,6 +110,118 @@ func TestCreateAttorney(t *testing.T) {
 
 				err := client.CreateAttorney(Context{Context: context.Background()}, 800, tc.attorney)
 				if (tc.expectedError) == nil {
+					assert.Nil(t, err)
+				} else {
+					assert.Equal(t, tc.expectedError(config.Port), err)
+				}
+				return nil
+			}))
+		})
+	}
+}
+
+func TestUpdateAttorney(t *testing.T) {
+	t.Parallel()
+
+	pact, err := newPact()
+	assert.NoError(t, err)
+
+	testCases := []struct {
+		name          string
+		attorney      Attorney
+		setup         func()
+		expectedError func(int) error
+	}{
+		{
+			name: "OK",
+			attorney: Attorney{
+				RelationshipToDonor: "no relation",
+				SystemStatus:        shared.BoolPtr(true),
+				Person: Person{
+					ID:                    321,
+					Salutation:            "Prof",
+					Firstname:             "Melanie",
+					Middlenames:           "Josefina",
+					Surname:               "Vanvolkenburg",
+					DateOfBirth:           DateString("1978-04-19"),
+					PreviouslyKnownAs:     "Colton Bacman",
+					AlsoKnownAs:           "Mel",
+					AddressLine1:          "29737 Andrew Plaza",
+					AddressLine2:          "Apt. 814",
+					AddressLine3:          "Gislasonside",
+					Town:                  "Hirthehaven",
+					County:                "Saskatchewan",
+					Postcode:              "S7R 9F9",
+					Country:               "Canada",
+					IsAirmailRequired:     true,
+					PhoneNumber:           "072345678",
+					Email:                 "m.vancolkenburg@ca.test",
+					CorrespondenceByPost:  false,
+					CorrespondenceByEmail: true,
+					CorrespondenceByPhone: true,
+					CorrespondenceByWelsh: false,
+					ResearchOptOut:        true,
+				},
+			},
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("An attorney exists").
+					UponReceiving("A request to update an attorney").
+					WithCompleteRequest(consumer.Request{
+						Method: http.MethodPut,
+						Path:   matchers.String("/lpa-api/v1/attorneys/321"),
+						Headers: matchers.MapMatcher{
+							"Content-Type": matchers.String("application/json"),
+						},
+						Body: map[string]interface{}{
+							"id":                    321,
+							"salutation":            "Prof",
+							"firstname":             "Melanie",
+							"middlenames":           "Josefina",
+							"surname":               "Vanvolkenburg",
+							"dob":                   "19/04/1978",
+							"previousNames":         "Colton Bacman",
+							"otherNames":            "Mel",
+							"addressLine1":          "29737 Andrew Plaza",
+							"addressLine2":          "Apt. 814",
+							"addressLine3":          "Gislasonside",
+							"town":                  "Hirthehaven",
+							"county":                "Saskatchewan",
+							"postcode":              "S7R 9F9",
+							"country":               "Canada",
+							"sageId":                "",
+							"isAirmailRequired":     true,
+							"phoneNumber":           "072345678",
+							"email":                 "m.vancolkenburg@ca.test",
+							"correspondenceByPost":  false,
+							"correspondenceByEmail": true,
+							"correspondenceByPhone": true,
+							"correspondenceByWelsh": false,
+							"researchOptOut":        true,
+							"companyName":           "",
+							"companyReference":      "",
+							"relationshipToDonor":   "no relation",
+							"systemStatus":          true,
+						},
+					}).
+					WithCompleteResponse(consumer.Response{
+						Status:  http.StatusOK,
+						Headers: matchers.MapMatcher{"Content-Type": matchers.String("application/json")},
+					})
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup()
+
+			assert.Nil(t, pact.ExecuteTest(t, func(config consumer.MockServerConfig) error {
+				client := NewClient(http.DefaultClient, fmt.Sprintf("http://127.0.0.1:%d", config.Port))
+
+				err := client.UpdateAttorney(Context{Context: context.Background()}, 321, tc.attorney)
+				if tc.expectedError == nil {
 					assert.Nil(t, err)
 				} else {
 					assert.Equal(t, tc.expectedError(config.Port), err)
