a8ec1a7e07a08392047ba578dd8f342faa00973c
David Bridgwood <david.bridgwood@digital.justice.gov.uk>
2026-05-01 08:57:05 +0100

    VEGA-3777 cypress-test update attorney/correspondent details

--- cypress/e2e/create-attorney.cy.js
+++ cypress/e2e/create-attorney.cy.js
@@ -1,4 +1,26 @@
-describe("Create Attorney", () => {
+const fillInAttorneyDetails = () => {
+  cy.get("#f-salutation").type("Prof");
+  cy.get("#f-firstname").type("Melanie");
+  cy.get("#f-middlenames").type("Josefina");
+  cy.get("#f-surname").type("Vanvolkenburg");
+  cy.get("#f-dob").type("1978-04-19");
+  cy.get("#f-companyName").type("ACME");
+  cy.get('[data-module="app-address-finder"] .govuk-link').click();
+  cy.get("#f-addressLine1").type("29737 Andrew Plaza");
+  cy.get("#f-addressLine2").type("Apt. 814");
+  cy.get("#f-addressLine3").type("Gislasonside");
+  cy.get("#f-town").type("Hirthehaven");
+  cy.get("#f-county").type("Saskatchewan");
+  cy.get("#f-postcode").type("S7R 9F9");
+  cy.get("#f-country").type("Canada");
+  cy.get("label[for=f-isAirmailRequired]").click();
+  cy.get("#f-phoneNumber").type("072345678");
+  cy.get("#f-email").type("m.vancolkenburg@ca.test");
+  cy.get("#f-relationshipToDonor").select("Other relation");
+  cy.get("label[for=f-isAttorneyActive]").click();
+};
+
+describe("Create or Update Attorney", () => {
   beforeEach(() => {
     cy.addMock("/lpa-api/v1/reference-data/relationshipToDonor", "GET", {
       status: 200,
@@ -20,37 +42,66 @@ describe("Create Attorney", () => {
     });
 
     cy.visit("/create-attorney?id=1&caseId=2");
-
-    cy.get("#f-salutation").type("Prof");
-    cy.get("#f-firstname").type("Melanie");
-    cy.get("#f-middlenames").type("Josefina");
-    cy.get("#f-surname").type("Vanvolkenburg");
-    cy.get("#f-dob").type("1978-04-19");
-    cy.get("#f-companyName").type("ACME");
-    cy.get('[data-module="app-address-finder"] .govuk-link').click();
-    cy.get("#f-addressLine1").type("29737 Andrew Plaza");
-    cy.get("#f-addressLine2").type("Apt. 814");
-    cy.get("#f-addressLine3").type("Gislasonside");
-    cy.get("#f-town").type("Hirthehaven");
-    cy.get("#f-county").type("Saskatchewan");
-    cy.get("#f-postcode").type("S7R 9F9");
-    cy.get("#f-country").type("Canada");
-    cy.get("label[for=f-isAirmailRequired]").click();
-    cy.get("#f-phoneNumber").type("072345678");
-    cy.get("#f-email").type("m.vancolkenburg@ca.test");
-    cy.get("#f-relationshipToDonor").select("Other relation");
-    cy.get("label[for=f-isAttorneyActive]").click();
   });
 
   it("creates an attorney on an EPA", () => {
+    fillInAttorneyDetails();
     cy.contains("Add an attorney");
     cy.get("button[type=submit]").click();
     cy.url().should("include", "create-epa");
   });
 
   it("creates an attorney on an EPA and add another attorney", () => {
+    fillInAttorneyDetails();
     cy.contains("Add an attorney");
     cy.get("input[type=submit][name=add-another]").click();
     cy.url().should("include", "create-attorney");
+  });
+
+  it("updates an existing attorney on an EPA", () => {
+    cy.addMock("/lpa-api/v1/cases/2", "GET", {
+      status: 200,
+      body: {
+        id: 2,
+        attorneys: [
+          {
+            id: 3,
+            firstname: "Rudolph",
+            surname: "Stotesbury",
+            relationshipToDonor: "NO RELATION",
+          },
+        ],
+      },
+    });
+
+    cy.addMock("/lpa-api/v1/attorneys/3", "PUT", {
+      status: 200,
+      body: {},
+    });
+
+    cy.visit("/create-epa?id=2&caseId=2");
+    cy.contains("Edit EPA");
+    cy.contains("Rudolph Stotesbury");
+    cy.get("#f-update-attorney-3 .govuk-visually-hidden").should(
+      "contain.text",
+      "attorney Rudolph Stotesbury",
+    );
+    cy.get("#f-update-attorney-3")
+      .should(
+        "have.attr",
+        "href",
+        "/create-attorney?id=2&caseId=2&attorneyId=3",
+      )
+      .click();
+
+    cy.contains("Update attorney details");
+    cy.get("#f-firstname").should("have.value", "Rudolph");
+    cy.get("#f-surname").should("have.value", "Stotesbury");
+    cy.get("#f-relationshipToDonor").should("have.value", "NO RELATION");
+    cy.get("input[type=submit][name=add-another]").should("not.exist");
+
+    cy.get("#f-firstname").clear().type("Rafael");
+    cy.get("button[type=submit]").click();
+    cy.url().should("include", "create-epa");
   });
 });
--- cypress/e2e/create-correspondent.cy.js
+++ cypress/e2e/create-correspondent.cy.js
@@ -1,14 +1,26 @@
-describe("Create correspondent", () => {
+describe("Create or update correspondent", () => {
   beforeEach(() => {
     cy.addMock("/lpa-api/v1/persons", "POST", {
       status: 201,
       body: {},
     });
 
-    cy.visit("/create-correspondent?id=1&caseId=2");
+    cy.addMock("/lpa-api/v1/persons/3", "PUT", {
+      status: 200,
+      body: {},
+    });
   });
 
   it("creates a correspondent on an EPA", () => {
+    cy.addMock("/lpa-api/v1/cases/2", "GET", {
+      status: 200,
+      body: {
+        id: 2,
+      },
+    });
+
+    cy.visit("/create-correspondent?id=1&caseId=2");
+
     cy.contains("Add a correspondent");
     cy.get("#f-salutation").type("Prof");
     cy.get("#f-firstname").type("Melanie");
@@ -28,5 +40,39 @@ describe("Create correspondent", () => {
     cy.get("#f-email").type("m.vancolkenburg@ca.test");
     cy.get("button[type=submit]").click();
     cy.url().should("include", "/create-epa");
+  });
+
+  it("updates a correspondent on an EPA", () => {
+    cy.addMock("/lpa-api/v1/cases/2", "GET", {
+      status: 200,
+      body: {
+        id: 2,
+        correspondent: {
+          id: 3,
+          firstName: "Melanie",
+          surname: "Vanvolkenburg",
+        },
+      },
+    });
+
+    cy.visit("/create-epa?id=2&caseId=2");
+    cy.contains("Edit EPA");
+    cy.contains("Melanie Vanvolkenburg");
+    cy.get("#f-update-correspondent-3 .govuk-visually-hidden").should(
+      "contain.text",
+      "correspondent Melanie Vanvolkenburg",
+    );
+    cy.get("#f-update-correspondent-3")
+      .should("have.attr", "href", "/create-correspondent?id=2&caseId=2")
+      .click();
+
+    cy.contains("Update correspondent details");
+    cy.get("#f-firstname").should("have.value", "Melanie");
+    cy.get("#f-surname").should("have.value", "Vanvolkenburg");
+    cy.get("input[type=submit][name=add-another]").should("not.exist");
+
+    cy.get("#f-firstname").clear().type("Mindy");
+    cy.get("button[type=submit]").click();
+    cy.url().should("include", "create-epa");
   });
 });
