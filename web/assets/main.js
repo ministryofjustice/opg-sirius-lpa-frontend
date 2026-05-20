1d93d7717a8f5bf6cdc5b3a5fd691731e54b5687
David Bridgwood <david.bridgwood@digital.justice.gov.uk>
2026-05-20 09:43:10 +0100

    vega-3879 action panel can be toggled with details/summary pattern

--- web/assets/main.js
+++ web/assets/main.js
@@ -18,6 +18,7 @@ import addressFinder from "./address-finder";
 import autoApplyFilter from "./auto-apply-filter";
 import showHideCaseSummary from "./show-hide-case-summary";
 import disableAfterClick from "./disable-after-click";
+import "htmx.org";
 import documentListSort from "./document-list-sort";
 
 const prefix = document.body.getAttribute("data-prefix");
@@ -41,6 +42,7 @@ addressFinder(prefix);
 autoApplyFilter();
 showHideCaseSummary();
 disableAfterClick();
+window.htmx = require("htmx.org");
 documentListSort();
 
 if (window.self !== window.parent) {
