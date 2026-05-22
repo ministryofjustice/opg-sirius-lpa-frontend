f7c2303230b2e6b4369bf4f5d350c0bb09dfa80b
Mike Reckless <Michael.Reckless@justice.gov.uk>
2026-05-22 10:57:03 +0100

    Revert "VEGA-3914 - SVG sprite icons (#978)"
    This reverts commit e93f34899a9c38aff631992a10567d48a5f2dd85.

--- internal/templatefn/fn.go
+++ internal/templatefn/fn.go
@@ -33,14 +33,6 @@ func All(siriusPublicURL, prefix, staticHash string) map[string]interface{} {
 				return prefix + s
 			}
 		},
-		"prefixSVG": func(s string) string {
-			path := prefix + "/assets/images/icons-sprite.svg"
-			if len(staticHash) >= 11 {
-				return path + "?" + url.QueryEscape(staticHash[3:11]) + "#" + s
-			} else {
-				return path + "#" + s
-			}
-		},
 		"today": func() string {
 			return time.Now().Format("2006-01-02")
 		},
