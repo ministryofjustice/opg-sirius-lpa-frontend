714fbd66a3a7cb443d5146afcc3f8f3be4b5030e
David Bridgwood <david.bridgwood@digital.justice.gov.uk>
2026-05-27 09:11:17 +0100

    vega-3879 add simple cypress test

---
+++ cypress/e2e/action-panel.cy.js
@@ -0,0 +1,34 @@
+describe("Action Panel", () => {
+  beforeEach(() => {
+        cy.addMock("/lpa-api/v1/persons/1/cases", "GET", {
+      status: 200,
+      body: {
+        cases: [
+          { caseType: "LPA", caseSubtype: "pfa", id: 34, uId: "7000-1234-1234" },
+          { caseType: "LPA", caseSubtype: "hw", id: 78, uId: "7000-5678-5678" },
+        ],
+      },
+    });
+
+    cy.addMock(
+      "/lpa-api/v1/persons/1/documents?filter=draft:0,preview:0&limit=999",
+      "GET",
+      {
+        status: 200,
+        body: {
+          total: 0,
+          documents: [],
+        },
+      },
+    );
+    cy.visit("/donor/1/documents");
+  });
+
+  it("can open and close the action panel", () => {
+    cy.get("#actions-tab").click();
+    cy.get("#actions-content").should("be.visible");
+
+    cy.get("#actions-tab").click();
+    cy.get("#actions-content").should("not.be.visible");
+  });
+});
