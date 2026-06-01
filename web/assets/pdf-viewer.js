510303a22bcbd7f20be650bd8a2e5784b1d0fa60
nicholassully <nicholas.sully@digital.justice.gov.uk>
2026-06-01 11:42:19 +0100

    vega-3944 passing pane number around

--- web/assets/pdf-viewer.js
+++ web/assets/pdf-viewer.js
@@ -18,10 +18,18 @@ class PDFViewer {
     this.pageCanvases = [];
     this.isScrolling = false;
     this.rotation = this.loadRotation();
+    this.storageKey = this.getStorageKey();
   }
 
   getRotationStorageKey() {
     return `pdf-viewer-rotation-${this.url}`;
+  }
+
+  getStorageKey() {
+    // Create a unique key based on the PDF URL and pane ID
+    // This ensures each pane maintains its own state for the same document
+    console.log(this.paneId)
+    return `${STORAGE_KEY}_${this.paneId}_${this.url}`;
   }
 
   loadRotation() {
