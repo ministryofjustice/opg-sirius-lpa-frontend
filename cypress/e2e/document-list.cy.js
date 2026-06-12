331cd9a1381d8eac15363dc0d673abb432910296
Michael.Reckless <Michael.Reckless@justice.gov.uk>
2026-06-12 11:28:18 +0100

    VEGA-3953 - Add tests and fix broken tests

--- cypress/e2e/document-list.cy.js
+++ cypress/e2e/document-list.cy.js
@@ -342,6 +342,11 @@ describe("View documents", () => {
         },
       },
     );
+
+    cy.addMock("/lpa-api/v1/permissions", "GET", {
+      status: 200,
+      body: {},
+    })
   });
 
   it("on a person", () => {
@@ -926,6 +931,11 @@ describe("sort by dropdown", () => {
         },
       },
     );
+
+    cy.addMock("/lpa-api/v1/permissions", "GET", {
+      status: 200,
+      body: {},
+    })
   });
 
   it("renders the dropdown with a default placeholder and all expected options", () => {
