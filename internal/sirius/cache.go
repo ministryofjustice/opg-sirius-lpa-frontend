88af9c5c3a1114809ae22cd60567e34f1be7a135
David Bridgwood <124149420+davidbridgwood@users.noreply.github.com>
2026-06-17 16:18:41 +0100

    VEGA-3959 : service to get bank-holidays (#1021)

--- internal/sirius/cache.go
+++ internal/sirius/cache.go
@@ -7,22 +7,22 @@ import (
 
 type cacheItem struct {
 	time  time.Time
-	value []RefDataItem
+	value interface{}
 }
 
 var mutex = &sync.RWMutex{}
 
 var cache map[string]cacheItem
 
-func getCached(category string) ([]RefDataItem, bool) {
-	var v []RefDataItem
+func getCached(category string) (interface{}, bool) {
+	var v interface{}
 	found := false
 
 	oneHourAgo := time.Now().Add(-1 * time.Hour)
 
 	mutex.RLock()
 
-	if cache[category].time.After(oneHourAgo) && len(cache[category].value) > 0 {
+	if cache[category].time.After(oneHourAgo) && cache[category].value != nil {
 		v = cache[category].value
 		found = true
 	}
@@ -32,7 +32,7 @@ func getCached(category string) ([]RefDataItem, bool) {
 	return v, found
 }
 
-func setCached(category string, value []RefDataItem) {
+func setCached(category string, value interface{}) {
 	if cache == nil {
 		cache = map[string]cacheItem{}
 	}
