a2698de29f16a3687c792ab7500366fea83979a4
David Bridgwood <david.bridgwood@digital.justice.gov.uk>
2026-06-19 11:38:10 +0100

    VEGA-3886 : return to anchor in cancel URLs

--- web/assets/main.js
+++ web/assets/main.js
@@ -70,6 +70,7 @@ htmx.on("htmx:afterSwap", (event) => {
     handleInsertCheckboxes({ scope: swapDetails.target });
     autoClick(swapDetails.target);
     textEditor();
+    addressFinder(prefix);
 
     // Update the action panel width if swapping in create-document or edit-document content
     if (swapDetails.target.id === "actions-content") {
