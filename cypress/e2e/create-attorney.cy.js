2985b8bba52f12c6da4729adc671eb1dcf35a711
David Bridgwood <david.bridgwood@digital.justice.gov.uk>
2026-06-22 12:34:40 +0100

    another attempt

--- cypress/e2e/create-attorney.cy.js
+++ cypress/e2e/create-attorney.cy.js
@@ -22,6 +22,20 @@ const fillInAttorneyDetails = () => {
 
 describe("Create or Update Attorney", () => {
   beforeEach(() => {
+    cy.addMock("/lpa-api/v1/reference-data/relationshipToDonor", "GET", {
+      status: 200,
+      body: [
+        {
+          handle: "NO RELATION",
+          label: "No relation",
+        },
+        {
+          handle: "OTHER RELATION",
+          label: "Other relation",
+        },
+      ],
+    });
+
     cy.addMock("/lpa-api/v1/epas/2/attorneys", "POST", {
       status: 201,
       body: {},
