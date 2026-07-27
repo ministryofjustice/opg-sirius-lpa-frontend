795755c6232750bcd4ec69934d166f44e58ae3fa
allan.morris <allan.morris@justice.gov.uk>
2026-07-27 18:34:51 +0100

    Enhance `CreateLpa` handler to handle creation and updates of LPAs, expand form handling, implement user permissions, and update tests accordingly.

--- internal/server/create_lpa.go
+++ internal/server/create_lpa.go
@@ -2,25 +2,33 @@ package server
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/ministryofjustice/opg-go-common/template"
+	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/shared"
 	"github.com/ministryofjustice/opg-sirius-lpa-frontend/internal/sirius"
 )
 
 type CreateLpaClient interface {
 	Person(sirius.Context, int) (sirius.Person, error)
-	Case(sirius.Context, int) (sirius.Case, error)
+	Lpa(sirius.Context, int) (sirius.Lpa, error)
+	CreateLpa(ctx sirius.Context, donorID int, lpa sirius.Lpa) (sirius.Lpa, error)
+	UpdateLpa(ctx sirius.Context, caseID int, lpa sirius.Lpa) error
+	GetUserPermissions(sirius.Context) (sirius.Permissions, error)
 }
 
 type createLpaData struct {
-	XSRFToken string
-	Success   bool
-	Error     sirius.ValidationError
-	DonorId   int
-	DonorName string
-	Title     string
-	CaseId    int
-	CaseItem  sirius.Case
+	XSRFToken          string
+	Success            bool
+	IsUpdate           bool
+	Error              sirius.ValidationError
+	DonorId            int
+	DonorName          string
+	Title              string
+	CaseId             int
+	Lpa                sirius.Lpa
+	AppointmentType    string
+	CanEditReceiptDate bool
 }
 
 func CreateLpa(client CreateLpaClient, tmpl template.Template, partialTmpl template.Template) Handler {
@@ -37,11 +45,17 @@ func CreateLpa(client CreateLpaClient, tmpl template.Template, partialTmpl templ
 			return err
 		}
 
+		userPermissions, err := client.GetUserPermissions(ctx)
+		if err != nil {
+			return err
+		}
+
 		data := createLpaData{
-			XSRFToken: ctx.XSRFToken,
-			DonorId:   donorID,
-			DonorName: donor.Firstname + " " + donor.Surname,
-			Title:     "Create an LPA",
+			XSRFToken:          ctx.XSRFToken,
+			DonorId:            donorID,
+			DonorName:          donor.Firstname + " " + donor.Surname,
+			Title:              "Create an LPA",
+			CanEditReceiptDate: userPermissions.Includes("v1-lpas-edit-dates", "PUT"),
 		}
 
 		caseIdStr := r.FormValue("caseId")
@@ -52,12 +66,113 @@ func CreateLpa(client CreateLpaClient, tmpl template.Template, partialTmpl templ
 				return err
 			}
 
-			data.CaseItem, err = client.Case(ctx, data.CaseId)
+			data.Lpa, err = client.Lpa(ctx, data.CaseId)
 			if err != nil {
 				return err
 			}
 
+			data.AppointmentType = appointmentTypeFromCase(data.Lpa.Case)
 			data.Title = "Edit LPA"
+			data.IsUpdate = true
+		}
+
+		if r.Method == http.MethodPost {
+			caseAttorneyValue := r.FormValue("caseAttorney")
+
+			lpa := sirius.Lpa{
+				OnlineLpaId:            postFormString(r, "onlineLpaId"),
+				AttorneyActDecisions:   postFormString(r, "attorneyActDecisions"),
+				ApplicantType:          postFormString(r, "applicantType"),
+				ApplicantSignatureDate: postFormDateString(r, "applicantSignatureDate"),
+				CardPaymentContact:     postFormString(r, "cardPaymentContact"),
+				Case: sirius.Case{
+					SubType:                                   postFormString(r, "caseSubtype"),
+					ApplicationType:                           postFormString(r, "applicationType"),
+					CaseAttorneySingular:                      shared.BoolPtr(caseAttorneyValue == "singular"),
+					CaseAttorneyJointly:                       shared.BoolPtr(caseAttorneyValue == "jointly"),
+					CaseAttorneyJointlyAndSeverally:           shared.BoolPtr(caseAttorneyValue == "jointly-and-severally"),
+					CaseAttorneyJointlyAndJointlyAndSeverally: shared.BoolPtr(caseAttorneyValue == "jointly-and-jointly-and-severally"),
+					LifeSustainingTreatment:                   postFormString(r, "lifeSustainingTreatment"),
+					LifeSustainingTreatmentSignatureDateA:     postFormDateString(r, "lifeSustainingTreatmentSignatureDate"),
+					LpaDonorSignatureDate:                     postFormDateString(r, "lpaDonorSignatureDate"),
+				},
+			}
+
+			for _, idStr := range r.PostForm["applicantIds"] {
+				if id, err := strconv.Atoi(idStr); err == nil {
+					lpa.ApplicantIds = append(lpa.ApplicantIds, id)
+				}
+			}
+
+			lpa.LifeSustainingTreatmentSignedAndWitnessed = shared.BoolPtr(postFormString(r, "lifeSustainingTreatmentSignedAndWitnessed") == "true")
+
+			reducedFeeSelected := postFormCheckboxChecked(r, "applicationFee", "reducedFee")
+			reducedFeeType := postFormString(r, "reducedFeeType")
+			lpa.PaymentByCheque = shared.BoolPtr(postFormCheckboxChecked(r, "applicationFee", "cheque"))
+			lpa.PaymentByDebitCreditCard = shared.BoolPtr(postFormCheckboxChecked(r, "applicationFee", "card"))
+			lpa.PaymentExemption = shared.BoolPtr(reducedFeeSelected && reducedFeeType == "exemption")
+			lpa.PaymentRemission = shared.BoolPtr(reducedFeeSelected && reducedFeeType == "remission")
+			lpa.RepeatApplication = shared.BoolPtr(postFormCheckboxChecked(r, "applicationFee", "repeatApplication"))
+
+			if !*lpa.PaymentByDebitCreditCard {
+				lpa.CardPaymentContact = ""
+			}
+
+			lpa.AnyOtherInfo = shared.BoolPtr(postFormString(r, "anyOtherInfo") == "true")
+			lpa.AdditionalInfo = postFormString(r, "additionalInfo")
+			if !*lpa.AnyOtherInfo {
+				lpa.AdditionalInfo = ""
+			}
+
+			if lpa.SubType != "pfa" {
+				lpa.AttorneyActDecisions = ""
+			}
+			if lpa.SubType != "hw" {
+				lpa.LifeSustainingTreatment = ""
+				lpa.LifeSustainingTreatmentSignatureDateA = ""
+				lpa.LifeSustainingTreatmentSignedAndWitnessed = nil
+			}
+
+			preferencesNone := postFormCheckboxChecked(r, "preferencesAndInstructions", "none")
+			lpa.ApplicationHasGuidance = shared.BoolPtr(!preferencesNone && postFormCheckboxChecked(r, "preferencesAndInstructions", "guidance"))
+			lpa.ApplicationHasRestrictions = shared.BoolPtr(!preferencesNone && postFormCheckboxChecked(r, "preferencesAndInstructions", "restrictions"))
+
+			if data.CanEditReceiptDate {
+				lpa.ReceiptDate = postFormDateString(r, "receiptDate")
+			} else if isEditing {
+				lpa.ReceiptDate = data.Lpa.ReceiptDate
+			}
+
+			if lpa.ApplicationType != "Online" {
+				lpa.OnlineLpaId = ""
+			}
+
+			data.AppointmentType = caseAttorneyValue
+
+			if isEditing {
+				err = client.UpdateLpa(ctx, data.CaseId, lpa)
+				data.Lpa, _ = client.Lpa(ctx, data.CaseId)
+			} else {
+				lpa, err = client.CreateLpa(ctx, donorID, lpa)
+				if err == nil {
+					data.Lpa = lpa
+					data.CaseId = lpa.ID
+				}
+			}
+
+			if ve, ok := err.(sirius.ValidationError); ok {
+				w.WriteHeader(http.StatusBadRequest)
+				data.Error = ve
+
+				if r.Header.Get("HX-Request") == "true" {
+					return partialTmpl(w, data)
+				}
+				return tmpl(w, data)
+			} else if err != nil {
+				return err
+			}
+
+			data.Success = true
 		}
 
 		if r.Header.Get("HX-Request") == "true" {
@@ -65,5 +180,20 @@ func CreateLpa(client CreateLpaClient, tmpl template.Template, partialTmpl templ
 		}
 
 		return tmpl(w, data)
+	}
+}
+
+func appointmentTypeFromCase(c sirius.Case) string {
+	switch {
+	case c.CaseAttorneySingular != nil && *c.CaseAttorneySingular:
+		return "singular"
+	case c.CaseAttorneyJointly != nil && *c.CaseAttorneyJointly:
+		return "jointly"
+	case c.CaseAttorneyJointlyAndSeverally != nil && *c.CaseAttorneyJointlyAndSeverally:
+		return "jointly-and-severally"
+	case c.CaseAttorneyJointlyAndJointlyAndSeverally != nil && *c.CaseAttorneyJointlyAndJointlyAndSeverally:
+		return "jointly-and-jointly-and-severally"
+	default:
+		return ""
 	}
 }
