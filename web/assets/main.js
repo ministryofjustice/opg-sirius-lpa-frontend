9254d3fac61bc96004b1eaac28dd3656e9ae9ec3
Mike Reckless <Michael.Reckless@justice.gov.uk>
2026-09-14 11:07:58 +0100

    VEGA-2067 - Add payment amount radio options (#1177)
    * VEGA-2067 - Add predefined paymenr amount options to the add payment form
    
    * VEGA-2067 - Replace payment source option select field with radios
    
    * VEGA-2067 - Update payment amount regex validation to allow integers
    
    * VEGA-2067 - Fix tests
    
    * VEGA-2067 - Add option for zero payment amount

--- web/assets/main.js
+++ web/assets/main.js
@@ -26,6 +26,7 @@ import initSiriusHeader from "./sirius-header.js";
 import lpaFormSubtype from "./lpa-form-subtype.js";
 import showHideTrustCorpActiveRadios from "./show-hide-trust-corp-active-radios.js";
 import scrollSectionIntoView from "./scroll-section-into-view.js";
+import clearPaymentValue from "./clear-payment-value.js";
 import autoCheckSingleAttorneyApplicant from "./auto-check-single-attorney-applicant.js";
 
 const prefix = document.body.getAttribute("data-prefix");
@@ -55,6 +56,7 @@ initSiriusHeader();
 lpaFormSubtype();
 showHideTrustCorpActiveRadios();
 scrollSectionIntoView();
+clearPaymentValue();
 autoCheckSingleAttorneyApplicant();
 
 globalThis.htmx = htmx;
@@ -83,6 +85,7 @@ htmx.on("htmx:afterSettle", (event) => {
     lpaFormSubtype(swapDetails.target);
     scrollSectionIntoView(swapDetails.target);
     showHideTrustCorpActiveRadios(swapDetails.target);
+    clearPaymentValue(swapDetails.target);
     autoCheckSingleAttorneyApplicant(swapDetails.target);
 
     // Update the action panel width if swapping in create-document or edit-document content
