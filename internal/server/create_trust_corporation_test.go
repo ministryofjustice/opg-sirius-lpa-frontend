d4004271273ec9dac73e06568b85cdbecfe8c15d
Michael.Reckless <Michael.Reckless@justice.gov.uk>
2026-09-15 15:05:59 +0100

    VEGA-4167 - Fix test name after rebase

--- internal/server/create_trust_corporation_test.go
+++ internal/server/create_trust_corporation_test.go
@@ -530,7 +530,7 @@ func TestPostEditTrustCorporationUpdateNextAttorney(t *testing.T) {
 	}
 }
 
-func TestGetIdForNextAttorneyWillReturnNextNumberWithSameAppointedType(t *testing.T) {
+func TestGetIdForNextAttorneyTrustCorpsAndActiveAttorneys(t *testing.T) {
 
 	trustCorporations := []sirius.TrustCorporation{
 		{
