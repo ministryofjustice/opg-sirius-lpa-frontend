af03ebc2f1ced02f7b37dc572d64d23be6497fc2
Mike Reckless <Michael.Reckless@justice.gov.uk>
2026-09-23 11:39:50 +0100

    VEGA-4194 - Display trust corporation company name when selecting correspondent on LPA (#1198)
    * VEGA-4194 - Display trust corporation company name when selecting correspondent on an LPA. Increase cypress test coverage
    
    * VEGA-4194 - Add company name to correspondent details summary

--- cypress/e2e/select-or-create-correspondent.cy.js
+++ cypress/e2e/select-or-create-correspondent.cy.js
@@ -6,40 +6,44 @@ describe("Select or create correspondent", () => {
         attorneys: [
           {
             id: 3,
-            salutation: "Prof",
             firstname: "Melanie",
-            middlenames: "Josefina",
             surname: "Vanvolkenburg",
-            dob: "19/04/1978",
-            addressLine1: "29737 Andrew Plaza",
-            addressLine2: "Apt. 814",
-            addressLine3: "Gislasonside",
-            town: "Hirthehaven",
-            county: "Saskatchewan",
-            postcode: "S7R 9F9",
-            country: "Canada",
-            isAirmailRequired: true,
-            phoneNumber: "072345678",
-            email: "m.vancolkenburg@ca.test",
-            companyName: "ACME",
           },
           {
             id: 4,
-            salutation: "Dr",
             firstname: "Will",
-            middlenames: "Oswald",
             surname: "Niesborella",
-            dob: "01/07/1995",
-            addressLine1: "47209 Stacey Plain",
-            addressLine2: "Suite 113",
-            addressLine3: "Devonburgh",
-            town: "Marquardtville",
-            county: "North Carolina",
-            postcode: "40936",
-            country: "United States",
-            isAirmailRequired: true,
-            phoneNumber: "0841781784",
-            email: "docniesborella@mail.test",
+          },
+        ],
+        donor: {
+          id: 5,
+          firstname: "Sarah",
+          surname: "Jones",
+        },
+        replacementAttorneys: [
+          {
+            id: 6,
+            firstname: "Steven",
+            surname: "Munnell",
+          },
+        ],
+        certificateProviders: [
+          {
+            id: 7,
+            firstname: "Will",
+            surname: "Oswald",
+          },
+        ],
+        notifiedPersons: [
+          {
+            id: 8,
+            firstname: "James",
+            surname: "Rubin",
+          },
+        ],
+        trustCorporations: [
+          {
+            id: 9,
             companyName: "ACME",
           },
         ],
@@ -50,11 +54,10 @@ describe("Select or create correspondent", () => {
       status: 201,
       body: {},
     });
-
-    cy.visit("/select-or-create-correspondent?id=1&caseId=2&caseType=epa");
   });
 
   it("can select an existing attorney to create a correspondent", () => {
+    cy.visit("/select-or-create-correspondent?id=1&caseId=2&caseType=epa");
     cy.contains("Add a correspondent");
     cy.get("label[for=f-actor-1]").should(
       "contain.text",
@@ -67,7 +70,31 @@ describe("Select or create correspondent", () => {
     cy.url().should("include", "/create-epa");
   });
 
+  it("displays the correct actors for an lpa", () => {
+    cy.visit("/select-or-create-correspondent?id=1&caseId=2&caseType=lpa");
+    cy.contains("Add a correspondent");
+    cy.get("label[for=f-actor-donor]").should("contain.text", "Sarah Jones");
+    cy.get("label[for=f-attorney-1]").should(
+      "contain.text",
+      "Melanie Vanvolkenburg",
+    );
+    cy.get("label[for=f-replacement-1]").should(
+      "contain.text",
+      "Steven Munnell",
+    );
+    cy.get("label[for=f-certified-provider-1]").should(
+      "contain.text",
+      "Will Oswald",
+    );
+    cy.get("label[for=f-notified-person-1]").should(
+      "contain.text",
+      "James Rubin",
+    );
+    cy.get("label[for=f-trust-corporation-1]").should("contain.text", "ACME");
+  });
+
   it("select create a new correspondent by default", () => {
+    cy.visit("/select-or-create-correspondent?id=1&caseId=2&caseType=epa");
     cy.contains("Add a correspondent");
     cy.get("input#f-actor-new").should("be.checked");
     cy.get("button[type=submit]").click();
